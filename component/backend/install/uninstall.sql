6d3a5e1c5ed5584b131618a4f376d4d519f5334b
nikosdion <nikosdion@3e86161a-2ea4-46d7-bf8c-0be632b4e80f>
2011-02-14 22:17:32 +0000

    Deleting the test
    git-svn-id: https://subversion.assembla.com/svn/akeeba-subscriptions/trunk@3 3e86161a-2ea4-46d7-bf8c-0be632b4e80f

--- component/backend/install/uninstall.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `#__akeebasubs_levels`;
-DROP TABLE IF EXISTS `#__akeebasubs_subscriptions`;
-DROP TABLE IF EXISTS `#__akeebasubs_taxrules`;
-DROP TABLE IF EXISTS `#__akeebasubs_coupons`;
-DROP TABLE IF EXISTS `#__akeebasubs_upgrades`;
-DROP TABLE IF EXISTS `#__akeebasubs_users`;
-DROP VIEW IF EXISTS `#__akeebasubs_view_users`;
-DROP VIEW IF EXISTS #__akeebasubs_view_subscriptions;
