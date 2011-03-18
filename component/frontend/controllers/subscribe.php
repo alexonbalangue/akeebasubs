ecd6fec19c81ae32e65fe9ca1c9a4883a5b8f5d8
nikosdion <nikosdion@3e86161a-2ea4-46d7-bf8c-0be632b4e80f>
2011-03-18 00:05:37 +0000

    Subscribing doesn't work; it would redirect to the subscription page over and over and over
    git-svn-id: https://subversion.assembla.com/svn/akeeba-subscriptions/trunk@56 3e86161a-2ea4-46d7-bf8c-0be632b4e80f

--- component/frontend/controllers/subscribe.php
+++ component/frontend/controllers/subscribe.php
@@ -15,11 +15,21 @@ class ComAkeebasubsControllerSubscribe extends ComAkeebasubsControllerDefault
 		
 		$this->registerCallback('before.browse', array($this, '_denyAccess'));
 		$this->registerCallback('before.read', array($this, '_denyAccess'));
-		$this->registerCallback('before.edit', array($this, '_denyAccess'));
 		$this->registerCallback('before.delete', array($this, '_denyAccess'));
 	}
 	
+	/**
+	 * After March 17th, 2011 this is no longer called, but let's make sure it
+	 * won't break AGAIN.
+	 * 
+	 * @param KCommandContext $context
+	 */
 	protected function _actionAdd(KCommandContext $context)
+	{
+		return $this->_actionEdit($context);
+	}
+	
+	protected function _actionEdit(KCommandContext $context)
 	{
 		// For some odd reason, the add action is called twice?! Let's work around it...
 		static $result = null;
@@ -28,16 +38,24 @@ class ComAkeebasubsControllerSubscribe extends ComAkeebasubsControllerDefault
 		}
 
 		if($result) {
+			// CRUCIAL! If we don't set the redirect to an empty string, a redirection DOES occur; to the subscription page...
+			$this->setRedirect('');
 			// Show the (usually auto-submitting) form to the user
 			$view = $this->getView();
 			$view->setLayout('form');
-			return $view->display();
+			echo $view->display();
 		} else {
 			// Redirect to the level page
 			$url = JRoute::_('index.php?option=com_akeebasubs&view=level&id='.$this->getModel()->get('id','int',0));
 			$this->setRedirect($url);
 			return false;
 		}
+	}
+	
+	public function unlockData(KCommandContext $context)
+	{
+		// Intentionally do nothing; the default behaviour is to unlock a row,
+		// but this doesn't apply to this view.
 	}
 	
 	public function _denyAccess()
