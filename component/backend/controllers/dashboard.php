003311e9716af223462bb19605e538cf03d0387f
nikosdion <nikosdion@3e86161a-2ea4-46d7-bf8c-0be632b4e80f>
2011-03-23 23:14:38 +0000

    Refactored dashboard to use ComDefaultControllerView
    git-svn-id: https://subversion.assembla.com/svn/akeeba-subscriptions/trunk@71 3e86161a-2ea4-46d7-bf8c-0be632b4e80f

--- component/backend/controllers/dashboard.php
+++ component/backend/controllers/dashboard.php
@@ -7,7 +7,7 @@
 
 defined('KOOWA') or die('');
 
-class ComAkeebasubsControllerDashboard extends ComAkeebasubsControllerDefault 
+class ComAkeebasubsControllerDashboard extends ComDefaultControllerView
 {
 	public function __construct(KConfig $config)
 	{
@@ -29,25 +29,5 @@ class ComAkeebasubsControllerDashboard extends ComAkeebasubsControllerDefault
     {
         KRequest::set('get.hidemainmenu', 0);
         return $this;
-    }
-    
-    /**
-     * This method is required because KControllerBread (the default ancestor of all controllers)
-     * tries to load a row. We have no rows -or even a table- in this particular view, so...
-     * 
-     * @param $context KCommandContext The command context of the request
-     */
-    public function _actionDisplay(KCommandContext $context)
-    {
-		$view = $this->getView();
-		    
-        //Set the layout in the view
-	    if(isset($this->_request->layout)) {
-            $view->setLayout($this->_request->layout);
-	     }
-		
-        //Render the view and return the output
-		return $view->display();
-    }
-    
+    }    
 }
