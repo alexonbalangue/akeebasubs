638216dbb03dc80dca5bae5b690ff368a1f741e4
nikosdion <nikosdion@3e86161a-2ea4-46d7-bf8c-0be632b4e80f>
2011-05-23 22:12:18 +0000

    Smarter token check removal, with less code (thank you Johan!)
    git-svn-id: https://subversion.assembla.com/svn/akeeba-subscriptions/trunk@212 3e86161a-2ea4-46d7-bf8c-0be632b4e80f

--- component/frontend/controllers/behaviors/executable.php
+++ component/frontend/controllers/behaviors/executable.php
@@ -7,7 +7,7 @@
 
 defined('KOOWA') or die('');
 
-class ComAkeebasubsControllerBehaviorExecutable extends KControllerBehaviorExecutable
+class ComAkeebasubsControllerBehaviorExecutable extends ComDefaultControllerBehaviorExecutable
 {
 	/**
 	 * Remove CSRF protection from specific actions
@@ -33,12 +33,11 @@ class ComAkeebasubsControllerBehaviorExecutable extends KControllerBehaviorExecu
 	 */
 	private $_unprotectedViews = array('validate','callback','message');
 	
-	/**
-	 * Specialized behavior to handle POST requests without tokens on specific views
-	 */
-	public function execute( $name, KCommandContext $context) 
+	protected function _checkToken(KCommandContext $context)
 	{
-		if(KRequest::method() != 'GET') {
+	    //Check the token
+	    if($context->caller->isDispatched())
+	    {
 	    	// Always allow POSTS to the 'validate', 'callback' and 'message' views
 	    	$view = KRequest::get('post.view', 'cmd');
 	    	if(empty($view)) {
@@ -56,111 +55,11 @@ class ComAkeebasubsControllerBehaviorExecutable extends KControllerBehaviorExecu
 			if(in_array($action,$this->_unprotectedActions)) {
 				return true;
 			}
+			
+			// In all other cases, call the parent
+	        return parent::_checkToken($context);
 	    }
 	    
-	    //return parent::execute($name, $context);
-		return $this->_koowa_execute($name, $context);
+	    return true;
 	}
-	
-	// ======================================================================
-	// WARNING!! NON-DRY CODE AHEAD!!
-	// ======================================================================
-	// The rest of the code here is copied verbatim from
-	// ComDefaultControllerBehaviorExecutable because the execute() method is
-	// defined final and can not be overriden in child classes. We need to
-	// override it in order to disable token checks in our callback and
-	// message views, therefore we have to defy the DRY principle and do some
-	// ugly copying & pasting.
-	
-	public function _koowa_execute( $name, KCommandContext $context) 
-    { 
-        $parts = explode('.', $name); 
-        
-        if($parts[0] == 'before') 
-        { 
-            if(!$this->_checkToken($context)) 
-            {    
-                $context->setError(new KControllerException(
-                	'Invalid token or session time-out', KHttpResponse::FORBIDDEN
-                ));
-                
-                return false;
-            }
-        }
-        
-        return parent::execute($name, $context); 
-    }
-	
-	/**
-     * Generic authorize handler for controller add actions
-     * 
-     * @param   object      The command context
-     * @return  boolean     Can return both true or false.  
-     */
-    protected function _beforeAdd(KCommandContext $context)
-    {
-        if(version_compare(JVERSION,'1.6.0','ge')) {
-            $result = KFactory::get('lib.joomla.user')->authorise('core.create');
-        } else {
-            $result = KFactory::get('lib.joomla.user')->get('gid') > 18;
-        }
-          
-        return $result;
-    }
-    
-    /**
-     * Generic authorize handler for controller edit actions
-     * 
-     * @param   object      The command context
-     * @return  boolean     Can return both true or false.  
-     */
-    protected function _beforeEdit(KCommandContext $context)
-    {
-        if(version_compare(JVERSION,'1.6.0','ge')) {
-            $result = KFactory::get('lib.joomla.user')->authorise('core.edit');
-        } else {
-            $result = KFactory::get('lib.joomla.user')->get('gid') > 19;
-        }
-              
-        return $result;
-    }
-    
-    /**
-     * Generic authorize handler for controller delete actions
-     * 
-     * @param   object      The command context
-     * @return  boolean     Can return both true or false.  
-     */
-    protected function _beforeDelete(KCommandContext $context)
-    {
-        if(version_compare(JVERSION,'1.6.0','ge')) {
-            $result = KFactory::get('lib.joomla.user')->authorise('core.delete');
-        } else {
-            $result = KFactory::get('lib.joomla.user')->get('gid') > 20;
-        }
-            
-        return $result;
-    }
-    
-	/**
-	 * Check the token to prevent CSRF exploits
-	 *
-	 * @param   object  The command context
-	 * @return  boolean Returns FAKSE if the check failed. Otherwise TRUE.
-	 */
-    protected function _checkToken(KCommandContext $context)
-    {
-        //Check the token
-        if($context->caller->isDispatched())
-        {  
-            if((KRequest::method() != KHttpRequest::GET)) 
-            {     
-                if( KRequest::token() !== JUtility::getToken()) {     
-                    return false;
-                }
-            }
-        }
-        
-        return true;
-    }
 }
