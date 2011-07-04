ac70ac0c7175340e894b9090723743ecf9caa674
nikosdion <nikosdion@3e86161a-2ea4-46d7-bf8c-0be632b4e80f>
2011-07-04 08:17:22 +0000

    + Integration with ccInvoices to automatically create invoices for new subscriptions
    git-svn-id: https://subversion.assembla.com/svn/akeeba-subscriptions/trunk@253 3e86161a-2ea4-46d7-bf8c-0be632b4e80f

---
+++ plugins/akeebasubs/ccinvoices/ccinvoices.php
@@ -0,0 +1,154 @@
+<?php
+/**
+ * @package		akeebasubs
+ * @copyright	Copyright (c)2010-2011 Nicholas K. Dionysopoulos / AkeebaBackup.com
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html> or later
+ */
+
+defined('_JEXEC') or die();
+
+class plgAkeebasubsCcinvoices extends JPlugin
+{
+	/**
+	 * Called whenever a subscription is modified. Namely, when its enabled status,
+	 * payment status or valid from/to dates are changed.
+	 */
+	public function onAKSubscriptionChange(KDatabaseRowDefault $row)
+	{
+		// Only handle not expired subscriptions
+		if( ($row->state == "C") && ($row->enabled == 1) ) {
+			$db = JFactory::getDBO();
+		
+			// Get or create ccInvoices contact for user
+			$contact_id = $this->getContactID($row->user_id)
+			
+			// @todo Load the existing invoices of the user
+			$sql = 'SELECT * FROM `#__ccinvoices_invoices` WHERE `contact_id` = '.$contact_id;
+			$db->setQuery($sql);
+			$invoices = $db->loadObjectList();
+			
+			// Check if any of the invoices references this subscription
+			if(count($invoices)) foreach($invoices as $invoice) {
+				// Try to understand which subscription ID corresponds to this invoice
+				$note = strip_tags($invoice->note);
+				if(strstr($note, 'Subscription ID: ') === false) continue;
+				$start = strpos($note, 'Subscription ID: '); //17
+				$partial = substr($note, $start+17);
+				$parts = explode(' ', $partial);
+				$sub_id = $parts[0];
+				// If there is an invoice for the current subscription, bail out
+				if($sub_id == $row->id) return;
+			}
+			
+			// @todo Create new invoice
+			$db->setQuery('SELECT max(`number`) FROM `#__ccinvoices_invoices`');
+			$max1 = $db->loadResult();
+			$db->setQuery('SELECT max(`custom_invoice_number`) FROM `#__ccinvoices_invoices`');
+			$max2 = $db->loadResult();
+			$invoice_number = max($max1, $max2);
+			$invoice_number++;
+			
+			$subname = KFactory::tmp('admin::com.akeebasubs.model.levels')
+					->id($row->akeebasubs_level_id)
+					->getItem()
+					->title;
+			
+			$invoice = (object)array(
+				'number'		=> $invoice_number,
+				'invoice_date'	=> $row->created_on,
+				'status'		=> 4,
+				'duedate'		=> $row->created_on,
+				'numbercheck'	=> 0,
+				'communication'	=> '',
+				'discount'		=> 0,
+				'subtotal'		=> $row->net_amount,
+				'totaltax'		=> $row->tax_amount,
+				'total'			=> $row->gross_amount,
+				'quantity'		=> 1,
+				'pname'			=> $subname.' subscription',
+				'price'			=> $row->net_amount,
+				'tax'			=> sprintf('%.2f', 100*($row->tax_amount/$row->net_amount)),
+				'note'			=> "<p>Subscription ID: {$row->id}<br/>Paid with {$row->processor}, ref nr {$row->processor_key}</p>",
+				'contact_id'	=> $contact_id
+			);
+			$db->insertObject('', $invoice, 'id');
+			
+			// @todo Try to send the invoice
+		}
+	}
+	
+	/**
+	 * Called whenever the administrator asks to refresh integration status.
+	 * 
+	 * @param $user_id int The Joomla! user ID to refresh information for.
+	 */
+	public function onAKUserRefresh($user_id)
+	{
+		// Do nothing
+	}
+	
+	private function getContactID($userid)
+	{
+		$db = JFactory::getDBO();
+		
+		$sql = 'SELECT `contact_id` FROM `#__ccinvoices_users` WHERE `user_id` = '.(int)$userid;
+		$db->setQuery($sql);
+		$id = $db->loadResult();
+		
+		if(!$id) {
+			$id = $this->createContact($userid);
+		}
+		
+		return $id;
+	}
+	
+	private function createContact($userid)
+	{
+		$db = JFactory::getDBO();
+		
+		// Load user data
+		$juser = KFactory::tmp('admin::com.akeebasubs.model.jusers')
+			->id($user_id)
+			->getItem();
+			
+		$kuser = KFactory::tmp('admin::com.akeebasubs.model.users')
+			->user_id($user_id)
+			->getItem();
+		
+		// get the next contact number
+		$db->setQuery('SELECT max(contact_number) FROM `#__ccinvoices_contacts`');
+		$contact_number = $db->loadResult();
+		if(!$contact_number) $contact_number = 0;
+		$contact_number++;
+		
+		// get country/state names
+		$dummy = KFactory::get('admin::com.akeebasubs.template.helper.listbox');
+		$country = ComAkeebasubsTemplateHelperListbox::$countries[$kuser->country];
+		$state = ComAkeebasubsTemplateHelperListbox::$states[$kuser->state];
+		if($state == 'N/A') $state = '';
+		
+		// Create contact
+		$name = $juser->name;
+		$contact = $name;
+		$address = $kuser->address."\n".
+			(empty($kuser->address2) ? '' : $kuser->address2."\n").
+			$kuser->zip." ".$kuser->city."\n".
+			(empty($state) ? '' : "$state\n") .
+			$country;
+		$email = $juser->email;
+		
+		$sql = 'REPLACE INTO `#__ccinvoices_contacts` (`name`,`contact`,`contact_number`,`address`,`email`) VALUES ('.
+			$db->quote($name).', '.
+			$db->quote($contact).', '.
+			$db->quote($contact_number).', '.
+			$db->quote($address).', '.
+			$db->quote($email).
+		')';
+		$db->setQuery($sql);
+		$db->query();
+		
+		$id = $db->insertid();
+		return $id;
+	}
+
+}
