37c4e5a2c55a4fe8afc1423ea06fed70381e9cb0
nikosdion <nikosdion@3e86161a-2ea4-46d7-bf8c-0be632b4e80f>
2011-07-04 08:16:51 +0000

    # Joomla! 1.6 doesn't run the installation SQL file on update; workaround applied
    git-svn-id: https://subversion.assembla.com/svn/akeeba-subscriptions/trunk@250 3e86161a-2ea4-46d7-bf8c-0be632b4e80f

--- component/script.akeebasubs.php
+++ component/script.akeebasubs.php
@@ -7,4 +7,34 @@ class Com_AkeebasubsInstallerScript {
 		define('_AKEEBA_HACK', 1);
 		require_once('install.akeebasubs.php');
 	}
+	
+	/*
+	 * Joomla! 1.6 doesn't run the SQL commands on update. Workaround dully applied!
+	 */
+	function update($parent) {
+		$db = JFactory::getDBO();
+		if(method_exists($parent, 'extension_root')) {
+			$sqlfile = $parent->getPath('extension_root').DS.'install'.DS.'install.sql';
+		} else {
+			$sqlfile = $parent->getParent()->getPath('extension_root').DS.'install'.DS.'install.sql';
+		}
+		$buffer = file_get_contents($sqlfile);
+		if ($buffer !== false) {
+			jimport('joomla.installer.helper');
+			$queries = JInstallerHelper::splitSql($buffer);
+			if (count($queries) != 0) {
+				foreach ($queries as $query)
+				{
+					$query = trim($query);
+					if ($query != '' && $query{0} != '#') {
+						$db->setQuery($query);
+						if (!$db->query()) {
+							JError::raiseWarning(1, JText::sprintf('JLIB_INSTALLER_ERROR_SQL_ERROR', $db->stderr(true)));
+							return false;
+						}
+					}
+				}
+			}
+		}
+	}
 }
