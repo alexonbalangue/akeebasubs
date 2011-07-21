30ae2abc00a4c2c0d9f1caf3f3bc1e753a4d459d
nikosdion <nikosdion@3e86161a-2ea4-46d7-bf8c-0be632b4e80f>
2011-07-21 15:18:15 +0000

    Streamlined Dashboard UI, looks better in Joomla! 1.6 too.
    git-svn-id: https://subversion.assembla.com/svn/akeeba-subscriptions/trunk@311 3e86161a-2ea4-46d7-bf8c-0be632b4e80f

--- component/backend/views/dashboard/tmpl/default.php
+++ component/backend/views/dashboard/tmpl/default.php
@@ -24,243 +24,117 @@ defined('KOOWA') or die('Restricted access');?>
 -->
 
 <div id="cpanel"  style="width:51%;float:left;">
-	<?= @helper('tabs.startPane', array('id' => 'quick', 'attribs' => array('height' => '275px'))) ?>
+	<!--
+	<h2><?//=@text('COM_AKEEBASUBS_DASHBOARD_WELCOME')?></h2>
+	<?//=@template('default_welcome');?>
+	-->
+
+	<h2><?=@text('COM_AKEEBASUBS_DASHBOARD_SALES')?></h2>
+	<div id="aksaleschart">
+		<img src="media://com_akeebasubs/images/throbber.gif" id="akthrobber" />
+		<p id="aksaleschart-nodata" style="display:none">
+			<?=@text('COM_AKEEBASUBS_DASHBOARD_STATS_NODATA')?>
+		</p>
+	</div>
 	
-	<?= @helper('tabs.startPanel', array('title' => @text('COM_AKEEBASUBS_DASHBOARD_WELCOME'))) ?>
-		<?=@template('default_welcome');?>
-    <?= @helper('tabs.endPanel') ?>
-
-	<?= @helper('tabs.startPanel', array('title' => @text('COM_AKEEBASUBS_DASHBOARD_OPERATIONS'))) ?>
-		<div style="margin-left: 13px; text-align:center; height:234px;max-width:575px">
-			<?=@template('default_quickicons'); ?>
-			<div class="clr">    
-	    </div>
-    <?= @helper('tabs.endPanel') ?>
-
-	<?= @helper('tabs.endPane') ?>
+	<h2><?=@text('COM_AKEEBASUBS_DASHBOARD_OPERATIONS')?></h2>
+	<?=@template('default_quickicons'); ?>
 </div>
 
 <div style="width:47%;float:right;">
-	<?= @helper('tabs.startPane', array('id' => 'stats', 'attribs' => array('height' => '300px'))) ?>
-	
-	<?= @helper('tabs.startPanel', array('title' => @text('COM_AKEEBASUBS_DASHBOARD_SALES'))) ?>
-		<div id="aksaleschart">
-			<img src="media://com_akeebasubs/images/throbber.gif" id="akthrobber" />
-			<p id="aksaleschart-nodata" style="display:none">
-				<?=@text('COM_AKEEBASUBS_DASHBOARD_STATS_NODATA')?>
-			</p>
-		</div>
-	<?= @helper('tabs.endPanel') ?>
-	
-	<?= @helper('tabs.startPanel', array('title' => @text('COM_AKEEBASUBS_DASHBOARD_STATS'))) ?>
-		<table width="100%" class="adminlist">
-			<tbody>
-			<tr class="row0">
-				<td width="50%"><?=@text('COM_AKEEBASUBS_DASHBOARD_STATS_LASTYEAR')?></td>
-				<td align="right" width="25%">
-					<?= KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
-						->publish_up((gmdate('Y')-1).'-01-01 00:00:00')
+	<h2><?=@text('COM_AKEEBASUBS_DASHBOARD_STATS')?></h2>
+	<table width="100%" class="adminlist">
+		<tbody>
+		<tr class="row0">
+			<td width="50%"><?=@text('COM_AKEEBASUBS_DASHBOARD_STATS_LASTYEAR')?></td>
+			<td align="right" width="25%">
+				<?= KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
+					->publish_up((gmdate('Y')-1).'-01-01 00:00:00')
+					->publish_down((gmdate('Y')-1).'-12-31 23:59:59')
+					->paystate('C')
+					->getTotal()
+				?>
+			</td>
+			<td align="right" width="25%">
+				<?=KFactory::get('admin::com.akeebasubs.model.configs')->getConfig()->currencysymbol?>
+				<?= sprintf('%.02f',
+					KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
+						->publish_up((gmdate('Y')-1).'-01-01')
 						->publish_down((gmdate('Y')-1).'-12-31 23:59:59')
+						->moneysum(1)
 						->paystate('C')
 						->getTotal()
-					?>
-				</td>
-				<td align="right" width="25%">
-					<?=KFactory::get('admin::com.akeebasubs.model.configs')->getConfig()->currencysymbol?>
-					<?= sprintf('%.02f',
-						KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
-							->publish_up((gmdate('Y')-1).'-01-01')
-							->publish_down((gmdate('Y')-1).'-12-31 23:59:59')
-							->moneysum(1)
-							->paystate('C')
-							->getTotal()
-					)?>
-				</td>
-			</tr>
-			<tr class="row1">
-				<td><?=@text('COM_AKEEBASUBS_DASHBOARD_STATS_THISYEAR')?></td>
-				<td align="right">
-					<?= KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
+				)?>
+			</td>
+		</tr>
+		<tr class="row1">
+			<td><?=@text('COM_AKEEBASUBS_DASHBOARD_STATS_THISYEAR')?></td>
+			<td align="right">
+				<?= KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
+					->publish_up(gmdate('Y').'-01-01')
+					->publish_down(gmdate('Y').'-12-31 23:59:59')
+					->paystate('C')
+					->getTotal()
+				?>
+			</td>
+			<td align="right">
+				<?=KFactory::get('admin::com.akeebasubs.model.configs')->getConfig()->currencysymbol?>
+				<?= sprintf('%.02f',
+					KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
 						->publish_up(gmdate('Y').'-01-01')
 						->publish_down(gmdate('Y').'-12-31 23:59:59')
+						->moneysum(1)
 						->paystate('C')
 						->getTotal()
-					?>
-				</td>
-				<td align="right">
-					<?=KFactory::get('admin::com.akeebasubs.model.configs')->getConfig()->currencysymbol?>
-					<?= sprintf('%.02f',
-						KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
-							->publish_up(gmdate('Y').'-01-01')
-							->publish_down(gmdate('Y').'-12-31 23:59:59')
-							->moneysum(1)
-							->paystate('C')
-							->getTotal()
-					)?>
-				</td>
-			</tr>
-			<tr class="row0">
-				<td><?=@text('COM_AKEEBASUBS_DASHBOARD_STATS_LASTMONTH')?></td>
-				<td align="right">
-					<?
-						$y = gmdate('Y');
-						$m = gmdate('m');
-						if($m == 1) {
-							$m = 12; $y -= 1;
-						} else {
-							$m -= 1;
-						}
-						switch($m) {
-							case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-								$lmday = 31; break;
-							case 4: case 6: case 9: case 11:
-								$lmday = 30; break;
-							case 2:
-								if( !($y % 4) && ($y % 400) ) {
-									$lmday = 29;
-								} else {
-									$lmday = 28;
-								}
-						}
-					?>
-					<?= KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
+				)?>
+			</td>
+		</tr>
+		<tr class="row0">
+			<td><?=@text('COM_AKEEBASUBS_DASHBOARD_STATS_LASTMONTH')?></td>
+			<td align="right">
+				<?
+					$y = gmdate('Y');
+					$m = gmdate('m');
+					if($m == 1) {
+						$m = 12; $y -= 1;
+					} else {
+						$m -= 1;
+					}
+					switch($m) {
+						case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+							$lmday = 31; break;
+						case 4: case 6: case 9: case 11:
+							$lmday = 30; break;
+						case 2:
+							if( !($y % 4) && ($y % 400) ) {
+								$lmday = 29;
+							} else {
+								$lmday = 28;
+							}
+					}
+				?>
+				<?= KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
+					->publish_up($y.'-'.$m.'-01')
+					->publish_down($y.'-'.$m.'-'.$lmday.' 23:59:59')
+					->paystate('C')
+					->getTotal()
+				?>
+			</td>
+			<td align="right">
+				<?=KFactory::get('admin::com.akeebasubs.model.configs')->getConfig()->currencysymbol?>
+				<?= sprintf('%.02f',
+					KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
 						->publish_up($y.'-'.$m.'-01')
 						->publish_down($y.'-'.$m.'-'.$lmday.' 23:59:59')
+						->moneysum(1)
 						->paystate('C')
 						->getTotal()
-					?>
-				</td>
-				<td align="right">
-					<?=KFactory::get('admin::com.akeebasubs.model.configs')->getConfig()->currencysymbol?>
-					<?= sprintf('%.02f',
-						KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
-							->publish_up($y.'-'.$m.'-01')
-							->publish_down($y.'-'.$m.'-'.$lmday.' 23:59:59')
-							->moneysum(1)
-							->paystate('C')
-							->getTotal()
-					)?>
-				</td>
-			</tr>
-			<tr class="row1">
-				<td><?=@text('COM_AKEEBASUBS_DASHBOARD_STATS_THISMONTH')?></td>
-				<td align="right">
-					<?
-						switch(gmdate('m')) {
-							case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-								$lmday = 31; break;
-							case 4: case 6: case 9: case 11:
-								$lmday = 30; break;
-							case 2:
-								$y = gmdate('Y');
-								if( !($y % 4) && ($y % 400) ) {
-									$lmday = 29;
-								} else {
-									$lmday = 28;
-								}
-						}
-					?>
-					<?= KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
-						->publish_up(gmdate('Y').'-'.gmdate('m').'-01')
-						->publish_down(gmdate('Y').'-'.gmdate('m').'-'.$lmday.' 23:59:59')
-						->paystate('C')
-						->getTotal()
-					?>
-				</td>
-				<td align="right">
-					<?=KFactory::get('admin::com.akeebasubs.model.configs')->getConfig()->currencysymbol?>
-					<?= sprintf('%.02f',
-						KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
-							->publish_up(gmdate('Y').'-'.gmdate('m').'-01')
-							->publish_down(gmdate('Y').'-'.gmdate('m').'-'.$lmday.' 23:59:59')
-							->moneysum(1)
-							->paystate('C')
-							->getTotal()
-					)?>
-				</td>
-			</tr>
-			<tr class="row0">
-				<td width="50%"><?=@text('COM_AKEEBASUBS_DASHBOARD_STATS_LAST7DAYS')?></td>
-				<td align="right" width="25%">
-					<?= KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
-						->publish_up( gmdate('Y-m-d', time()-7*24*3600) )
-						->publish_down( gmdate('Y-m-d') )
-						->paystate('C')
-						->getTotal()
-					?>
-				</td>
-				<td align="right" width="25%">
-					<?=KFactory::get('admin::com.akeebasubs.model.configs')->getConfig()->currencysymbol?>
-					<?= sprintf('%.02f',
-						KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
-							->publish_up( gmdate('Y-m-d', time()-7*24*3600) )
-							->publish_down( gmdate('Y-m-d') )
-							->moneysum(1)
-							->paystate('C')
-							->getTotal()
-					)?>
-				</td>
-			</tr>
-			<tr class="row1">
-				<td width="50%"><?=@text('COM_AKEEBASUBS_DASHBOARD_STATS_YESTERDAY')?></td>
-				<td align="right" width="25%">
-					<?
-					$date = new DateTime();
-					$date->setDate(gmdate('Y'), gmdate('m'), gmdate('d'));
-					$date->modify("-1 day");
-					$yesterday = $date->format("Y-m-d");
-					$date->modify("+1 day")
-					?>
-					<?= KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
-						->publish_up( $yesterday )
-						->publish_down( $date->format("Y-m-d") )
-						->paystate('C')
-						->getTotal()
-					?>
-				</td>
-				<td align="right" width="25%">
-					<?=KFactory::get('admin::com.akeebasubs.model.configs')->getConfig()->currencysymbol?>
-					<?= sprintf('%.02f',
-						KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
-							->publish_up( $yesterday )
-							->publish_down( $date->format("Y-m-d") )
-							->moneysum(1)
-							->paystate('C')
-							->getTotal()
-					)?>
-				</td>
-			</tr>
-			<tr class="row0">
-				<td width="50%"><strong><?=@text('COM_AKEEBASUBS_DASHBOARD_STATS_TODAY')?></strong></td>
-				<td align="right" width="25%">
-					<strong>
-					<?php
-						$expiry = clone $date;
-						$expiry->modify('+1 day');
-					?>
-					<?= KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
-						->publish_up( $date->format("Y-m-d") )
-						->publish_down( $expiry->format("Y-m-d") )
-						->paystate('C')
-						->getTotal()
-					?>
-					</strong>
-				</td>
-				<td align="right" width="25%">
-					<strong>
-					<?=KFactory::get('admin::com.akeebasubs.model.configs')->getConfig()->currencysymbol?>
-					<?= sprintf('%.02f',
-						KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
-							->publish_up( $date->format("Y-m-d") )
-							->publish_down( $expiry->format("Y-m-d") )
-							->paystate('C')
-							->moneysum(1)
-							->getTotal()
-					)?>
-					</strong>
-				</td>
-			</tr>
-			<tr class="row1">
+				)?>
+			</td>
+		</tr>
+		<tr class="row1">
+			<td><?=@text('COM_AKEEBASUBS_DASHBOARD_STATS_THISMONTH')?></td>
+			<td align="right">
 				<?
 					switch(gmdate('m')) {
 						case 1: case 3: case 5: case 7: case 8: case 10: case 12:
@@ -275,47 +149,160 @@ defined('KOOWA') or die('Restricted access');?>
 								$lmday = 28;
 							}
 					}
-					$daysin = gmdate('d');
-					$numsubs = KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
-						->publish_up(gmdate('Y').'-'.gmdate('m').'-01')
-						->publish_down(gmdate('Y').'-'.gmdate('m').'-'.$lmday.' 23:59:59')
-						->paystate('C')
-						->getTotal();
-					$summoney = KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
+				?>
+				<?= KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
+					->publish_up(gmdate('Y').'-'.gmdate('m').'-01')
+					->publish_down(gmdate('Y').'-'.gmdate('m').'-'.$lmday.' 23:59:59')
+					->paystate('C')
+					->getTotal()
+				?>
+			</td>
+			<td align="right">
+				<?=KFactory::get('admin::com.akeebasubs.model.configs')->getConfig()->currencysymbol?>
+				<?= sprintf('%.02f',
+					KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
 						->publish_up(gmdate('Y').'-'.gmdate('m').'-01')
 						->publish_down(gmdate('Y').'-'.gmdate('m').'-'.$lmday.' 23:59:59')
 						->moneysum(1)
 						->paystate('C')
-						->getTotal();
+						->getTotal()
+				)?>
+			</td>
+		</tr>
+		<tr class="row0">
+			<td width="50%"><?=@text('COM_AKEEBASUBS_DASHBOARD_STATS_LAST7DAYS')?></td>
+			<td align="right" width="25%">
+				<?= KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
+					->publish_up( gmdate('Y-m-d', time()-7*24*3600) )
+					->publish_down( gmdate('Y-m-d') )
+					->paystate('C')
+					->getTotal()
 				?>
-				<td width="50%"><strong><?=@text('COM_AKEEBASUBS_DASHBOARD_STATS_AVERAGETHISMONTH')?></strong></td>
-				<td align="right" width="25%">
-					<strong><?=sprintf('%01.1f', $numsubs/$daysin)?><strong>
-				</td>
-				<td align="right" width="25%">
-					<strong>
-					<?=KFactory::get('admin::com.akeebasubs.model.configs')->getConfig()->currencysymbol?>
-					<?=sprintf('%01.2f', $summoney/$daysin)?>
-					</strong>
-				</td>
-			</tr>
-			<tr class="row0">
-				<td width="50%"><strong><?=@text('COM_AKEEBASUBS_DASHBOARD_STATS_PROJECTION')?></strong></td>
-				<td align="right" width="25%">
-					<em><?=sprintf('%01u', $lmday * ($numsubs/$daysin))?></em>
-				</td>
-				<td align="right" width="25%">
-					<em>
-					<?=KFactory::get('admin::com.akeebasubs.model.configs')->getConfig()->currencysymbol?>
-					<?=sprintf('%01.2f', $lmday * ($summoney/$daysin))?>
-					</em>
-				</td>
-			</tr>
-			</tbody>
-		</table>
-	<?= @helper('tabs.endPanel') ?>
-	
-	<?= @helper('tabs.endPane') ?>
+			</td>
+			<td align="right" width="25%">
+				<?=KFactory::get('admin::com.akeebasubs.model.configs')->getConfig()->currencysymbol?>
+				<?= sprintf('%.02f',
+					KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
+						->publish_up( gmdate('Y-m-d', time()-7*24*3600) )
+						->publish_down( gmdate('Y-m-d') )
+						->moneysum(1)
+						->paystate('C')
+						->getTotal()
+				)?>
+			</td>
+		</tr>
+		<tr class="row1">
+			<td width="50%"><?=@text('COM_AKEEBASUBS_DASHBOARD_STATS_YESTERDAY')?></td>
+			<td align="right" width="25%">
+				<?
+				$date = new DateTime();
+				$date->setDate(gmdate('Y'), gmdate('m'), gmdate('d'));
+				$date->modify("-1 day");
+				$yesterday = $date->format("Y-m-d");
+				$date->modify("+1 day")
+				?>
+				<?= KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
+					->publish_up( $yesterday )
+					->publish_down( $date->format("Y-m-d") )
+					->paystate('C')
+					->getTotal()
+				?>
+			</td>
+			<td align="right" width="25%">
+				<?=KFactory::get('admin::com.akeebasubs.model.configs')->getConfig()->currencysymbol?>
+				<?= sprintf('%.02f',
+					KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
+						->publish_up( $yesterday )
+						->publish_down( $date->format("Y-m-d") )
+						->moneysum(1)
+						->paystate('C')
+						->getTotal()
+				)?>
+			</td>
+		</tr>
+		<tr class="row0">
+			<td width="50%"><strong><?=@text('COM_AKEEBASUBS_DASHBOARD_STATS_TODAY')?></strong></td>
+			<td align="right" width="25%">
+				<strong>
+				<?php
+					$expiry = clone $date;
+					$expiry->modify('+1 day');
+				?>
+				<?= KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
+					->publish_up( $date->format("Y-m-d") )
+					->publish_down( $expiry->format("Y-m-d") )
+					->paystate('C')
+					->getTotal()
+				?>
+				</strong>
+			</td>
+			<td align="right" width="25%">
+				<strong>
+				<?=KFactory::get('admin::com.akeebasubs.model.configs')->getConfig()->currencysymbol?>
+				<?= sprintf('%.02f',
+					KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
+						->publish_up( $date->format("Y-m-d") )
+						->publish_down( $expiry->format("Y-m-d") )
+						->paystate('C')
+						->moneysum(1)
+						->getTotal()
+				)?>
+				</strong>
+			</td>
+		</tr>
+		<tr class="row1">
+			<?
+				switch(gmdate('m')) {
+					case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+						$lmday = 31; break;
+					case 4: case 6: case 9: case 11:
+						$lmday = 30; break;
+					case 2:
+						$y = gmdate('Y');
+						if( !($y % 4) && ($y % 400) ) {
+							$lmday = 29;
+						} else {
+							$lmday = 28;
+						}
+				}
+				$daysin = gmdate('d');
+				$numsubs = KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
+					->publish_up(gmdate('Y').'-'.gmdate('m').'-01')
+					->publish_down(gmdate('Y').'-'.gmdate('m').'-'.$lmday.' 23:59:59')
+					->paystate('C')
+					->getTotal();
+				$summoney = KFactory::tmp('admin::com.akeebasubs.model.subscriptions')
+					->publish_up(gmdate('Y').'-'.gmdate('m').'-01')
+					->publish_down(gmdate('Y').'-'.gmdate('m').'-'.$lmday.' 23:59:59')
+					->moneysum(1)
+					->paystate('C')
+					->getTotal();
+			?>
+			<td width="50%"><strong><?=@text('COM_AKEEBASUBS_DASHBOARD_STATS_AVERAGETHISMONTH')?></strong></td>
+			<td align="right" width="25%">
+				<strong><?=sprintf('%01.1f', $numsubs/$daysin)?><strong>
+			</td>
+			<td align="right" width="25%">
+				<strong>
+				<?=KFactory::get('admin::com.akeebasubs.model.configs')->getConfig()->currencysymbol?>
+				<?=sprintf('%01.2f', $summoney/$daysin)?>
+				</strong>
+			</td>
+		</tr>
+		<tr class="row0">
+			<td width="50%"><strong><?=@text('COM_AKEEBASUBS_DASHBOARD_STATS_PROJECTION')?></strong></td>
+			<td align="right" width="25%">
+				<em><?=sprintf('%01u', $lmday * ($numsubs/$daysin))?></em>
+			</td>
+			<td align="right" width="25%">
+				<em>
+				<?=KFactory::get('admin::com.akeebasubs.model.configs')->getConfig()->currencysymbol?>
+				<?=sprintf('%01.2f', $lmday * ($summoney/$daysin))?>
+				</em>
+			</td>
+		</tr>
+		</tbody>
+	</table>
 	
 	<?=@helper('site::com.akeebasubs.template.helper.modules.loadposition', array('position' => 'akeebasubscriptionsstats'))?>
 </div>
