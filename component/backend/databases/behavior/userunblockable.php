05605cff563d246dc7a5a10a9a0f30bda32f8762
nikosdion <nikosdion@3e86161a-2ea4-46d7-bf8c-0be632b4e80f>
2011-08-11 08:51:50 +0000

    Fixed brainfart
    git-svn-id: https://subversion.assembla.com/svn/akeeba-subscriptions/trunk@344 3e86161a-2ea4-46d7-bf8c-0be632b4e80f

--- component/backend/databases/behavior/userunblockable.php
+++ component/backend/databases/behavior/userunblockable.php
@@ -14,31 +14,9 @@ defined('KOOWA') or die('');
  */
 class ComAkeebasubsDatabaseBehaviorUserunblockable extends KDatabaseBehaviorAbstract
 {
-	protected $_cache = array();
-	
-	/**
-	 * Fires whenever a record is read.It will cache the status fields
-	 * (enabled, state, publish_up, publish_down) so that they can be
-	 * checked in _afterTableUpdate.
-	 */	
-	public function _afterTableSelect(KCommandContext $context)
+	public function _afterTableInsert(KCommandContext $context)
 	{
-		$contextRows = $context->data;
-		if($contextRows instanceof KDatabaseRowAbstract) {
-			$rows = array($contextRows);
-		} else {
-			if(!($contextRows instanceof KDatabaseRowsetAbstract)) return true;
-			$rows = $contextRows;
-		}
-		
-		foreach($rows as $row) {
-			if( !($row instanceof KDatabaseRowAbstract) ) continue;
-			$this->_cache[$row->id] = array(
-				'enabled'		=> $row->enabled,
-			);
-		}
-		
-		return true;
+		return $this->_afterTableUpdate($context);
 	}
 	
 	/**
@@ -57,24 +35,7 @@ class ComAkeebasubsDatabaseBehaviorUserunblockable extends KDatabaseBehaviorAbst
 		
 		foreach($rows as $row) {
 			if( !($row instanceof KDatabaseRowAbstract) ) continue;
-			
-			if(array_key_exists($row->id, $this->_cache)) {
-				$cache = $this->_cache[$row->id];
-			} else {
-				$cache = array(
-					'enabled'		=> null,
-				);
-			}
-			
-			$modified = false;
-			foreach($cache as $key => $value) {
-				if($row->$key != $value) {
-					$modified = true;
-					break;
-				}
-			}
-			
-			if($modified && $row->enabled) {
+			if($row->enabled) {
 				// Is this a paid subscription?
 				if($row->state != 'C') continue;
 				// Cool! Let's unblock the user
@@ -89,33 +50,4 @@ class ComAkeebasubsDatabaseBehaviorUserunblockable extends KDatabaseBehaviorAbst
 		
 		return true;
 	}
-	
-	/**
-	 * Fires after creating a record. It will always trigger the events.
-	 */
-	public function _afterTableInsert(KCommandContext $context)
-	{
-		$contextRows = $context->data;
-		if($contextRows instanceof KDatabaseRowAbstract) {
-			$rows = array($contextRows);
-		} else {
-			if(!($contextRows instanceof KDatabaseRowsetAbstract)) return true;
-			$rows = $contextRows;
-		}
-		
-		// Load the "akeebasubs" plugins
-		jimport('joomla.plugin.helper');
-		JPluginHelper::importPlugin('akeebasubs');
-		$app = JFactory::getApplication();
-	
-		foreach($rows as $row) {
-			if( !($row instanceof KDatabaseRowAbstract) ) continue;
-			
-			// Fire plugins (onAKSubscriptionCreate)
-			$jResponse = $app->triggerEvent('onAKSubscriptionCreate', array($row));
-		}
-	
-		return true;
-	}
-	
 }
