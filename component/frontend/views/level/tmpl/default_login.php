9064409e60111afb42834ad6273933d3152a7c64
nikosdion <nikosdion@3e86161a-2ea4-46d7-bf8c-0be632b4e80f>
2011-09-09 17:09:24 +0000

    # Integrated login form in subscription page not working when using SEF URLs without Apache's mod_rewrite
    git-svn-id: https://subversion.assembla.com/svn/akeeba-subscriptions/trunk@372 3e86161a-2ea4-46d7-bf8c-0be632b4e80f

--- component/frontend/views/level/tmpl/default_login.php
+++ component/frontend/views/level/tmpl/default_login.php
@@ -1,6 +1,6 @@
 <?php defined('KOOWA') or die(); ?>
 
-<form action="index.php" method="post">
+<form action="<?php echo JURI::base() ?>index.php" method="post">
 	<input type="hidden" name="option" value="<?=version_compare(JVERSION,'1.6.0','ge') ? 'com_users' : 'com_user'?>" />
 	<input type="hidden" name="task" value="<?=version_compare(JVERSION,'1.6.0','ge') ? 'user.login' : 'login'?>" />
 	<input type="hidden" name="return" value="<?=base64_encode(str_replace('&amp;','&',@route('view=level&layout=default&slug='.KRequest::get('get.slug','slug',''))))?>" />
