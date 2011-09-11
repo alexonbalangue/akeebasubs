3820e9d974d1b213d9bcfd7f1b1c7b040224d87f
nikosdion <nikosdion@3e86161a-2ea4-46d7-bf8c-0be632b4e80f>
2011-09-11 13:16:55 +0000

    Typo
    git-svn-id: https://subversion.assembla.com/svn/akeeba-subscriptions/trunk@386 3e86161a-2ea4-46d7-bf8c-0be632b4e80f

--- build/templates/version.php
+++ build/templates/version.php
@@ -4,5 +4,5 @@ defined('KOOWA') or die();
 define('AKEEBASUBS_VERSION', '##VERSION##');
 define('AKEEBASUBS_DATE', '##DATE##');
 define('AKEEBASUBS_PRO', '##PRO##');
-define('AKEEBASUBS_VERSIONHASH', md5(AKEEBASUBS_VERSION.AKEEBASUBS_DATE.KFactory::get('lib.joomla.config')->getValue('secret','')));
+define('AKEEBASUBS_VERSIONHASH', md5(AKEEBASUBS_VERSION.AKEEBASUBS_DATE.JFactory::getConfig()->getValue('secret','')));
 ?>
