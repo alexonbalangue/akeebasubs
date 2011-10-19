4c32866a23ef11f7d79a0d9bba304b445833b25c
Nicholas Dionysopoulos <nicholas@akeebabackup.com>
2011-10-19 18:24:24 +0300

    Minor changes in messages view

--- component/frontend/controllers/messages.php
+++ component/frontend/controllers/messages.php
@@ -5,6 +5,8 @@
  *  @license GNU General Public License version 3, or later
  */
 
+defined('_JEXEC') or die();
+
 class AkeebasubsControllerMessages extends FOFController
 {
 	public function __construct($config = array()) {
