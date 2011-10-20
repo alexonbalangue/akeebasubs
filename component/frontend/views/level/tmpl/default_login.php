6696ede8211d1bedc7c23ae6d3318da65ddd058b
Nicholas Dionysopoulos <nicholas@akeebabackup.com>
2011-10-20 13:25:50 +0300

    Improved login box

--- component/frontend/views/level/tmpl/default_login.php
+++ component/frontend/views/level/tmpl/default_login.php
@@ -6,12 +6,12 @@
  */
 
 defined('_JEXEC') or die();
+
+$login_url = version_compare(JVERSION, '1.6.0', 'ge') ? 'index.php?option=com_users&task=user.login' : 'option=com_user&task=login';
 ?>
 
-<form action="<?php echo JURI::base() ?>index.php" method="post">
-	<input type="hidden" name="option" value="<?php echo version_compare(JVERSION,'1.6.0','ge') ? 'com_users' : 'com_user'?>" />
-	<input type="hidden" name="task" value="<?php echo version_compare(JVERSION,'1.6.0','ge') ? 'user.login' : 'login'?>" />
-	<input type="hidden" name="return" value="<?php echo base64_encode(str_replace('&amp;','&',JRoute::_('index.php?option=com_akeebasubs&view=level&layout=default&slug='.FOFInput::getString('slug','',$this->input))))?>" />
+<form action="<?php echo rtrim(JURI::base(),'/') ?><?php echo JRoute::_($login_url) ?>" method="post">
+	<input type="hidden" name="return" value="<?php echo base64_encode(str_replace('&amp;','&',rtrim(JURI::base(),'/').JRoute::_('index.php?option=com_akeebasubs&view=level&layout=default&slug='.FOFInput::getString('slug','',$this->input))))?>" />
 	<input type="hidden" name="remember" value="1" />
 	<input type="hidden" name="<?php echo JUtility::getToken();?>" value="1" />
 	<fieldset>
