9445e33a60eb5cd2f0783bacc9e551ef5729d0c9
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2011-10-21 20:50:21 +0300

    Administrator module (latest subscriptions) is now refactored. Two to go!

---
+++ modules/admin/akeebasubs/mod_akeebasubs.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package		mod_akeebasubs
+ * @copyright 	Copyright (c) 2011 Nicholas K. Dionysopoulos / AkeebaBackup.com
+ * @license 	GNU General Public License version 3 or later
+ * 
+ * Originally written by Sander Potjer for Akeeba Subscriptions 1.0. Refactored
+ * for Akeeba Subscriptions 2.0 by Nicholas K. Dionysopoulos
+ */
+
+defined('_JEXEC') or die();
+
+include_once JPATH_ADMINISTRATOR.'/components/com_akeebasubs/version.php';
+require_once JPATH_ADMINISTRATOR.'/components/com_akeebasubs/fof/include.php';
+require_once JPATH_ADMINISTRATOR.'/components/com_akeebasubs/helpers/format.php';
+require_once JPATH_ADMINISTRATOR.'/components/com_akeebasubs/helpers/cparams.php';
+
+$jlang =& JFactory::getLanguage();
+$jlang->load('com_akeebasubs', JPATH_ADMINISTRATOR, 'en-GB', true);
+$jlang->load('com_akeebasubs', JPATH_ADMINISTRATOR, null, true);
+
+$items = FOFModel::getTmpInstance('Subscriptions','AkeebasubsModel')
+	->filter_order('akeebasubs_subscription_id')
+	->filter_order_Dir('desc')
+	->limit(10)
+	->getList();
+
+require_once JModuleHelper::getLayoutPath($module->module);
---
+++ modules/admin/akeebasubs/tmpl/default.php
@@ -0,0 +1,86 @@
+<? 
+/**
+ * @package		mod_akeebasubs
+ * @copyright 	Copyright (c) 2011 Sander Potjer
+ * @license 	GNU General Public License version 3 or later
+ */
+ 
+defined('_JEXEC') or die();
+
+FOFTemplateUtils::addCSS('media://com_akeebasubs/css/backend.css?'.AKEEBASUBS_VERSIONHASH);
+?>
+
+<table class="adminlist">
+	<thead>
+		<tr>
+			<th>
+				<?php echo JText::_( 'COM_AKEEBASUBS_COMMON_ID' ); ?>
+			</th>
+			<th>
+				<?php echo JText::_( 'COM_AKEEBASUBS_SUBSCRIPTIONS_LEVEL' ); ?>
+			</th>
+			<th>
+				<?php echo JText::_( 'COM_AKEEBASUBS_SUBSCRIPTIONS_USER' ); ?>
+			</th>
+			<th>
+				<?php echo JText::_( 'COM_AKEEBASUBS_SUBSCRIPTION_NET_AMOUNT' ); ?>
+			</th>
+			<th>
+				<?php echo JText::_( 'COM_AKEEBASUBS_SUBSCRIPTION_CREATED_ON' ); ?>
+			</th>
+        </tr>
+	</thead>
+	<tbody>
+		<?php if(count($items)): ?>
+		<?php $m = 1; $i = 0; ?>
+		<?php foreach ($items as $subscription) : ?>
+		<?php
+			$m = 1 - $m;
+			$rowClass = ($subscription->enabled) ? '' : 'expired';
+				
+			$users = FOFModel::getTmpInstance('Users','AkeebasubsModel')
+				->user_id($subscription->user_id)
+				->getList();
+			if(empty($users)) {
+				$user_id = 0;
+			} else {
+				foreach($users as $user) {
+					$user_id = $user->akeebasubs_user_id;
+					break;
+				}
+			}
+		?>
+        <tr class="row<?php echo $m?> <?php echo $rowClass?>">
+	        <td>
+	      		<a href="index.php?option=com_akeebasubs&view=subscription&id=<?php $subscription->akeebasubs_subscription_id ?>">
+					<?php echo sprintf('%05u', (int)$subscription->akeebasubs_subscription_id)?>
+				</a>
+	        </td>
+	        <td>
+	          	<a href="index.php?option=com_akeebasubs&view=level&id=<?php echo $subscription->akeebasubs_level_id ?>">
+					<?php echo htmlspecialchars($subscription->title, ENT_COMPAT, 'UTF-8')?>
+				</a>
+	        </td>
+	        <td>
+				<a href="<?php echo JRoute::_('index.php?option=com_akeebasubs&view=user&id='.$user_id)?>" class="title">
+					<strong><?php echo htmlspecialchars($subscription->username, ENT_COMPAT, 'UTF-8')?></strong>
+					<br/><?php echo htmlspecialchars($subscription->name, ENT_COMPAT, 'UTF-8')?>
+				</a>
+	        </td>
+	        <td>
+				<?php echo sprintf('%2.2f', (float)$subscription->net_amount) ?> <?php echo AkeebasubsHelperCparams::getParam('currencysymbol','€')?>
+	        </td>
+	        <td>
+				<?php echo AkeebasubsHelperFormat::date($subscription->created_on, '%Y-%m-%d %H:%M'); ?>
+	        </td>
+		</tr>
+		<?php endforeach; ?>
+		<?php else: ?>
+		<tr>
+			<td colspan="5">
+				<?php echo JText::_('COM_AKEEBASUBS_COMMON_NORECORDS') ?>
+			</td>
+		</tr>
+		<?php endif; ?>
+	</tbody>
+</table>
