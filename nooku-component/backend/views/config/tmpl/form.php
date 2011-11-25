b33675369831721debc3f37f4cc8f75b0723d4da
Nicholas Dionysopoulos <nicholas@akeebabackup.com>
2011-11-25 20:15:59 +0200

    I no longer need the Nooku-based version of the component. Bye-bye!

--- nooku-component/backend/views/config/tmpl/form.php
+++
@@ -1,9 +0,0 @@
-<? defined('KOOWA') or die('Restricted access'); ?>
-
-<?= @helper('behavior.tooltip'); ?>
-<script src="media://lib_koowa/js/koowa.js?<?=AKEEBASUBS_VERSIONHASH?>" />
-<style src="media://com_akeebasubs/css/backend.css?<?=AKEEBASUBS_VERSIONHASH?>" />
-
-<form action="<?= @route() ?>" method="post" class="-koowa-form">
-	<?=$formhtml?>
-</form>
--- nooku-component/frontend/views/message/tmpl/awesome.php
+++
@@ -1,5 +0,0 @@
-<?php defined('KOOWA') or die(); ?>
-<!--  --
-<style src="media://com_akeebasubs/css/frontend.css?<?=AKEEBASUBS_VERSIONHASH?>" />
-<!--  -->
-<?= @template('com://site/akeebasubs.view.message.order',array()) ?>
