0310dcedc0b8f0e97c87a6d950c277452f68d275
Nicholas Dionysopoulos <nicholas@akeebabackup.com>
2011-12-21 13:51:55 +0200

    + Allow third party software to fetch the subscriptions list remotely (sending the Super Administrator username/password in the request)

--- component/frontend/controllers/subscriptions.php
+++ component/frontend/controllers/subscriptions.php
@@ -27,8 +27,38 @@ class AkeebasubsControllerSubscriptions extends FOFController
 	
 	public function onBeforeBrowse()
 	{
-		// Do we have a user?
-		if(JFactory::getUser()->guest) {
+		// If we have a username/password pair, log in the user if he's a guest
+		$username = FOFInput::getString('username','',$this->input);
+		$password = FOFInput::getString('password','',$this->input);
+		$user = JFactory::getUser();
+		
+		if($user->guest && !empty($username) && !empty($password)) {
+			jimport( 'joomla.user.authentication');
+			$credentials = array(
+				'username'	=> $username,
+				'password'	=> $password
+			);
+			$app = JFactory::getApplication();
+			$options = array('remember' => false);
+			$authenticate = JAuthentication::getInstance();
+			$response	  = $authenticate->authenticate($credentials, $options);
+			if ($response->status == JAUTHENTICATE_STATUS_SUCCESS) {
+				JPluginHelper::importPlugin('user');
+				$results = $app->triggerEvent('onLoginUser', array((array)$response, $options));
+				if(version_compare(JVERSION,'1.6.0','ge')) {
+					jimport('joomla.user.helper');
+					$userid = JUserHelper::getUserId($response->username);
+					$user = &JFactory::getUser($userid);
+				} else {
+					$user = JFactory::getUser();
+				}
+				$parameters['username']	= $user->get('username');
+				$parameters['id']		= $user->get('id');
+			}
+		}
+		
+		// If we still have a guest user, show the login page
+		if($user->guest) {
 			// Show login page
 			$juri = JURI::getInstance();
 			$myURI = base64_encode($juri->toString());
@@ -37,13 +67,20 @@ class AkeebasubsControllerSubscriptions extends FOFController
 			return false;
 		}
 		
-		if(FOFInput::getInt('allUsers',0,$this->input) && (JFactory::getUser()->gid >= 23)) {
+		// Does the user have core.manage access or belongs to SA group?
+		if(version_compare(JVERSION, '1.6.0', 'ge')) {
+			$isAdmin = $user->authorise('core.manage','com_akeebasubs');
+		} else {
+			$isAdmin = $user->gid >= 23;
+		}
+		
+		if(FOFInput::getInt('allUsers',0,$this->input) && $isAdmin) {
 			$this->getThisModel()->user_id(null);
 		} else {
 			$this->getThisModel()->user_id(JFactory::getUser()->id);
 		}
 
-		if(FOFInput::getInt('allStates',0,$this->input) && (JFactory::getUser()->gid >= 23)) {
+		if(FOFInput::getInt('allStates',0,$this->input) && $isAdmin) {
 			$this->getThisModel()->paystate(null);
 		} else {
 			$this->getThisModel()->paystate('C,P');
