42b06725f5880bbf17ba99eca2f4f02b0f549177
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2012-01-10 17:58:05 +0200

    Updating internal link script

--- link.sh
+++ link.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 #
+# Internal linking with the language files
+#
+# -- Component
+rm component/language/backend/en-GB
+ln -s `pwd`/translations/component/backend/en-GB component/language/backend/en-GB
+rm component/language/frontend/en-GB
+ln -s `pwd`/translations/component/frontend/en-GB component/language/frontend/en-GB
+# -- Everything else
+php ./langlink.php
+
+#
 # Link with Live Update
 #
 rm component/backend/liveupdate/LICENSE.txt
@@ -9,16 +20,27 @@ rm component/backend/liveupdate/assets
 ln -s `pwd`/../liveupdate/code/assets component/backend/liveupdate/assets
 rm component/backend/liveupdate/classes
 ln -s `pwd`/../liveupdate/code/classes component/backend/liveupdate/classes
-rm component/backend/liveupdate/language
-ln -s `pwd`/../liveupdate/code/language component/backend/liveupdate/language
 rm component/backend/liveupdate/liveupdate.php
 ln `pwd`/../liveupdate/code/liveupdate.php component/backend/liveupdate/liveupdate.php
+rm component/backend/liveupdate/language/en-GB
+ln -s `pwd`/../liveupdate/code/language/en-GB component/backend/liveupdate/language/en-GB
 
 #
 # Link with the OTP plugin
 #
-rm plugins/system/oneclickaction
-ln -s `pwd`/../liveupdate/plugins/system/oneclickaction plugins/system/oneclickaction
+# -- Files
+rm plugins/system/oneclickaction/LICENSE.txt
+ln -s `pwd`/../liveupdate/plugins/system/oneclickaction/LICENSE.txt plugins/system/oneclickaction/LICENSE.txt
+rm plugins/system/oneclickaction/oneclickaction.php
+ln -s `pwd`/../liveupdate/plugins/system/oneclickaction/oneclickaction.php plugins/system/oneclickaction/oneclickaction.php
+rm plugins/system/oneclickaction/oneclickaction.xml
+ln -s `pwd`/../liveupdate/plugins/system/oneclickaction/oneclickaction.xml plugins/system/oneclickaction/oneclickaction.xml
+# -- Link external translations to the language directory
+rm translations/plugins/system/oneclickaction
+ln -s `pwd`/../liveupdate/plugins/system/oneclickaction/language translations/plugins/system/oneclickaction
+# -- Link from the language directory to the plugin directory
+rm plugins/system/oneclickaction/language/en-GB
+ln -s `pwd`/translations/plugins/system/oneclickaction/en-GB plugins/system/oneclickaction/language/en-GB
 
 #
 # Link with Framework-on-Framework
