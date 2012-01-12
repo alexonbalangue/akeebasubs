b42cc16317775e98a027a6aef62b6d99f61cb04f
Nicholas Dionysopoulos <nicholas@akeebabackup.com>
2012-01-12 11:50:35 +0200

    + Phoca Downloads plugin (thank you, Roland!)

---
+++ plugins/akeebasubs/phocadownload/phocadownload.php
@@ -0,0 +1,274 @@
+<?php
+/**
+ * @package		akeebasubs
+ * @copyright	Copyright (c) 2012 Roland Dalmulder / csvimproved.com
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html> or later
+ */
+
+defined('_JEXEC') or die();
+
+class plgAkeebasubsPhocadownload extends JPlugin
+{
+	/** @var array Levels to Groups to Add mapping */
+	private $addGroups = array();
+	
+	/** @var array Levels to Groups to Remove mapping */
+	private $removeGroups = array();
+
+	public function __construct(& $subject, $config = array())
+	{
+		if(!version_compare(JVERSION, '1.6.0', 'ge')) {
+			if(!is_object($config['params'])) {
+				$config['params'] = new JParameter($config['params']);
+			}
+		}
+		parent::__construct($subject, $config);
+
+		// Load level to group mapping from plugin parameters		
+		$strAddGroups = $this->params->get('addgroups','');
+		$this->addGroups = $this->parseGroups($strAddGroups);
+		
+		$strRemoveGroups = $this->params->get('removegroups','');
+		$this->removeGroups = $this->parseGroups($strRemoveGroups);
+	}
+	
+	/**
+	 * Called whenever a subscription is modified. Namely, when its enabled status,
+	 * payment status or valid from/to dates are changed.
+	 */
+	public function onAKSubscriptionChange($row, $info)
+	{
+		if(is_null($info['modified']) || empty($info['modified'])) return;
+		if(array_key_exists('enabled', (array)$info['modified'])) {
+			$this->onAKUserRefresh($row->user_id);
+		}
+	}
+	
+	/**
+	 * Called whenever the administrator asks to refresh integration status.
+	 * 
+	 * @param $user_id int The Joomla! user ID to refresh information for.
+	 */
+	public function onAKUserRefresh($user_id)
+	{
+		// Make sure we're configured
+		if(empty($this->addGroups) && empty($this->removeGroups)) return;
+	
+		// Get all of the user's subscriptions
+		$subscriptions = FOFModel::getTmpInstance('Subscriptions','AkeebasubsModel')
+			->user_id($user_id)
+			->getList();
+			
+		// Make sure there are subscriptions set for the user
+		if(!count($subscriptions)) return;
+		
+		// Get the initial list of groups to add/remove from
+		$addGroups = array();
+		$removeGroups = array();
+		foreach($subscriptions as $sub) {
+			$level = $sub->akeebasubs_level_id;
+			if($sub->enabled) {
+				// Enabled subscription, add groups
+				if(empty($this->addGroups)) continue;
+				if(!array_key_exists($level, $this->addGroups)) continue;
+				$groups = $this->addGroups[$level];
+				foreach($groups as $group) {
+					if(!in_array($group, $addGroups) && ($group > 0)) {
+						$addGroups[] = $group;
+					}
+				}
+			} else {
+				// Disabled subscription, remove groups
+				if(empty($this->removeGroups)) continue;
+				if(!array_key_exists($level, $this->removeGroups)) continue;
+				$groups = $this->removeGroups[$level];
+				
+				foreach($groups as $group) {
+					if(!in_array($group, $removeGroups) && ($group > 0)) {
+						$removeGroups[] = $group;
+					}
+				}
+			}
+		}
+		
+		// If no groups are detected, do nothing
+		if(empty($addGroups) && empty($removeGroups)) return;
+		
+		// Sort the lists
+		asort($addGroups);
+		asort($removeGroups);
+		
+		// Clean up the remove groups: if we are asked to both add and remove a user
+		// from a group, add wins.
+		if(!empty($removeGroups) && !empty($addGroups)) {
+			$temp = $removeGroups;
+			$removeGroups = array();
+			foreach($temp as $group) {
+				if(!in_array($group, $addGroups)) {
+					$removeGroups[] = $group;
+				}
+			}
+		}
+		
+		// Get DB connection
+		$db = JFactory::getDBO();
+		
+		// For each of the add groups, load them and make sure the user is added to them
+		foreach($addGroups as $gid) {
+			$db->setQuery('SELECT * FROM `#__phocadownload_categories` WHERE `id` = '.$db->Quote($gid));
+			$groupData = $db->loadObject();
+			if(empty($groupData)) continue;
+			$mustAdd = false;
+			$userList = array();
+			if(empty($groupData->accessuserid)) {
+				$mustAdd = true;
+			} else {
+				$userList = explode(',',$groupData->accessuserid);
+				$mustAdd = !in_array($user_id, $userList);
+			}
+			if($mustAdd) {
+				$userList[] = $user_id;
+				$users = implode(',', $userList);
+				$sql = 'UPDATE `#__phocadownload_categories` SET `accessuserid` = '.$db->Quote($users).' WHERE `id` = '.$db->Quote($gid);
+				$db->setQuery($sql);
+				$db->query();
+			}
+		}
+
+		// For each of the remove groups, load them and make sure the user is not in them
+		foreach($removeGroups as $gid) {
+			$db->setQuery('SELECT * FROM `#__phocadownload_categories` WHERE `id` = '.$db->Quote($gid));
+			$groupData = $db->loadObject();
+			if(empty($groupData)) continue;
+			$mustRemove = false;
+			$userList = array();
+			if(empty($groupData->accessuserid)) {
+				$mustRemove = false;
+			} else {
+				$userList = explode(',',$groupData->accessuserid);
+				$mustRemove = in_array($user_id, $userList);
+			}
+			if($mustRemove) {
+				$key = array_search($user_id, $userList);
+				if($key !== false) {
+					unset($userList[$key]);
+				}
+				$users = empty($userList) ? '' : implode(',', $userList);
+				$sql = 'UPDATE `#__phocadownload_categories` SET `accessuserid` = '.$db->Quote($users).' WHERE `id` = '.$db->Quote($gid);
+				$db->setQuery($sql);
+				$db->query();
+			}
+		}
+		
+	}
+	
+	/**
+	 * Converts an Akeeba Subscriptions level to a numeric ID
+	 * 
+	 * @param $title string The level's name to be converted to an ID
+	 *
+	 * @return int The subscription level's ID or -1 if no match is found
+	 */
+	private function ASLevelToId($title)
+	{
+		static $levels = null;
+		
+		// Don't process invalid titles
+		if(empty($title)) return -1;
+		
+		// Fetch a list of subscription levels if we haven't done so already
+		if(is_null($levels)) {
+			$levels = array();
+			$list = FOFModel::getTmpInstance('Levels','AkeebasubsModel')
+				->getList();
+			if(count($list)) foreach($list as $level) {
+				$thisTitle = strtoupper($level->title);
+				$levels[$thisTitle] = $level->akeebasubs_level_id;
+			}
+		}
+		
+		$title = strtoupper($title);
+		if(array_key_exists($title, $levels)) {
+			// Mapping found
+			return($levels[$title]);
+		} elseif( (int)$title == $title ) {
+			// Numeric ID passed
+			return (int)$title;
+		} else {
+			// No match!
+			return -1;
+		}
+	}
+	
+	private function PhocadownloadGroupToId($title)
+	{
+		static $groups = null;
+		
+		if(empty($title)) return -1;
+		
+		if(is_null($groups)) {
+			$groups = array();
+			
+			$db = JFactory::getDBO();
+			$sql = 'SELECT `title`, `id` FROM `#__phocadownload_categories`';
+			$db->setQuery($sql);
+			$res = $db->loadObjectList();
+			
+			if(!empty($res)) {
+				foreach($res as $item) {
+					$t = strtoupper(trim($item->title));
+					$groups[$t] = $item->id;
+				}
+			}
+		}
+
+		$title = strtoupper(trim($title));
+		if(array_key_exists($title, $groups)) {
+			// Mapping found
+			return($groups[$title]);
+		} elseif( (int)$title == $title ) {
+			// Numeric ID passed
+			return (int)$title;
+		} else {
+			// No match!
+			return -1;
+		}
+	}
+
+	private function parseGroups($rawData)
+	{
+		if(empty($rawData)) return array();
+		
+		$ret = array();
+		
+		// Just in case something funky happened...
+		$rawData = str_replace("\\n", "\n", $rawData);
+		$rawData = str_replace("\r", "\n", $rawData);
+		$rawData = str_replace("\n\n", "\n", $rawData);
+		
+		$lines = explode("\n", $rawData);
+		
+		foreach($lines as $line) {
+			$line = trim($line);
+			$parts = explode('=', $line, 2);
+			if(count($parts) != 2) continue;
+			
+			$level = $parts[0];
+			$rawGroups = $parts[1];
+			
+			$groups = explode(',', $rawGroups);
+			if(empty($groups)) continue;
+			if(!is_array($groups)) $groups = array($groups);
+			
+			$levelId = $this->ASLevelToId($level);
+			$groupIds = array();
+			foreach($groups as $groupTitle) {
+				$groupIds[] = $this->PhocadownloadGroupToId($groupTitle);
+			}
+			
+			$ret[$levelId] = $groupIds;
+		}
+		
+		return $ret;
+	}
+}
