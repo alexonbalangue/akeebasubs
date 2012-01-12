ddaabc1abd5b80deda16ab095f86a8fda0fdb217
Nicholas Dionysopoulos <nicholas@akeebabackup.com>
2012-01-12 19:08:36 +0200

    + The aslink plugin now adds the correct Itemid (thank you Daniel!)

--- plugins/content/aslink/aslink.php
+++ plugins/content/aslink/aslink.php
@@ -14,110 +14,142 @@ if(!defined('FOF_INCLUDED')) return;
 
 class plgContentAslink extends JPlugin
 {
-	/**
-	 * Gets the level ID out of a level title. If an ID was passed, it simply returns the ID.
-	 * If a non-existent subscription level is passed, it returns -1.
-	 *
-	 * @param $title string|int The subscription level title or ID
-	 *
-	 * @return int The subscription level ID
-	 */
-	private static function getId($title, $slug = false)
-	{
-		static $levels = null;
-		static $slugs = null;
-		static $upperSlugs = null;
-		
-		// Don't process invalid titles
+    /**
+     * Gets the level ID out of a level title. If an ID was passed, it simply returns the ID.
+     * If a non-existent subscription level is passed, it returns -1.
+     *
+     * @param $title string|int The subscription level title or ID
+     *
+     * @return int The subscription level ID
+     */
+    private static function getId($title, $slug = false)
+    {
+        static $levels = null;
+        static $slugs = null;
+        static $upperSlugs = null;
+
+        // Don't process invalid titles
 		if(empty($title)) return -1;
-		
-		// Fetch a list of subscription levels if we haven't done so already
+
+        // Fetch a list of subscription levels if we haven't done so already
 		if(is_null($levels)) {
-			$levels = array();
-			$slugs = array();
-			$upperSlugs = array();
+            $levels = array();
+            $slugs = array();
+            $upperSlugs = array();
 			$list = FOFModel::getTmpInstance('Levels','AkeebasubsModel')
-				->getList();
+                ->getList();
 			if(count($list)) foreach($list as $level) {
-				$thisTitle = strtoupper($level->title);
-				$levels[$thisTitle] = $level->akeebasubs_level_id;
-				$slugs[$thisTitle] = $level->slug;
-				$upperSlugs[strtoupper($level->slug)] = $level->slug;
-			}
-		}
-		
-		$title = strtoupper($title);
+                $thisTitle = strtoupper($level->title);
+                $levels[$thisTitle] = $level->akeebasubs_level_id;
+                $slugs[$thisTitle] = $level->slug;
+                $upperSlugs[strtoupper($level->slug)] = $level->slug;
+            }
+        }
+
+        $title = strtoupper($title);
 		if(array_key_exists($title, $levels)) {
-			// Mapping found
-			return $slug ? $slugs[$title] : $levels[$title];
+            // Mapping found
+            return $slug ? $slugs[$title] : $levels[$title];
 		} elseif(array_key_exists($title, $upperSlugs)) {
-			$mySlug = $upperSlugs[$title];
+            $mySlug = $upperSlugs[$title];
 			if($slug) {
-				return $mySlug;
-			} else {
+                return $mySlug;
+            } else {
 				foreach($slugs as $t => $s) {
 					if($s = $mySlug) {
-						return $levels[$t];
-					}
-				}
-				return -1;
-			}
+                        return $levels[$t];
+                    }
+                }
+                return -1;
+            }
 		} elseif( (int)$title == $title ) {
-			$id = (int)$title;
-			$title = '';
-			// Find the title from the ID
+            $id = (int)$title;
+            $title = '';
+            // Find the title from the ID
 			foreach($levels as $t => $lid) {
 				if($lid == $id) {
-					$title = $t;
-					break;
-				}
-			}
-			
+                    $title = $t;
+                    break;
+                }
+            }
+
 			if(empty($title)) {
-				return $slug ? '' : -1;
-			} else {
-				return $slug ? $slugs[$title] : $levels[$title];
-			}
-		} else {
-			// No match!
-			return $slug ? '' : -1;
-		}
-	}
-	
-	private static function process($match)
-	{
-		$ret = '';
-		
-		$slug = self::getId($match[1], true);
+                return $slug ? '' : -1;
+            } else {
+                return $slug ? $slugs[$title] : $levels[$title];
+            }
+        } else {
+            // No match!
+            return $slug ? '' : -1;
+        }
+    }
+
+    private static function process($match)
+    {
+        $ret = '';
+
+        $slug = self::getId($match[1], true);
 		if(!empty($slug)) {
 			$root = rtrim(JURI::base(),'/');
-			$subpath = JURI::base(true);
+            $subpath = JURI::base(true);
 			if(!empty($subpath) && ($subpath != '/')) {
-				$root = substr($root, 0, -1 * strlen($subpath));
-			}
-			//@ob_clean(); header('Content-type: text/plain'); var_dump(JURI::base(),JURI::base(true));die();
-			$ret = rtrim($root,'/').JRoute::_('index.php?option=com_akeebasubs&view=level&layout=default&slug='.$slug);
-		}
-		
-		return $ret;
-	}
-	
+                $root = substr($root, 0, -1 * strlen($subpath));
+            }
+
+            $itemId = self::_findItem($slug);
+            if($itemId) {
+                $itemId = '&Itemid=' . $itemId;
+            }
+
+            $ret = rtrim($root, '/') . JRoute::_('index.php?option=com_akeebasubs&view=level&layout=default&slug=' . $slug . $itemId);
+        }
+
+        return $ret;
+    }
+
 	public function onPrepareContent( &$article, &$params, $limitstart = 0 )
-	{
-		// Check whether the plugin should process or not
-		if ( JString::strpos( $article->text, 'aslink' ) === false )
-		{
-			return true;
-		}
-		
-		// Search for this tag in the content
-		$regex = "#{aslink (.*?)}#s";
-		
-		$article->text = preg_replace_callback( $regex, array('self', 'process'), $article->text );
-	}
-	
-	public function onContentPrepare($context, &$article, &$params, $limitstart = 0)
-	{
-		return $this->onPrepareContent($article, $params, $limitstart);
-	}
+    {
+        // Check whether the plugin should process or not
+        if (JString::strpos($article->text, 'aslink') === false) {
+            return true;
+        }
+
+        // Search for this tag in the content
+        $regex = "#{aslink (.*?)}#s";
+
+        $article->text = preg_replace_callback($regex, array('self', 'process'), $article->text);
+    }
+
+    public function onContentPrepare($context, &$article, &$params, $limitstart = 0)
+    {
+        return $this->onPrepareContent($article, $params, $limitstart);
+    }
+
+
+    /**
+     * Finds out the Itemid for the subscription
+     * @static
+     * @param $slug
+     * @return null
+     */
+    private static function _findItem($slug)
+    {
+
+        $component = & JComponentHelper::getComponent('com_akeebasubs');
+        $menus = & JApplication::getMenu('site', array());
+        $items = $menus->getItems('component_id', $component->id);
+        $itemId = null;
+
+        if (count($items)) {
+            foreach ($items as $item) {
+                if (@$item->query['view'] == 'level') {
+                    if ((@$item->params->get('slug') == $slug)) {
+                        $itemId = $item->id;
+                        break;
+                    }
+                }
+            }
+        }
+        return $itemId;
+    }
 }
