4d18f4d94ba0c31104aadbd6226c547c4ad3b09f
Daniel Dimitrov <danielsd_bg@yahoo.fr>
2012-01-13 11:53:56 +0100

    adding the option to create links to the levels view. Syntax: {aslink view=levels} - this needs to be added to the documentation :)

--- plugins/content/aslink/aslink.php
+++ plugins/content/aslink/aslink.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * @package		akeebasubs
- * @copyright	Copyright (c)2010-2012 Nicholas K. Dionysopoulos / AkeebaBackup.com
- * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html> or later
+ * @package        akeebasubs
+ * @copyright    Copyright (c)2010-2012 Nicholas K. Dionysopoulos / AkeebaBackup.com
+ * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html> or later
  */
 
 defined('_JEXEC') or die();
 
 jimport('joomla.plugin.plugin');
 
-include_once JPATH_ADMINISTRATOR.'/components/com_akeebasubs/fof/include.php';
-if(!defined('FOF_INCLUDED')) return;
+include_once JPATH_ADMINISTRATOR . '/components/com_akeebasubs/fof/include.php';
+if (!defined('FOF_INCLUDED')) return;
 
 class plgContentAslink extends JPlugin
 {
@@ -29,16 +29,16 @@ class plgContentAslink extends JPlugin
         static $upperSlugs = null;
 
         // Don't process invalid titles
-		if(empty($title)) return -1;
+        if (empty($title)) return -1;
 
         // Fetch a list of subscription levels if we haven't done so already
-		if(is_null($levels)) {
+        if (is_null($levels)) {
             $levels = array();
             $slugs = array();
             $upperSlugs = array();
-			$list = FOFModel::getTmpInstance('Levels','AkeebasubsModel')
+            $list = FOFModel::getTmpInstance('Levels', 'AkeebasubsModel')
                 ->getList();
-			if(count($list)) foreach($list as $level) {
+            if (count($list)) foreach ($list as $level) {
                 $thisTitle = strtoupper($level->title);
                 $levels[$thisTitle] = $level->akeebasubs_level_id;
                 $slugs[$thisTitle] = $level->slug;
@@ -47,33 +47,33 @@ class plgContentAslink extends JPlugin
         }
 
         $title = strtoupper($title);
-		if(array_key_exists($title, $levels)) {
+        if (array_key_exists($title, $levels)) {
             // Mapping found
             return $slug ? $slugs[$title] : $levels[$title];
-		} elseif(array_key_exists($title, $upperSlugs)) {
+        } elseif (array_key_exists($title, $upperSlugs)) {
             $mySlug = $upperSlugs[$title];
-			if($slug) {
+            if ($slug) {
                 return $mySlug;
             } else {
-				foreach($slugs as $t => $s) {
-					if($s = $mySlug) {
+                foreach ($slugs as $t => $s) {
+                    if ($s = $mySlug) {
                         return $levels[$t];
                     }
                 }
                 return -1;
             }
-		} elseif( (int)$title == $title ) {
+        } elseif ((int)$title == $title) {
             $id = (int)$title;
             $title = '';
             // Find the title from the ID
-			foreach($levels as $t => $lid) {
-				if($lid == $id) {
+            foreach ($levels as $t => $lid) {
+                if ($lid == $id) {
                     $title = $t;
                     break;
                 }
             }
 
-			if(empty($title)) {
+            if (empty($title)) {
                 return $slug ? '' : -1;
             } else {
                 return $slug ? $slugs[$title] : $levels[$title];
@@ -88,26 +88,44 @@ class plgContentAslink extends JPlugin
     {
         $ret = '';
 
-        $slug = self::getId($match[1], true);
-		if(!empty($slug)) {
-			$root = rtrim(JURI::base(),'/');
-            $subpath = JURI::base(true);
-			if(!empty($subpath) && ($subpath != '/')) {
-                $root = substr($root, 0, -1 * strlen($subpath));
-            }
+        if($match[1] != 'view=levels') {
+            $slug = self::getId($match[1], true);
+            if (!empty($slug)) {
+                $root = self::getRootUrl();
 
-            $itemId = self::_findItem($slug);
+                $itemId = self::_findItem($slug);
+                if($itemId) {
+                    $itemId = '&Itemid=' . $itemId;
+                }
+
+                //@ob_clean(); header('Content-type: text/plain'); var_dump(JURI::base(),JURI::base(true));die();
+                $ret = rtrim($root, '/') . JRoute::_('index.php?option=com_akeebasubs&view=level&slug=' . $slug . $itemId);
+            }
+        } else {
+            $root = self::getRootUrl();
+
+            $itemId = self::_findItem('levels');
             if($itemId) {
                 $itemId = '&Itemid=' . $itemId;
             }
 
-            $ret = rtrim($root, '/') . JRoute::_('index.php?option=com_akeebasubs&view=level&layout=default&slug=' . $slug . $itemId);
+            $ret = rtrim($root, '/') . JRoute::_('index.php?option=com_akeebasubs&view=levels&Itemid=' . $itemId);
         }
 
         return $ret;
     }
 
-	public function onPrepareContent( &$article, &$params, $limitstart = 0 )
+    private function getRootUrl() {
+        $root = rtrim(JURI::base(), '/');
+        $subpath = JURI::base(true);
+        if (!empty($subpath) && ($subpath != '/')) {
+            $root = substr($root, 0, -1 * strlen($subpath));
+        }
+
+        return $root;
+    }
+
+    public function onPrepareContent(&$article, &$params, $limitstart = 0)
     {
         // Check whether the plugin should process or not
         if (JString::strpos($article->text, 'aslink') === false) {
@@ -144,6 +162,13 @@ class plgContentAslink extends JPlugin
             foreach ($items as $item) {
                 if (@$item->query['view'] == 'level') {
                     if ((@$item->params->get('slug') == $slug)) {
+                        $itemId = $item->id;
+                        break;
+                    }
+                }
+
+                if (@$item->query['view'] == 'levels') {
+                    if($item->query['view'] == $slug) {
                         $itemId = $item->id;
                         break;
                     }
