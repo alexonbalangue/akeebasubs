e81bebeba573115d88ec691131b717ebc0072bfa
Nicholas Dionysopoulos <nicholas@akeebabackup.com>
2012-01-20 19:45:21 +0200

    Removing publish/unpublish buttons from Subscriptions page (they wouldn't work, anyway)

--- component/backend/toolbar.php
+++ component/backend/toolbar.php
@@ -12,8 +12,22 @@ class AkeebasubsToolbar extends FOFToolbar
 {
 	public function onSubscriptionsBrowse()
 	{
-		// Normal buttons
-		$this->onBrowse();
+		// Set toolbar title
+		$subtitle_key = FOFInput::getCmd('option','com_foobar',$this->input).'_TITLE_'.strtoupper(FOFInput::getCmd('view','cpanel',$this->input));
+		JToolBarHelper::title(JText::_( FOFInput::getCmd('option','com_foobar',$this->input)).' &ndash; <small>'.JText::_($subtitle_key).'</small>', str_replace('com_', '', FOFInput::getCmd('option','com_foobar',$this->input)));
+		
+		// Add toolbar buttons
+		if($this->perms->delete) {
+			JToolBarHelper::deleteList();
+		}
+		if($this->perms->edit) {
+			JToolBarHelper::editListX();
+		}
+		if($this->perms->create) {
+			JToolBarHelper::addNewX();
+		}
+		
+		$this->renderSubmenu();
 		
 		$bar = JToolBar::getInstance('toolbar');
 		
