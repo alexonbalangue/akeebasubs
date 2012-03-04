06b169140a35199af92d205c3cc7c3a0a397b3b3
Nicholas Dionysopoulos <nicholas@akeebabackup.com>
2012-03-04 12:11:20 +0200

    Re #59 The PagSeguro payment form would not auto-submit

--- plugins/akpayment/pagseguro/pagseguro/form.php
+++ plugins/akpayment/pagseguro/pagseguro/form.php
@@ -7,7 +7,7 @@ $t2 = JText::_('COM_AKEEBASUBS_LEVEL_REDIRECTING_BODY');
 <h3><?php echo JText::_('COM_AKEEBASUBS_LEVEL_REDIRECTING_HEADER') ?></h3>
 <p><?php echo JText::_('COM_AKEEBASUBS_LEVEL_REDIRECTING_BODY') ?></p>
 <p align="center">
-<form action="<?php echo htmlentities($data->url) ?>"  method="post" >
+<form action="<?php echo htmlentities($data->url) ?>"  method="post" id="paymentForm">
         <input type="image" src="https://p.simg.uol.com.br/out/pagseguro/i/botoes/pagamentos/184x42-pagar-cinza-assina.gif" border="0" name="submit" id="pagsegurosubmit" />
 </form>
 </p>
