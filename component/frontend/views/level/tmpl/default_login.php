2631805c3eec11a78f1a1c5dfdb471946bf86f24
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2012-04-02 20:16:36 +0300

    Wrong redirect in login when the site is in a subdirectory

--- component/frontend/views/level/tmpl/default_login.php
+++ component/frontend/views/level/tmpl/default_login.php
@@ -8,10 +8,19 @@
 defined('_JEXEC') or die();
 
 $login_url = version_compare(JVERSION, '1.6.0', 'ge') ? 'index.php?option=com_users&task=user.login' : 'index.php?option=com_user&task=login';
+
+$rootURL = rtrim(JURI::base(),'/');
+$subpathURL = JURI::base(true);
+if(!empty($subpathURL) && ($subpathURL != '/')) {
+	$rootURL = substr($rootURL, 0, -1 * strlen($subpathURL));
+}
+
+$redirectURL = $rootURL. str_replace('&amp;','&',
+	JRoute::_('index.php?option=com_akeebasubs&view=level&layout=default&slug='.FOFInput::getString('slug','',$this->input)))
 ?>
 
 <form action="<?php echo rtrim(JURI::base(),'/') ?>/<?php echo $login_url ?>" method="post">
-	<input type="hidden" name="return" value="<?php echo base64_encode(str_replace('&amp;','&',rtrim(JURI::base(),'/').JRoute::_('index.php?option=com_akeebasubs&view=level&layout=default&slug='.FOFInput::getString('slug','',$this->input))))?>" />
+	<input type="hidden" name="return" value="<?php echo base64_encode($redirectURL)?>" />
 	<input type="hidden" name="remember" value="1" />
 	<input type="hidden" name="<?php echo JUtility::getToken();?>" value="1" />
 	<fieldset>
