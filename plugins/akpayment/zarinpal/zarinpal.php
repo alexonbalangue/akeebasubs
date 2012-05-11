ab97b80564e99b61f5eba6099437e15538b58e8e
tobias.dev <tobias.satzger@gmail.com>
2012-05-11 20:47:11 +0200

    Re #95 Initial Implementation

---
+++ plugins/akpayment/zarinpal/zarinpal.php
@@ -0,0 +1,247 @@
+<?php
+/**
+ * @package		akeebasubs
+ * @copyright	Copyright (c)2010-2012 Nicholas K. Dionysopoulos / AkeebaBackup.com
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html> or later
+ */
+
+defined('_JEXEC') or die();
+
+jimport('joomla.plugin.plugin');
+
+class plgAkpaymentZarinPal extends JPlugin
+{
+	private $ppName = 'zarinpal';
+	private $ppKey = 'PLG_AKPAYMENT_ZARINPAL_TITLE';
+	
+	public function __construct(&$subject, $config = array())
+	{
+		if(!version_compare(JVERSION, '1.6.0', 'ge')) {
+			if(!is_object($config['params'])) {
+				$config['params'] = new JParameter($config['params']);
+			}
+		}
+		parent::__construct($subject, $config);
+		
+		require_once JPATH_ADMINISTRATOR.'/components/com_akeebasubs/helpers/cparams.php';
+		//require_once dirname(__FILE__).'/zarinpal/library/nusoap.php';
+		
+		// Load the language files
+		$jlang = JFactory::getLanguage();
+		$jlang->load('plg_akpayment_zarinpal', JPATH_ADMINISTRATOR, 'en-GB', true);
+		$jlang->load('plg_akpayment_zarinpal', JPATH_ADMINISTRATOR, $jlang->getDefault(), true);
+		$jlang->load('plg_akpayment_zarinpal', JPATH_ADMINISTRATOR, null, true);
+	}
+
+	public function onAKPaymentGetIdentity()
+	{
+		$title = $this->params->get('title','');
+		if(empty($title)) $title = JText::_($this->ppKey);
+		$ret = array(
+			'name'		=> $this->ppName,
+			'title'		=> $title
+		);
+		$ret['image'] = trim($this->params->get('ppimage',''));
+		if(empty($ret['image'])) {
+			$ret['image'] = rtrim(JURI::base(),'/').'/media/com_akeebasubs/images/frontend/zarinpal.png';
+		}
+		return (object)$ret;
+	}
+	
+	/**
+	 * Returns the payment form to be submitted by the user's browser. The form must have an ID of
+	 * "paymentForm" and a visible submit button.
+	 * 
+	 * @param string $paymentmethod
+	 * @param JUser $user
+	 * @param AkeebasubsTableLevel $level
+	 * @param AkeebasubsTableSubscription $subscription
+	 * @return string
+	 */
+	public function onAKPaymentNew($paymentmethod, $user, $level, $subscription)
+	{
+		if($paymentmethod != $this->ppName) return false;
+		
+		$data = (object)array(
+			'merchant_id'			=> trim($this->params->get('merchant_id','')),
+			'amount'				=> sprintf('%.2f',$subscription->gross_amount),
+			'description'			=> $level->title . ' - [ ' . $user->username . ' ]',
+			'callback_url'			=> JURI::base()
+										.'index.php?option=com_akeebasubs&view=callback&paymentmethod=zarinpal&sid='
+										.$subscription->akeebasubs_subscription_id
+		);
+		
+		$soap = new SoapClient('https://www.zarinpal.com/WebserviceGateway/wsdl', array('encoding'=>'UTF-8'));
+		$pRequest = $soap->PaymentRequest(
+				$data->merchant_id,
+				$data->amount,
+				$data->callback_url,
+				urlencode($data->description)
+				);
+		
+		$data->url = 'https://www.zarinpal.com/users/pay_invoice/'.$pRequest;
+
+		@ob_start();
+		include dirname(__FILE__).'/zarinpal/form.php';
+		$html = @ob_get_clean();
+		
+		return $html;
+	}
+	
+	public function onAKPaymentCallback($paymentmethod, $data)
+	{
+		jimport('joomla.utilities.date');
+		
+		// Check if we're supposed to handle this$
+		if($paymentmethod != $this->ppName) return false;
+		
+		// Check IPN data for validity (i.e. protect against fraud attempt)
+		$isValid = $this->isValidIPN($data);
+		if(!$isValid) $data['akeebasubs_failure_reason'] = 'Invalid response received.';
+
+		// Load the relevant subscription row
+		if($isValid) {
+			$id = $data['sid'];
+			$subscription = null;
+			if($id > 0) {
+				$subscription = FOFModel::getTmpInstance('Subscriptions','AkeebasubsModel')
+					->setId($id)
+					->getItem();
+				if( ($subscription->akeebasubs_subscription_id <= 0) || ($subscription->akeebasubs_subscription_id != $id) ) {
+					$subscription = null;
+					$isValid = false;
+				}
+			} else {
+				$isValid = false;
+			}
+		}
+        
+		// Check that refID has not been previously processed
+		if($isValid && !is_null($subscription)) {
+			if($subscription->processor_key == $data['refID']) {
+				$isValid = false;
+				$data['akeebasubs_failure_reason'] = "I will not process the same " . $data['refID'] . " twice";
+			}
+		}
+
+		// Check that amount is correct
+		$isPartialRefund = false;
+		if($isValid && !is_null($subscription)) {
+			$mc_gross = floatval($data['amount']);
+			$gross = $subscription->gross_amount;
+			if($mc_gross > 0) {
+				// A positive value means "payment". The prices MUST match!
+				// Important: NEVER, EVER compare two floating point values for equality.
+				$isValid = ($gross - $mc_gross) < 0.01;
+			} else {
+				$isPartialRefund = false;
+				$temp_mc_gross = -1 * $mc_gross;
+				$isPartialRefund = ($gross - $temp_mc_gross) > 0.01;
+			}
+			if(!$isValid) $data['akeebasubs_failure_reason'] = 'Paid amount does not match the subscription amount';
+		}
+			
+		// Log the IPN data
+		$this->logIPN($data, $isValid);
+
+		// Fraud attempt? Do nothing more!
+		if(!$isValid) return false;
+
+		// Payment status always complete if this point is reached
+		$newStatus = 'C';
+
+		// Update subscription status (this also automatically calls the plugins)
+		$updates = array(
+				'akeebasubs_subscription_id'	=> $id,
+				'processor_key'					=> $data['refID'],
+				'state'							=> $newStatus,
+				'enabled'						=> 0
+		);
+		jimport('joomla.utilities.date');
+		if($newStatus == 'C') {
+			// Fix the starting date if the payment was accepted after the subscription's start date. This
+			// works around the case where someone pays by e-Check on January 1st and the check is cleared
+			// on January 5th. He'd lose those 4 days without this trick. Or, worse, if it was a one-day pass
+			// the user would have paid us and we'd never given him a subscription!
+			$jNow = new JDate();
+			$jStart = new JDate($subscription->publish_up);
+			$jEnd = new JDate($subscription->publish_down);
+			$now = $jNow->toUnix();
+			$start = $jStart->toUnix();
+			$end = $jEnd->toUnix();
+
+			if($start < $now) {
+				$duration = $end - $start;
+				$start = $now;
+				$end = $start + $duration;
+				$jStart = new JDate($start);
+				$jEnd = new JDate($end);
+			}
+
+			$updates['publish_up'] = $jStart->toMySQL();
+			$updates['publish_down'] = $jEnd->toMySQL();
+			$updates['enabled'] = 1;
+
+		}
+		$subscription->save($updates);
+
+		// Run the onAKAfterPaymentCallback events
+		jimport('joomla.plugin.helper');
+		JPluginHelper::importPlugin('akeebasubs');
+		$app = JFactory::getApplication();
+		$jResponse = $app->triggerEvent('onAKAfterPaymentCallback',array(
+			$subscription
+		));
+   
+		return true;
+	}
+	
+	private function isValidIPN($data)
+	{
+		if(! empty($data['au'])) {
+			$soap = new SoapClient('https://www.zarinpal.com/WebserviceGateway/wsdl', array('encoding'=>'UTF-8'));
+			$pVerification = $soap->PaymentVerification(
+					trim($this->params->get('merchant_id','')),
+					$data['au'],
+					$data['amount']);
+			
+			if($pVerification == 1) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private function logIPN($data, $isValid)
+	{
+		$config = JFactory::getConfig();
+		$logpath = $config->getValue('log_path');
+		$logFile = $logpath.'/akpayment_zarinpal_ipn.php';
+		jimport('joomla.filesystem.file');
+		if(!JFile::exists($logFile)) {
+			$dummy = "<?php die(); ?>\n";
+			JFile::write($logFile, $dummy);
+		} else {
+			if(@filesize($logFile) > 1048756) {
+				$altLog = $logpath.'/akpayment_zarinpal_ipn-1.php';
+				if(JFile::exists($altLog)) {
+					JFile::delete($altLog);
+				}
+				JFile::copy($logFile, $altLog);
+				JFile::delete($logFile);
+				$dummy = "<?php die(); ?>\n";
+				JFile::write($logFile, $dummy);
+			}
+		}
+		$logData = JFile::read($logFile);
+		if($logData === false) $logData = '';
+		$logData .= "\n" . str_repeat('-', 80);
+		$logData .= $isValid ? 'VALID ZARINPAL IPN' : 'INVALID ZARINPAL IPN *** FRAUD ATTEMPT OR INVALID NOTIFICATION ***';
+		$logData .= "\nDate/time : ".gmdate('Y-m-d H:i:s')." GMT\n\n";
+		foreach($data as $key => $value) {
+			$logData .= '  ' . str_pad($key, 30, ' ') . $value . "\n";
+		}
+		$logData .= "\n";
+		JFile::write($logFile, $logData);
+	}
+}
