00e6c168d2e4d20f4a6bc1aed56f31aaca3caa6b
Nicholas Dionysopoulos <nicholas@akeebabackup.com>
2012-06-14 16:14:19 +0300

    Fixed #100 Language merge tags for the Off-Line payment plugin

--- plugins/akpayment/offline/offline.php
+++ plugins/akpayment/offline/offline.php
@@ -96,6 +96,12 @@ ENDTEMPLATE;
 		$html = str_replace('{SUBSCRIPTION}', sprintf('%06u', $subscription->akeebasubs_subscription_id), $html);
 		$html = str_replace('{FIRSTNAME}', $firstName, $html);
 		$html = str_replace('{LASTNAME}', $lastName, $html);
+		
+		@include_once JPATH_SITE.'/components/com_akeebasubs/helpers/message.php';
+		if(class_exists('AkeebasubsHelperMessage')) {
+			$html = AkeebasubsHelperMessage::processLanguage($html);
+		}
+		
 		$html = '<div>'.$html.'</div>';
 
 		return $html;
