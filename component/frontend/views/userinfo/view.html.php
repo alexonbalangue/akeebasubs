c02f13ac9c54a328f5a97c602fb85e6afdf63189
Nicholas Dionysopoulos <nicholas@akeebabackup.com>
2012-06-25 13:40:17 +0300

    # [LOW] Notices thrown in userinfo frontend view

---
+++ component/frontend/views/userinfo/view.html.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package AkeebaSubs
+ * @copyright Copyright (c)2010-2012 Nicholas K. Dionysopoulos
+ * @license GNU General Public License version 3, or later
+ */
+
+// Protect from unauthorized access
+defined('_JEXEC') or die();
+
+class AkeebasubsViewUserinfo extends FOFViewHtml
+{
+	protected function onBrowse($tpl = null)
+	{
+		return true;
+	}
+}
