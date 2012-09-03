d041e7e7a66752f7861cfc146bc770020783cc98
Nicholas Dionysopoulos <nicholas@akeebabackup.com>
2012-09-03 14:01:56 +0300

    Translation conditioning script

--- fixtranslations.php
+++ fixtranslations.php
@@ -1,17 +1,95 @@
 <?php
 
-/**
- * Fixes the translation INI files to be compatible with Joomla! 1.5 and 1.6
- */
+function recursiveApply($path)
+{
+	$dh = opendir($path);
+	while($entry = readdir($dh)) {
+		if($entry == '.') continue;
+		if($entry == '..') continue;
+		if(substr($entry, 0, 1) == '.') continue;
+		if(substr($entry, 0, 1) == '_') continue;
+		
+		$myDir = $path.DIRECTORY_SEPARATOR.$entry;
+		
+		if(is_link($myDir)) continue;
+		if(is_dir($myDir)) {
+			recursiveApply($myDir);
+		}
+		if(is_file($myDir)) {
+			if(substr($myDir, -4) == '.ini') {
+				fixTranslation($myDir);
+			}
+		}
+	}
+	closedir($dh);
+}
 
-$translationPaths = array(
-	'installers/abi/installation/lang'
-);
+function fixTranslation($filename)
+{
+	static $count = 0;
+	
+	$count++;
+	
+	echo sprintf('%05u', $count) . "\t$filename\n";
+	echo str_repeat('-', 79)."\n";
+	
+	$fp = fopen($filename, 'rt');
+	if($fp == false) {
+		echo "\tCOULD NOT OPEN FILE!\n";
+		return;
+	}
+	$out = '';
+	echo "\tReading file\n";
+	
+	$inEmptyLine = false;
+	while(!feof($fp)) {
+		$line = fgets($fp);
+		$trimmed = trim($line);
+
+		// Transform comments
+		if(substr($trimmed,0,1) == '#') {
+			$out .= ';'.substr($trimmed,1)."\n";
+			continue;
+		}
+
+		if(substr($trimmed,0,1) == ';') {
+			$out .= "$trimmed\n";
+			continue;
+		}
+
+		// Detect blank lines
+		if(empty($trimmed)) {
+			if($inEmptyLine) continue;
+			$inEmptyLine = true;
+			$out .= "\n";
+			continue;
+		}
+		
+		$inEmptyLine = false;
+
+		// Process key-value pairs
+		list($key, $value) = explode('=', $trimmed, 2);
+		$value = trim($value, '"');
+		$value = str_replace('"_QQ_"', '"', $value);
+		$value = str_replace('\\"', "'", $value);
+		$value = str_replace('"', '"_QQ_"', $value);
+		$key = strtoupper($key);
+		$key = trim($key);
+		$out .= "$key=\"$value\"\n";
+	}
+	$out = rtrim($out, "\n")."\n";
+	fclose($fp);
+	
+	#echo $out."\n\n";
+	echo "\tWriting fixed file\n";
+	file_put_contents($filename, $out);
+}
+
 
 echo <<<ENDBANNER
 Akeeba Translation INI Fixer
 ===============================================================================
-Copyright (c)2011 Nicholas K. Dionysopoulos - All legal rights reserved
+Copyright (c)2012 Nicholas K. Dionysopoulos - All legal rights reserved
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -26,51 +104,9 @@ Copyright (c)2011 Nicholas K. Dionysopoulos - All legal rights reserved
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    
+
 ENDBANNER;
 
-foreach($translationPaths as $root)
-{
-	echo "Fixing translations in $root\n";
-	$files = glob($root.'/*.ini');
-	if(!empty($files)) foreach($files as $file) {
-		echo "\t$file\n";
-		$fp = fopen($file, 'rt');
-		if($fp == false) die('Could not open file.');
-		$out = '';
-		while(!feof($fp)) {
-			$line = fgets($fp);
-			$trimmed = trim($line);
-			
-			// Transform comments
-			if(substr($trimmed,0,1) == '#') {
-				$out .= ';'.substr($trimmed,1)."\n";
-				continue;
-			}
-			
-			if(substr($trimmed,0,1) == ';') {
-				$out .= "$trimmed\n";
-				continue;
-			}
-			
-			// Detect blank lines
-			if(empty($trimmed)) {
-				$out .= "\n";
-				continue;
-			}
-			
-			// Process key-value pairs
-			list($key, $value) = explode('=', $trimmed, 2);
-			$value = trim($value, '"');
-			$value = str_replace('\\"', "'", $value);
-			$value = str_replace('"_QQ_"', "'", $value);
-			$value = str_replace('"', "'", $value);
-			$key = strtoupper($key);
-			$key = trim($key);
-			$out .= "$key=\"$value\"\n";
-		}
-		fclose($fp);
-		
-		file_put_contents($file, $out);
-	}
-}
+
+$path = __DIR__.'/translations';
+recursiveApply($path);
