00e31af4682bb5ebc5e1c0985946fa9833995bd9
tobias.dev <tobias.satzger@gmail.com>
2012-09-09 01:23:26 +0200

    Re #108 Initial Implementation of Beanstream

---
+++ plugins/akpayment/beanstream/beanstream.php
@@ -0,0 +1,298 @@
+<?php
+/**
+ * @package		akeebasubs
+ * @copyright	Copyright (c)2010-2012 Nicholas K. Dionysopoulos / AkeebaBackup.com
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html> or later
+ */
+
+defined('_JEXEC') or die();
+
+jimport('joomla.plugin.plugin');
+
+class plgAkpaymentBeanstream extends JPlugin
+{
+	private $ppName = 'beanstream';
+	private $ppKey = 'PLG_AKPAYMENT_BEANSTREAM_TITLE';
+	
+	public function __construct(&$subject, $config = array())
+	{
+		if(!is_object($config['params'])) {
+			jimport('joomla.registry.registry');
+			$config['params'] = new JRegistry($config['params']);
+		}
+		parent::__construct($subject, $config);
+		
+		require_once JPATH_ADMINISTRATOR.'/components/com_akeebasubs/helpers/cparams.php';
+		
+		// Load the language files
+		$jlang = JFactory::getLanguage();
+		$jlang->load('plg_akpayment_beanstream', JPATH_ADMINISTRATOR, 'en-GB', true);
+		$jlang->load('plg_akpayment_beanstream', JPATH_ADMINISTRATOR, $jlang->getDefault(), true);
+		$jlang->load('plg_akpayment_beanstream', JPATH_ADMINISTRATOR, null, true);
+	}
+
+	public function onAKPaymentGetIdentity()
+	{
+		$title = $this->params->get('title','');
+		if(empty($title)) $title = JText::_($this->ppKey);
+		$ret = array(
+			'name'		=> $this->ppName,
+			'title'		=> $title
+		);
+		$ret['image'] = trim($this->params->get('ppimage',''));
+		if(empty($ret['image'])) {
+			$ret['image'] = rtrim(JURI::base(),'/').'/media/com_akeebasubs/images/frontend/beanstream-logo.jpg';
+		}
+		return (object)$ret;
+	}
+	
+	/**
+	 * Returns the payment form to be submitted by the user's browser. The form must have an ID of
+	 * "paymentForm" and a visible submit button.
+	 * 
+	 * @param string $paymentmethod
+	 * @param JUser $user
+	 * @param AkeebasubsTableLevel $level
+	 * @param AkeebasubsTableSubscription $subscription
+	 * @return string
+	 */
+	public function onAKPaymentNew($paymentmethod, $user, $level, $subscription)
+	{
+		if($paymentmethod != $this->ppName) return false;
+		
+		$slug = FOFModel::getTmpInstance('Levels','AkeebasubsModel')
+				->setId($subscription->akeebasubs_level_id)
+				->getItem()
+				->slug;
+		
+		$rootURL = rtrim(JURI::base(),'/');
+		$subpathURL = JURI::base(true);
+		if(!empty($subpathURL) && ($subpathURL != '/')) {
+			$rootURL = substr($rootURL, 0, -1 * strlen($subpathURL));
+		}
+		
+		$kuser = FOFModel::getTmpInstance('Users','AkeebasubsModel')
+			->user_id($user->id)
+			->getFirstItem();
+		
+		$callbackUrl = JURI::base().'index.php?option=com_akeebasubs&view=callback&paymentmethod=beanstream&marker=000';
+		
+		$data = (object)array(
+			'url'				=> 'https://www.beanstream.com/scripts/payment/payment.asp',
+			'merchant_id'		=> trim($this->params->get('merchant_id','')),
+			'trnOrderNumber'	=> $subscription->akeebasubs_subscription_id,
+			'trnAmount'			=> sprintf('%.2f',$subscription->gross_amount),
+			'errorPage'			=> trim($this->params->get('error_url','')),
+			'declinedPage'		=> $callbackUrl,
+			'approvedPage'		=> $callbackUrl,
+			'ordName'			=> trim($user->name),
+			'ordEmailAddress'	=> trim($user->email),
+			'ordAddress1'		=> trim($kuser->address1),
+			'ordAddress2'		=> trim($kuser->address2),
+			'ordCity'			=> trim($kuser->city),
+			'ordProvince'		=> trim($kuser->state),
+			'ordPostalCode'		=> trim($kuser->zip),
+			'ordCountry'		=> strtoupper(trim($kuser->country))
+		);
+		
+		// Language settings ("fre" or "eng")
+		try {
+			$lang = strtolower(substr(JFactory::getLanguage()->getTag(), 0, 2));
+			$data->trnLanguage = ($lang == 'fr') ? 'fre' : 'eng';
+		} catch(Exception $e) {
+			// Shouldn't happend. But setting the language is optional... so do nothing here.
+		}
+
+		@ob_start();
+		include dirname(__FILE__).'/beanstream/form.php';
+		$html = @ob_get_clean();
+		
+		return $html;
+	}
+	
+	public function onAKPaymentCallback($paymentmethod, $data)
+	{
+		jimport('joomla.utilities.date');
+		
+		// Check if we're supposed to handle this
+		if($paymentmethod != $this->ppName) return false;
+		
+		// Clean up second question mark in response
+		preg_match('/000\?trnApproved=(.+)/', $data['marker'], $matches);
+		$data['trnApproved'] = $matches[1];
+		
+		// Check IPN data for validity (i.e. protect against fraud attempt)
+		$isValid = $this->isValidIPN($data);
+		if(!$isValid) $data['akeebasubs_failure_reason'] = 'Invalid response received.';
+
+		// Load the relevant subscription row
+		if($isValid) {
+			$id = $data['trnOrderNumber'];
+			$subscription = null;
+			if($id > 0) {
+				$subscription = FOFModel::getTmpInstance('Subscriptions','AkeebasubsModel')
+					->setId($id)
+					->getItem();
+				if( ($subscription->akeebasubs_subscription_id <= 0) || ($subscription->akeebasubs_subscription_id != $id) ) {
+					$subscription = null;
+					$isValid = false;
+				}
+			} else {
+				$isValid = false;
+			}
+		}
+        
+		// Check that trnId has not been previously processed
+		if($isValid && !is_null($subscription)) {
+			if($subscription->processor_key == $data['trnId']) {
+				$isValid = false;
+				$data['akeebasubs_failure_reason'] = "I will not process the same transaction " . $data['trnId'] . " twice";
+			}
+		}
+
+		// Check that amount is correct
+		$isPartialRefund = false;
+		if($isValid && !is_null($subscription)) {
+			$mc_gross = floatval($data['trnAmount']);
+			$gross = $subscription->gross_amount;
+			if($mc_gross > 0) {
+				// A positive value means "payment". The prices MUST match!
+				// Important: NEVER, EVER compare two floating point values for equality.
+				$isValid = ($gross - $mc_gross) < 0.01;
+			} else {
+				$isPartialRefund = false;
+				$temp_mc_gross = -1 * $mc_gross;
+				$isPartialRefund = ($gross - $temp_mc_gross) > 0.01;
+			}
+			if(!$isValid) $data['akeebasubs_failure_reason'] = 'Paid amount does not match the subscription amount';
+		}
+			
+		// Log the IPN data
+		$this->logIPN($data, $isValid);
+
+		// Fraud attempt? Do nothing more!
+		if(!$isValid) return false;
+
+		// Check the payment_status
+		switch($data['trnApproved'])
+		{
+			case 1:
+				$newStatus = 'C';
+				break;
+			default:
+				$newStatus = 'X';
+				break;
+		}
+
+		// Update subscription status (this also automatically calls the plugins)
+		$updates = array(
+				'akeebasubs_subscription_id'	=> $id,
+				'processor_key'					=> $data['trnId'],
+				'state'							=> $newStatus,
+				'enabled'						=> 0
+		);
+		jimport('joomla.utilities.date');
+		if($newStatus == 'C') {
+			// Fix the starting date if the payment was accepted after the subscription's start date. This
+			// works around the case where someone pays by e-Check on January 1st and the check is cleared
+			// on January 5th. He'd lose those 4 days without this trick. Or, worse, if it was a one-day pass
+			// the user would have paid us and we'd never given him a subscription!
+			$regex = '/^\d{1,4}(\/|-)\d{1,2}(\/|-)\d{2,4}[[:space:]]{0,}(\d{1,2}:\d{1,2}(:\d{1,2}){0,1}){0,1}$/';
+			if(!preg_match($regex, $subscription->publish_up)) {
+				$subscription->publish_up = '2001-01-01';
+			}
+			if(!preg_match($regex, $subscription->publish_down)) {
+				$subscription->publish_down = '2037-01-01';
+			}
+			$jNow = new JDate();
+			$jStart = new JDate($subscription->publish_up);
+			$jEnd = new JDate($subscription->publish_down);
+			$now = $jNow->toUnix();
+			$start = $jStart->toUnix();
+			$end = $jEnd->toUnix();
+
+			if($start < $now) {
+				$duration = $end - $start;
+				$start = $now;
+				$end = $start + $duration;
+				$jStart = new JDate($start);
+				$jEnd = new JDate($end);
+			}
+
+			$updates['publish_up'] = $jStart->toSql();
+			$updates['publish_down'] = $jEnd->toSql();
+			$updates['enabled'] = 1;
+
+		}
+		$subscription->save($updates);
+
+		// Run the onAKAfterPaymentCallback events
+		jimport('joomla.plugin.helper');
+		JPluginHelper::importPlugin('akeebasubs');
+		$app = JFactory::getApplication();
+		$jResponse = $app->triggerEvent('onAKAfterPaymentCallback',array(
+			$subscription
+		));
+   
+		return true;
+	}
+	
+	private function isValidIPN($data)
+	{
+		if($data['hashValue']) {
+			$requestString = null;
+			foreach($data as $key => $val) {
+				if($key == 'hashValue') break;
+				if(isset($requestString)) {
+					if($key == 'trnAmount') {
+						$requestString .= '&' . $key . '=' . urlencode($val);	
+					} else {
+						$requestString .= '&' . $key . '=' . str_replace('.', '%2E', urlencode($val));
+					}
+				} else if($key == 'marker') {
+					$requestString =  'trnApproved=' . $data['trnApproved'];	
+				}
+			}
+			$calcHash = sha1($requestString . trim($this->params->get('hash_key','')));
+			return $calcHash == $data['hashValue'];	
+		}
+		return true;
+	}
+	
+	private function logIPN($data, $isValid)
+	{
+		$config = JFactory::getConfig();
+		if(version_compare(JVERSION, '3.0', 'ge')) {
+			$logpath = $config->get('log_path');
+		} else {
+			$logpath = $config->getValue('log_path');
+		}
+		$logFile = $logpath.'/akpayment_beanstream_ipn.php';
+		jimport('joomla.filesystem.file');
+		if(!JFile::exists($logFile)) {
+			$dummy = "<?php die(); ?>\n";
+			JFile::write($logFile, $dummy);
+		} else {
+			if(@filesize($logFile) > 1048756) {
+				$altLog = $logpath.'/akpayment_beanstream_ipn-1.php';
+				if(JFile::exists($altLog)) {
+					JFile::delete($altLog);
+				}
+				JFile::copy($logFile, $altLog);
+				JFile::delete($logFile);
+				$dummy = "<?php die(); ?>\n";
+				JFile::write($logFile, $dummy);
+			}
+		}
+		$logData = JFile::read($logFile);
+		if($logData === false) $logData = '';
+		$logData .= "\n" . str_repeat('-', 80);
+		$logData .= $isValid ? 'VALID BEANSTREAM IPN' : 'INVALID BEANSTREAM IPN *** FRAUD ATTEMPT OR INVALID NOTIFICATION ***';
+		$logData .= "\nDate/time : ".gmdate('Y-m-d H:i:s')." GMT\n\n";
+		foreach($data as $key => $value) {
+			$logData .= '  ' . str_pad($key, 30, ' ') . $value . "\n";
+		}
+		$logData .= "\n";
+		JFile::write($logFile, $logData);
+	}
+}
---
+++ plugins/akpayment/beanstream/beanstream/form.php
@@ -0,0 +1,28 @@
+<?php defined('_JEXEC') or die(); ?>
+<?php
+$t1 = JText::_('COM_AKEEBASUBS_LEVEL_REDIRECTING_HEADER');
+$t2 = JText::_('COM_AKEEBASUBS_LEVEL_REDIRECTING_BODY');
+?>
+
+<h3><?php echo JText::_('COM_AKEEBASUBS_LEVEL_REDIRECTING_HEADER') ?></h3>
+<p><?php echo JText::_('COM_AKEEBASUBS_LEVEL_REDIRECTING_BODY') ?></p>
+<p align="center">
+<form action="<?php echo htmlentities($data->url) ?>"  method="post" id="paymentForm">
+	<input type="hidden" name="merchant_id" value="<?php echo $data->merchant_id ?>" />
+	<input type="hidden" name="trnOrderNumber" value="<?php echo $data->trnOrderNumber ?>" />
+	<input type="hidden" name="trnAmount" value="<?php echo $data->trnAmount ?>" />
+	<input type="hidden" name="errorPage" value="<?php echo $data->errorPage ?>" />
+	<input type="hidden" name="declinedPage" value="<?php echo $data->declinedPage ?>" />
+	<input type="hidden" name="approvedPage" value="<?php echo $data->approvedPage ?>" />
+	<input type="hidden" name="ordName" value="<?php echo $data->ordName ?>" />
+	<input type="hidden" name="ordEmailAddress" value="<?php echo $data->ordEmailAddress ?>" />
+	<input type="hidden" name="ordAddress1" value="<?php echo $data->ordAddress1 ?>" />
+	<input type="hidden" name="ordAddress2" value="<?php echo $data->ordAddress2 ?>" />
+	<input type="hidden" name="ordCity" value="<?php echo $data->ordCity ?>" />
+	<input type="hidden" name="ordProvince" value="<?php echo $data->ordProvince ?>" />
+	<input type="hidden" name="ordPostalCode" value="<?php echo $data->ordPostalCode ?>" />
+	<input type="hidden" name="ordCountry" value="<?php echo $data->ordCountry ?>" />
+	<input type="hidden" name="trnLanguage" value="<?php echo $data->trnLanguage ?>" />
+	<input type="submit" class="btn" />
+</form>
+</p>
