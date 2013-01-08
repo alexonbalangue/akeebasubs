16daf8f9cc6ff24bd2f6c86b0c2ce60519592c78
tobias.dev <tobias.satzger@gmail.com>
2013-01-08 13:13:35 +0100

    Re #120 Refactor akeebasubs plugins to inherit from superclass

--- plugins/akeebasubs/mailchimp/mailchimp.php
+++ plugins/akeebasubs/mailchimp/mailchimp.php
@@ -7,115 +7,61 @@
 
 defined('_JEXEC') or die();
 
-class plgAkeebasubsMailchimp extends JPlugin
-{
-	/** @var array Levels to Lists to Add mapping */
-	private $addLists = array();
-	
-	/** @var array Levels to Lists to Remove mapping */
-	private $removeLists = array();
-	
-	private $mcApi;
+$akeebasubsinclude = include_once JPATH_ADMINISTRATOR.'/components/com_akeebasubs/assets/akeebasubs.php';
+if(!$akeebasubsinclude) { unset($akeebasubsinclude); return; } else { unset($akeebasubsinclude); }
 
+class plgAkeebasubsMailchimp extends plgAkeebasubsAbstract
+{	
+	private $mcApi;
+	private $delete_member = false;
+	private $send_goodbye = true;
+	private $send_notify = true;
+	private $email_type = 'html';
+	private $double_optin = true;
+	private $send_welcome = false;
+	
 	public function __construct(& $subject, $config = array())
 	{
-		if(!is_object($config['params'])) {
-			jimport('joomla.registry.registry');
-			$config['params'] = new JRegistry($config['params']);
-		}
-
-		parent::__construct($subject, $config);
+		$templatePath = dirname(__FILE__);
+		$name = 'mailchimp';
+		
+		parent::__construct($subject, $name, $config, $templatePath);
 		
 		// Load the MailChimp library
 		require_once dirname(__FILE__).'/library/MCAPI.class.php';
-		$apiKey = $this->params->get('mailchimp_key','');
-		$this->mcApi = new MCAPI($apiKey);
-
-		// Load level to group mapping from plugin parameters		
-		$strAddLists = $this->params->get('addlists','');
-		$this->addLists = $this->parseLists($strAddLists);
 		
-		$strRemoveLists = $this->params->get('removelists','');
-		$this->removeLists = $this->parseLists($strRemoveLists);
-	}
+		$configParams = @json_decode($config['params']);
+		$apiKey = $configParams->mailchimp_key;
+		$this->mcApi = new MCAPI($apiKey);
+		$this->delete_member = $configParams->delete_member;
+		$this->send_goodbye = $configParams->send_goodbye;
+		$this->send_notify = $configParams->send_notify;
+		$this->email_type = $configParams->email_type;
+		$this->double_optin = $configParams->double_optin;
+		$this->send_welcome = $configParams->send_welcome;
+		
+		// Do we have values from the Olden Days?
+		$strAddGroups = $configParams->addlists;
+		$strRemoveGroups = $configParams->removelists;
 
-	/**
-	 * Called whenever a subscription is modified. Namely, when its enabled status,
-	 * payment status or valid from/to dates are changed.
-	 */
-	public function onAKSubscriptionChange($row, $info)
-	{
-		if(is_null($info['modified']) || empty($info['modified'])) return;
-		if(array_key_exists('enabled', (array)$info['modified'])) {
-			$this->onAKUserRefresh($row->user_id);
+		if(!empty($strAddGroups) || !empty($strAddGroups)) {
+			// Load level to group mapping from plugin parameters		
+			$this->addGroups = $this->parseGroups($strAddGroups);
+			$this->removeGroups = $this->parseGroups($strRemoveGroups);
+			// Do a transparent upgrade
+			$this->upgradeSettings($config);
+		} else {
+			$this->loadGroupAssignments();
 		}
 	}
 	
-	/**
-	 * Called whenever the administrator asks to refresh integration status.
-	 * 
-	 * @param $user_id int The Joomla! user ID to refresh information for.
-	 */
 	public function onAKUserRefresh($user_id)
 	{
-		// Make sure we're configured
-		if(empty($this->addLists) && empty($this->removeLists)) return;
-	
-		// Get all of the user's subscriptions
-		$subscriptions = FOFModel::getTmpInstance('Subscriptions','AkeebasubsModel')
-			->user_id($user_id)
-			->getList();
-			
-		// Make sure there are subscriptions set for the user
-		if(!count($subscriptions)) return;
-		
-		// Get the initial lists to add/remove from
+		// Load groups
 		$addLists = array();
 		$removeLists = array();
-		foreach($subscriptions as $sub) {
-			$level = $sub->akeebasubs_level_id;
-			if($sub->enabled) {
-				// Enabled subscription, add lists
-				if(empty($this->addLists)) continue;
-				if(!array_key_exists($level, $this->addLists)) continue;
-				$lists = $this->addLists[$level];
-				foreach($lists as $list) {
-					if(!in_array($list, $addLists) && !empty($list)) {
-						$addLists[] = $list;
-					}
-				}
-			} else {
-				// Disabled subscription, remove lists
-				if(empty($this->removeLists)) continue;
-				if(!array_key_exists($level, $this->removeLists)) continue;
-				$lists = $this->removeLists[$level];
-				
-				foreach($lists as $list) {
-					if(!in_array($list, $removeLists) && !empty($list)) {
-						$removeLists[] = $list;
-					}
-				}
-			}
-		}
-		
-		// If no groups are detected, do nothing
+		$this->loadUserGroups($user_id, $addLists, $removeLists);
 		if(empty($addLists) && empty($removeLists)) return;
-		
-		// Sort the lists
-		asort($addLists);
-		asort($removeLists);
-		
-		// Clean up the remove groups: if we are asked to both add and remove a user
-		// from a group, add wins.
-		if(!empty($removeLists) && !empty($addLists)) {
-			$temp = $removeLists;
-			$removeLists = array();
-			foreach($temp as $list) {
-				if(!in_array($list, $addLists)) {
-					$removeLists[] = $list;
-				}
-			}
-		}
 		
 		// Get the user's name and email
 		$user = JUser::getInstance($user_id);
@@ -136,39 +82,39 @@ class plgAkeebasubsMailchimp extends JPlugin
 		
 		// Remove from MailChimp list
 		if(!empty($removeLists)) {
-			foreach($removeLists as $listToRemove) {
-				if(is_array($currentLists) && in_array($listToRemove, $currentLists)) {
-					$mcSubscribeId = $user_id . ':' . $listToRemove;
+			foreach($removeLists as $mcListToRemove) {
+				if(is_array($currentLists) && in_array($mcListToRemove, $currentLists)) {
+					$mcSubscribeId = $user_id . ':' . $mcListToRemove;
 					$this->mcApi->listUnsubscribe(
-							$listToRemove,
+							$mcListToRemove,
 							$email,
-							$this->params->get('delete_member', false),
-							$this->params->get('send_goodbye', true),
-							$this->params->get('send_notify', true));
+							$this->delete_member,
+							$this->send_goodbye,
+							$this->send_notify);
 				}
-				$mcSubscribeId = $user_id . ':' . $listToRemove;
+				$mcSubscribeId = $user_id . ':' . $mcListToRemove;
 				$session->clear('mailchimp.' . $mcSubscribeId, 'com_akeebasubs');
 			}
 		}
 		
 		// Add to MailChimp list
 		if(!empty($addLists)) {
-			foreach($addLists as $listToAdd) {
-				if(! (is_array($currentLists) && in_array($listToAdd, $currentLists))) {
-					$mcSubscribeId = $user_id . ':' . $listToAdd;
+			foreach($addLists as $mcListToAdd) {
+				if(! (is_array($currentLists) && in_array($mcListToAdd, $currentLists))) {
+					$mcSubscribeId = $user_id . ':' . $mcListToAdd;
 					if($session->get('mailchimp.' . $mcSubscribeId, '', 'com_akeebasubs') != 'new') {
 						// Subscribe if email is not already in the MailChimp list and
 						// if the subscription is not already sent for that user (but not confirmed yet)
 						if($this->mcApi->listSubscribe(
-								$listToAdd,
+								$mcListToAdd,
 								$email,
 								array('FNAME' => $firstName, 'LNAME' => $lastName),
-								$this->params->get('email_type', 'html'),
-								$this->params->get('double_optin', true),
+								$this->email_type,
+								$this->double_optin,
 								true,
 								false,
-								$this->params->get('send_welcome', false))) {
-							// Add new MailChimp subscription to session to void that MailChimp sends multiple
+								$this->send_welcome)) {
+							// Add new MailChimp subscription to session to avoid that MailChimp sends multiple
 							// emails for one subscription (before subscription is confirmed by the user)
 							$session->set('mailchimp.' . $mcSubscribeId , 'new', 'com_akeebasubs');
 						}	
@@ -179,95 +125,100 @@ class plgAkeebasubsMailchimp extends JPlugin
 	}
 	
 	/**
-	 * Converts an Akeeba Subscriptions level to a numeric ID
+	 * =========================================================================
+	 * !!! CRUFT WARNING !!!
+	 * =========================================================================
 	 * 
-	 * @param $title string The level's name to be converted to an ID
-	 *
-	 * @return int The subscription level's ID or -1 if no match is found
+	 * The following methods are leftovers from the Olden Days (before 2.4.5).
+	 * At some point (most likely 2.6) they will be removed. For now they will
+	 * stay here so that we can do a transparent migration.
 	 */
-	private function ASLevelToId($title)
+	
+	/**
+	 * Moves this plugin's settings from the plugin into each subscription
+	 * level's configuration parameters.
+	 */
+	protected function upgradeSettings($config = array())
 	{
-		static $levels = null;
-		
-		// Don't process invalid titles
-		if(empty($title)) return -1;
-		
-		// Fetch a list of subscription levels if we haven't done so already
-		if(is_null($levels)) {
-			$levels = array();
-			$list = FOFModel::getTmpInstance('Levels','AkeebasubsModel')
-				->getList();
-			if(count($list)) foreach($list as $level) {
-				$thisTitle = strtoupper($level->title);
-				$levels[$thisTitle] = $level->akeebasubs_level_id;
+		$model = FOFModel::getTmpInstance('Levels','AkeebasubsModel');
+		$levels = $model->getList(true);
+		$addgroupsKey = strtolower($this->name).'_addgroups';
+		$removegroupsKey = strtolower($this->name).'_removegroups';
+		if(!empty($levels)) {
+			foreach($levels as $level)
+			{
+				$save = false;
+				if(is_string($level->params)) {
+					$level->params = @json_decode($level->params);
+					if(empty($level->params)) {
+						$level->params = new stdClass();
+					}
+				} elseif(empty($level->params)) {
+					$level->params = new stdClass();
+				}
+				if(array_key_exists($level->akeebasubs_level_id, $this->addGroups)) {
+					if(empty($level->params->$addgroupsKey)) {
+						$level->params->$addgroupsKey = $this->addGroups[$level->akeebasubs_level_id];
+						$save = true;
+					}
+				}
+				if(array_key_exists($level->akeebasubs_level_id, $this->removeGroups)) {
+					if(empty($level->params->$removegroupsKey)) {
+						$level->params->$removegroupsKey = $this->removeGroups[$level->akeebasubs_level_id];
+						$save = true;
+					}
+				}
+				if($save) {
+					$level->params = json_encode($level->params);
+					$result = $model->setId($level->akeebasubs_level_id)->save( $level );
+				}
 			}
 		}
 		
-		$title = strtoupper($title);
-		if(array_key_exists($title, $levels)) {
-			// Mapping found
-			return($levels[$title]);
-		} elseif( (int)$title == $title ) {
-			// Numeric ID passed
-			return (int)$title;
-		} else {
-			// No match!
-			return -1;
+		// Remove the plugin parameters
+		if(isset($config['params'])) {
+			$configParams = @json_decode($config['params']);
+			unset($configParams->addlists);
+			unset($configParams->removelists);
+			$param_string = @json_encode($configParams);
+			
+			$db = JFactory::getDbo();
+			$query = $db->getQuery(true)
+				->update($db->qn('#__extensions'))
+				->where($db->qn('type').'='.$db->q('plugin'))
+				->where($db->qn('element').'='.$db->q(strtolower($this->name)))
+				->where($db->qn('folder').'='.$db->q('akeebasubs'))
+				->set($db->qn('params').' = '.$db->q($param_string));
+			$db->setQuery($query);
+			$db->query();
 		}
-	}
-	
-	private function MailChimpListToId($name)
-	{
-		// Get the list
-		$mcLists = $this->mcApi->lists(array(
-			'list_name' => $name
-			), 0, 1
-		);
-		
-		if($mcLists['total'] > 0) {
-			// Get the id
-			$id = $mcLists['data'][0]['id'];
-			return $id;
-		}
-		
-		// No match!
-		return -1;
 	}
 
-	private function parseLists($rawData)
-	{
-		if(empty($rawData)) return array();
+	protected function getGroups() {
+		static $groups = null;
 		
-		$ret = array();
-		
-		// Just in case something funky happened...
-		$rawData = str_replace("\\n", "\n", $rawData);
-		$rawData = str_replace("\r", "\n", $rawData);
-		$rawData = str_replace("\n\n", "\n", $rawData);
-		
-		$lines = explode("\n", $rawData);
-		
-		foreach($lines as $line) {
-			$line = trim($line);
-			$parts = explode('=', $line, 2);
-			if(count($parts) != 2) continue;
+		if(is_null($groups)) {
+			$groups = array();
 			
-			$level = $parts[0];
-			$rawLists = $parts[1];
-			
-			$lists = explode(',', $rawLists);
-			if(empty($lists)) continue;
-			if(!is_array($lists)) $lists = array($lists);
-			
-			$levelId = $this->ASLevelToId($level);
-			$listIds = array();
-			foreach($lists as $listName) {
-				$listIds[] = $this->MailChimpListToId($listName);
+			$start = 0;
+			$limit = 100;
+			$mcLists = $this->mcApi->lists(array(), $start, $limit);
+			$total = $mcLists['total'];
+			while(true) {
+				foreach($mcLists['data'] as $list) {
+					$listTitle = $list['name'];
+					$listId = $list['id'];
+					$groups[$listTitle] = $listId;
+				}
+				if(($start + $limit) < $total) {
+					$start += $limit;
+					$mcLists = $this->mcApi->lists(array(), $start, $limit);
+				} else {
+					break;
+				}
 			}
-			
-			$ret[$levelId] = $listIds;
 		}
 		
-		return $ret;
+		return $groups;
 	}
 }
