a2f76624f29c2895060259ae1e7d1914037e9e0b
tobias.dev <tobias.satzger@gmail.com>
2013-02-08 00:28:04 +0100

    Re #136 Implementation of VIVA Payments

---
+++ plugins/akpayment/viva/viva/form.php
@@ -0,0 +1,13 @@
+<?php defined('_JEXEC') or die(); ?>
+<?php
+$t1 = JText::_('COM_AKEEBASUBS_LEVEL_REDIRECTING_HEADER');
+$t2 = JText::_('COM_AKEEBASUBS_LEVEL_REDIRECTING_BODY');
+?>
+
+<h3><?php echo JText::_('COM_AKEEBASUBS_LEVEL_REDIRECTING_HEADER') ?></h3>
+<p><?php echo JText::_('COM_AKEEBASUBS_LEVEL_REDIRECTING_BODY') ?></p>
+<p align="center">
+<form action="<?php echo htmlentities($url) ?>" method="post" id="paymentForm">
+	<input type="submit" class="btn" />
+</form>
+</p>
