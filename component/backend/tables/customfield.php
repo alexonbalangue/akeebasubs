9ab588c2f98b941885d0a95a0d47d735e001d0ff
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2013-03-31 16:12:47 +0300

    Re #151 Make sure the subscription levels exist when saving a custom field

--- component/backend/tables/customfield.php
+++ component/backend/tables/customfield.php
@@ -18,6 +18,34 @@ class AkeebasubsTableCustomfield extends FOFTable
 				$this->$slug = str_replace('-', '_', $this->$slug);
 			}
 		}
+
+		// Make sure assigned subscription levels really do exist and normalize the list
+		if(!empty($this->akeebasubs_level_id)) {
+			if(is_array($this->akeebasubs_level_id)) {
+				$subs = $this->akeebasubs_level_id;
+			} else {
+				$subs = explode(',', $this->akeebasubs_level_id);
+			}
+			if(empty($subs)) {
+				$this->akeebasubs_level_id = '';
+			} else {
+				$subscriptions = array();
+				foreach($subs as $id) {
+					$subObject = FOFModel::getTmpInstance('Levels','AkeebasubsModel')
+						->setId($id)
+						->getItem();
+					$id = null;
+					if(is_object($subObject)) {
+						if($subObject->akeebasubs_level_id > 0) {
+							$id = $subObject->akeebasubs_level_id;
+						}
+					}
+					if(!is_null($id)) $subscriptions[] = $id;
+				}
+				$this->akeebasubs_level_id = implode(',', $subscriptions);
+			}
+		}
+
 		return $result;
 	}
 }
