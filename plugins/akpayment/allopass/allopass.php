9395c733fb16e6f375395d6aac9b9c6050371ac0
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2013-04-16 11:01:45 +0300

    ~ Rewritten AlloPass integration plugin

--- plugins/akpayment/allopass/allopass.php
+++ plugins/akpayment/allopass/allopass.php
@@ -101,8 +101,25 @@ class plgAkpaymentAlloPass extends plgAkpaymentAbstract
 		// Check if we're supposed to handle this
 		if($paymentmethod != $this->ppName) return false;
 
+		// Get the action to perform after the validation
+		$action = 'success';
+		if (isset($data['aksubs_context']))
+		{
+			$action = $data['aksubs_context'];
+			unset ($data['aksubs_context']);
+		}
+		if (!in_array($action, array('success', 'cancel', 'postback')))
+		{
+			$action = 'postback';
+		}
+
+		$isValid = true;
+
 		// Check IPN data for validity (i.e. protect against fraud attempt)
-		$isValid = $this->isValidIPN($data);
+		if ($action == 'postback')
+		{
+			$isValid = $this->isValidIPN($data);
+		}
 		if(!$isValid) $data['akeebasubs_failure_reason'] = 'Invalid response received.';
 
 		// Load the relevant subscription row
@@ -123,96 +140,191 @@ class plgAkpaymentAlloPass extends plgAkpaymentAbstract
 			if(!$isValid) $data['akeebasubs_failure_reason'] = 'The merchant_transaction_id is invalid';
 		}
 
-		// Check that transaction_id has not been previously processed
-		if($isValid && !is_null($subscription)) {
-			if($subscription->processor_key == $data['transaction_id']) {
-				$isValid = false;
-				$data['akeebasubs_failure_reason'] = "I will not process the same transaction_id twice";
-			}
+		if ($action == 'success')
+		{
+			$isValid = $this->validateOneTime($data, $subscription);
+			if(!$isValid) $data['akeebasubs_failure_reason'] = 'Invalid response received.';
 		}
 
-		// Check that currency is correct and set corresponding amount
-		if($isValid && !is_null($subscription)) {
-			$mc_currency = strtoupper($data['currency']);
-			$currency = strtoupper(AkeebasubsHelperCparams::getParam('currency','EUR'));
-			if($mc_currency != $currency) {
-				$mc_currency = strtoupper($data['payout_currency']);
+		if ($action != 'cancel')
+		{
+			// Check that transaction_id has not been previously processed
+			if($isValid && !is_null($subscription)) {
+				if($subscription->processor_key == $data['transaction_id']) {
+					$isValid = false;
+					$data['akeebasubs_failure_reason'] = "I will not process the same transaction_id twice";
+				}
+			}
+
+			// Check that currency is correct and set corresponding amount
+			if($isValid && !is_null($subscription) && ($action == 'callback')) {
+				$mc_currency = strtoupper($data['currency']);
+				$currency = strtoupper(AkeebasubsHelperCparams::getParam('currency','EUR'));
 				if($mc_currency != $currency) {
-					$mc_currency = strtoupper($data['reference_currency']);
+					$mc_currency = strtoupper($data['payout_currency']);
 					if($mc_currency != $currency) {
-						$isValid = false;
-						$data['akeebasubs_failure_reason'] = "Invalid currency";
+						$mc_currency = strtoupper($data['reference_currency']);
+						if($mc_currency != $currency) {
+							$isValid = false;
+							$data['akeebasubs_failure_reason'] = "Invalid currency";
+						} else {
+							$mc_gross = floatval($data['reference_amount']);
+						}
 					} else {
-						$mc_gross = floatval($data['reference_amount']);
+						$mc_gross = floatval($data['payout_amount']);
 					}
 				} else {
-					$mc_gross = floatval($data['payout_amount']);
+					$mc_gross = floatval($data['amount']);
 				}
-			} else {
-				$mc_gross = floatval($data['amount']);
+			}
+
+			// Check that account is correct
+			$isPartialRefund = false;
+			if($isValid && !is_null($subscription)) {
+				$gross = $subscription->gross_amount;
+				if($mc_gross > 0) {
+					// A positive value means "payment". The prices MUST match!
+					// Important: NEVER, EVER compare two floating point values for equality.
+					$isValid = ($gross - $mc_gross) < 0.01;
+				} else {
+					$isPartialRefund = false;
+					$temp_mc_gross = -1 * $mc_gross;
+					$isPartialRefund = ($gross - $temp_mc_gross) > 0.01;
+				}
+				if(!$isValid) $data['akeebasubs_failure_reason'] = 'Paid amount does not match the subscription amount';
+			}
+
+			// Log the IPN data
+			$this->logIPN($data, $isValid);
+
+			// Fraud attempt? Do nothing more!
+			if(!$isValid) return false;
+
+
+			// Check the payment_status
+			switch($data['status'])
+			{
+				case -1:
+					// Payment accepted
+					$newStatus = 'P';
+					break;
+				case 0:
+					// The transaction has been initiated but has not yet completed
+					$newStatus = 'C';
+					break;
+				default:
+					$newStatus = 'X';
+					break;
+			}
+
+			// Update subscription status (this also automatically calls the plugins)
+			$updates = array(
+					'akeebasubs_subscription_id'	=> $id,
+					'processor_key'					=> $data['transaction_id'],
+					'state'							=> $newStatus,
+					'enabled'						=> 0
+			);
+			JLoader::import('joomla.utilities.date');
+			if($newStatus == 'C') {
+				$this->fixDates($subscription, $updates);
+
+			}
+			$subscription->save($updates);
+
+			// Run the onAKAfterPaymentCallback events
+			JLoader::import('joomla.plugin.helper');
+			JPluginHelper::importPlugin('akeebasubs');
+			$app = JFactory::getApplication();
+			$jResponse = $app->triggerEvent('onAKAfterPaymentCallback',array(
+				$subscription
+			));
+
+			// Return the result for the callback context
+			if ($action == 'callback')
+			{
+				return $isValid;
 			}
 		}
 
-		// Check that acount is correct
-		$isPartialRefund = false;
-		if($isValid && !is_null($subscription)) {
-			$gross = $subscription->gross_amount;
-			if($mc_gross > 0) {
-				// A positive value means "payment". The prices MUST match!
-				// Important: NEVER, EVER compare two floating point values for equality.
-				$isValid = ($gross - $mc_gross) < 0.01;
-			} else {
-				$isPartialRefund = false;
-				$temp_mc_gross = -1 * $mc_gross;
-				$isPartialRefund = ($gross - $temp_mc_gross) > 0.01;
-			}
-			if(!$isValid) $data['akeebasubs_failure_reason'] = 'Paid amount does not match the subscription amount';
-		}
+		// ==== The rest is called only in the success and cancel contexts
 
-		// Log the IPN data
-		$this->logIPN($data, $isValid);
-
-		// Fraud attempt? Do nothing more!
-		if(!$isValid) return false;
-
-
-		// Check the payment_status
-		switch($data['status'])
+		// If we don't have a valid subscription, quit
+		if (is_null($subscription) || !($subscription instanceof AkeebasubsTableSubscription))
 		{
-			case -1:
-				// Payment accepted
-				$newStatus = 'P';
-				break;
-			case 0:
-				// The transaction has been initiated but has not yet completed
-				$newStatus = 'C';
-				break;
-			default:
-				$newStatus = 'X';
-				break;
+			die('Subscription not found.');
 		}
 
-		// Update subscription status (this also automatically calls the plugins)
-		$updates = array(
-				'akeebasubs_subscription_id'	=> $id,
-				'processor_key'					=> $data['transaction_id'],
-				'state'							=> $newStatus,
-				'enabled'						=> 0
-		);
-		JLoader::import('joomla.utilities.date');
-		if($newStatus == 'C') {
-			$this->fixDates($subscription, $updates);
-
+		// If the response is not validated show the subscription cancellation message
+		if (!$isValid)
+		{
+			$action = 'cancel';
 		}
-		$subscription->save($updates);
 
-		// Run the onAKAfterPaymentCallback events
-		JLoader::import('joomla.plugin.helper');
-		JPluginHelper::importPlugin('akeebasubs');
+		// Perform a redirection to the "thank you" or "canelled" page
+		$slug = FOFModel::getTmpInstance('Levels','AkeebasubsModel')
+				->setId($subscription->akeebasubs_level_id)
+				->getItem()
+				->slug;
+		$rootURL = rtrim(JURI::base(),'/');
+		$subpathURL = JURI::base(true);
+		if(!empty($subpathURL) && ($subpathURL != '/')) {
+			$rootURL = substr($rootURL, 0, -1 * strlen($subpathURL));
+		}
+		$layout = ($action == 'cancel') ? 'cancel' : 'order';
+
+		$url = 'index.php?option=com_akeebasubs&view=message&layout=default&slug=' . $slug . '&layout=' . $layout . '&subid=' . $subscription->akeebasubs_subscription_id;
 		$app = JFactory::getApplication();
-		$jResponse = $app->triggerEvent('onAKAfterPaymentCallback',array(
-			$subscription
-		));
+		$app->redirect($url);
+
+		return true;
+	}
+
+	/**
+	 * Validate the incoming AlloPass transaction in the success context
+	 *
+	 * @param   array                        $data          The incoming data
+	 * @param   AkeebasubsTableSubscription  $subscription  Subscription record
+	 *
+	 * @return  boolean
+	 */
+	private function validateOneTime($data, $subscription)
+	{
+		// Not a valid subscription record? No way anything else is valid!
+		if (is_null($subscription))
+		{
+			return false;
+		}
+
+		$recall = $data['RECALL'];
+		$alloPass = $this->apMapping[$subscription->akeebasubs_level_id];
+		$auth = implode('/', array_values($alloPass));
+
+		$recall = urlencode($recall);
+		$auth = urlencode($auth);
+
+		$url = "http://payment.allopass.com/api/checkcode.apu?code=$recall&auth=$auth";
+
+		$ch = curl_init($url);
+		@curl_setopt($ch, CURLOPT_MAXREDIRS, 20);
+		curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+		curl_setopt($ch, CURLOPT_FAILONERROR, true);
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+		// Pretend we are IE7, so that webservers play nice with us
+		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.0.3705; .NET CLR 1.1.4322; Media Center PC 4.0)');
+		$result = curl_exec($ch);
+		curl_close($ch);
+
+		// If no data came through, forget about it
+		if(($result === false) || empty($result)) return false;
+
+		if (substr($result, 0, 2) != 'OK')
+		{
+			return false;
+		}
 
 		return true;
 	}
@@ -222,6 +334,8 @@ class plgAkpaymentAlloPass extends plgAkpaymentAbstract
 	 */
 	private function isValidIPN($data)
 	{
+		var_dump($data);die();
+
 		$secretKey = $this->params->get('skey','');
 
 		$apiHash = 'sha1';
@@ -230,7 +344,7 @@ class plgAkpaymentAlloPass extends plgAkpaymentAbstract
 		}
 		$apiSig = $data['api_sig'];
 
-		$ignore = array('api_sig', 'Itemid', 'option', 'view', 'paymentmethod');
+		$ignore = array('api_sig', 'Itemid', 'option', 'view', 'paymentmethod', 'aksubs_context', 'lang');
 
 		ksort($data);
 		$string2compute = '';
@@ -310,18 +424,36 @@ class plgAkpaymentAlloPass extends plgAkpaymentAbstract
 			if($levelId < 0) continue;
 
 			$rawAlloPass = $parts[1];
-			$alloPass = explode(':', $rawAlloPass);
+			if(stristr($parts[1], ':'))
+			{
+				// Legacy parameter handling
+				$alloPass = explode(':', $rawAlloPass);
+			}
+			else
+			{
+				$alloPass = explode('/', $rawAlloPass);
+			}
 			if(empty($alloPass)) continue;
 			if(count($alloPass) < 2) continue;
 
 			$siteId = trim($alloPass[0]);
-			if(empty($siteId)) continue;
+			if (empty($siteId))
+			{
+				continue;
+			}
+
 			$productId = trim($alloPass[1]);
-			if(empty($productId)) continue;
+			if (empty($productId))
+			{
+				continue;
+			}
+
+			$other_id = trim($alloPass[2]);
 
 			$pricePoint = array(
 				'site_id'		=> $siteId,
-				'product_id'	=> $productId
+				'product_id'	=> $productId,
+				'other_id'		=> $other_id
 			);
 
 			$ret[$levelId] = $pricePoint;
