50164472fc61d927d6ea476e4e659ba812be9f0a
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2013-05-20 09:24:55 +0300

    # [LOW] Front-end page titles include escaped HTML

---
+++ component/frontend/toolbar.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *  @package AkeebaSubs
+ *  @copyright Copyright (c)2010-2013 Nicholas K. Dionysopoulos
+ *  @license GNU General Public License version 3, or later
+ */
+
+// Protect from unauthorized access
+defined('_JEXEC') or die();
+
+class AkeebasubsToolbar extends FOFToolbar
+{
+
+}
