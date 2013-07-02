d9ff22137b694c728a1df79a7d14a70a3c3d9f42
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2013-07-02 19:00:43 +0300

    Upgraded users browse view to XML forms

--- component/script.akeebasubs.php
+++ component/script.akeebasubs.php
@@ -284,6 +284,7 @@ class Com_AkeebasubsInstallerScript
 			'administrator/components/com_akeebasubs/view/users/tmpl/default.php',
 
 			// Do not delete (used to render custom form page elements):
+			// 'administrator/components/com_akeebasubs/view/coupons/tmpl/default.php',
 			// 'administrator/components/com_akeebasubs/view/invoices/tmpl/default.php',
 			// 'administrator/components/com_akeebasubs/view/taxrules/tmpl/default.php',
 
