c180c01e78fbcb4b3b4371a753bfdf293d3bf6df
tampe125 <tampe125@gmail.com>
2013-08-06 15:52:09 +0200

    re #171 Created frontend skeleton

---
+++ component/backend/models/apicoupons.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package AkeebaSubs
+ * @copyright Copyright (c)2010-2013 Nicholas K. Dionysopoulos
+ * @license GNU General Public License version 3, or later
+ */
+
+// Protect from unauthorized access
+defined('_JEXEC') or die();
+
+class AkeebasubsModelApicoupons extends FOFModel
+{
+	public function createCoupon($key, $pwd)
+	{
+		
+	}
+}
