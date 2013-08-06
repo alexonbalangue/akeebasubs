99792dbb87e0cd0a8650e83b61c059a2851823d9
tampe125 <tampe125@gmail.com>
2013-08-06 14:21:21 +0200

    Added checks while saving

--- component/backend/tables/couponsapi.php
+++ component/backend/tables/couponsapi.php
@@ -35,6 +35,50 @@ class AkeebasubsTableCouponsapi extends FOFTable
 			$this->password = md5(microtime());
 		}
 
+		// Make sure assigned subscriptions really do exist and normalize the list
+		if(!empty($this->subscriptions)) {
+			if(is_array($this->subscriptions)) {
+				$subs = $this->subscriptions;
+			} else {
+				$subs = explode(',', $this->subscriptions);
+			}
+			if(empty($subs)) {
+				$this->subscriptions = '';
+			} else {
+				$subscriptions = array();
+				foreach($subs as $id) {
+					$subObject = FOFModel::getTmpInstance('Levels','AkeebasubsModel')
+						->setId($id)
+						->getItem();
+					$id = null;
+					if(is_object($subObject)) {
+						if($subObject->akeebasubs_level_id > 0) {
+							$id = $subObject->akeebasubs_level_id;
+						}
+					}
+					if(!is_null($id)) $subscriptions[] = $id;
+				}
+				$this->subscriptions = implode(',', $subscriptions);
+			}
+		}
+
+		// Check the type
+		if(!in_array($this->type, array('value','percent')))
+		{
+			$this->type = 'value';
+		}
+
+		// Check value
+		if($this->value < 0)
+		{
+			$this->setError(JText::_('COM_AKEEBASUBS_COUPON_ERR_VALUE'));
+			$result = false;
+		}
+		elseif( ($this->value > 100) && ($this->type == 'percent') )
+		{
+			$this->value = 100;
+		}
+
 		return parent::check() && $result;
 	}
 }
