45575ac84c63bc2894a3dae701bb1a44a973f2c2
tampe125 <tampe125@gmail.com>
2013-08-07 11:06:45 +0200

    re #171 Added limits checks for API coupon creation. Limits are: . number of coupon created . number of coupon used . value of coupon used. If you're going to use the last limit, you should never touch coupon value/type nor subscritpion value, or weird stuff will happen.

--- component/backend/models/apicoupons.php
+++ component/backend/models/apicoupons.php
@@ -22,14 +22,18 @@ class AkeebasubsModelApicoupons extends FOFModel
 		$table->load(array('key' => $key, 'password' => $pwd));
 
 		// Are they valid?
-		if(!$table->akeebasubs_apicoupon_id)
+		if(!$table->akeebasubs_apicoupon_id || !$table->enabled)
 		{
 			return array('error' => JText::_('COM_AKEEBASUBS_APICOUPONS_INVALID_CREDENTIALS'));
 		}
 
 		// Do I hit a limit?
-		// TODO Check for limits
+		if(!$this->performApiChecks($table))
+		{
+			return array('error' => JText::_('COM_AKEEBASUBS_APICOUPONS_LIMIT_EXCEEDED'));
+		}
 
+		// If I'm here, I'm clear to go
 		JLoader::import('joomla.user.helper');
 		$coupon = FOFTable::getAnInstance('Coupon', 'AkeebasubsTable');
 
@@ -51,5 +55,79 @@ class AkeebasubsModelApicoupons extends FOFModel
 		}
 
 		return array('coupon' => $coupon->coupon);
+	}
+
+	protected function performApiChecks($table)
+	{
+		$db = JFactory::getDbo();
+
+		if($table->creation_limit)
+		{
+			$query = $db->getQuery(true)
+				->select('COUNT(*)')
+				->from('#__akeebasubs_coupons')
+				->where('akeebasubs_apicoupon_id = '.$table->akeebasubs_apicoupon_id);
+			if($db->setQuery($query)->loadResult() >= $table->creation_limit)
+			{
+				return false;
+			}
+		}
+		elseif($table->subscription_limit)
+		{
+			$query = $db->getQuery(true)
+				->select('akeebasubs_coupon_id')
+				->from('#__akeebasubs_coupons')
+				->where('akeebasubs_apicoupon_id = '.$table->akeebasubs_apicoupon_id);
+			$coupons = $db->setQuery($query)->loadColumn();
+
+			if($coupons)
+			{
+				$query = $db->getQuery(true)
+					->select('COUNT(*)')
+					->from('#__akeebasubs_subscriptions')
+					->where('akeebasubs_coupon_id IN('.implode(',', $coupons).')');
+				if($db->setQuery($query)->loadResult() >= $table->subscription_limit)
+				{
+					return false;
+				}
+			}
+		}
+		elseif($table->value_limit)
+		{
+			$query = $db->getQuery(true)
+				->select('akeebasubs_coupon_id')
+				->from('#__akeebasubs_coupons')
+				->where('akeebasubs_apicoupon_id = '.$table->akeebasubs_apicoupon_id);
+			$coupons = $db->setQuery($query)->loadColumn();
+
+			if($coupons)
+			{
+				$query = $db->getQuery(true)
+					->select('SUM(net_amount) as total_amount, COUNT(*) as total')
+					->from('#__akeebasubs_subscriptions')
+					->where('akeebasubs_coupon_id IN('.implode(',', $coupons).')');
+				$sub = $db->setQuery($query)->loadObject();
+
+				if($table->type == 'value')
+				{
+					// Did I hit the limit using "fixed" discount value (ie 5$ off)?
+					if(($sub->total * $table->value) >= $table->value_limit)
+					{
+						return false;
+					}
+				}
+				else
+				{
+					// Did I hit the limit using % discount value (ie 15% off)?
+					if(($sub->total_amount * 100 / $table->value) >= $table->value_limit)
+					{
+						return false;
+					}
+				}
+
+			}
+		}
+
+		return true;
 	}
 }
