15001aa3fb2889a35163b8f9661c4ff12ee9f9fc
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2013-08-13 11:39:14 +0300

    # [LOW] Notices thrown by 2Checkout integration plugin

--- plugins/akpayment/2checkout/2checkout/form.php
+++ plugins/akpayment/2checkout/2checkout/form.php
@@ -11,26 +11,26 @@ $t2 = JText::_('COM_AKEEBASUBS_LEVEL_REDIRECTING_BODY');
 	<input type="hidden" name="sid" value="<?php echo $data->sid?>" />
 	<input type="hidden" name="cart_order_id" value="<?php echo $subscription->akeebasubs_subscription_id ?>" />
 	<input type="hidden" name="total" value="<?php echo sprintf('%02.2f',$subscription->gross_amount) ?>" />
-	
+
 	<input type="hidden" name="c_prod_1" value="<?php echo $level->akeebasubs_level_id ?>" />
 	<input type="hidden" name="c_name_1" value="<?php echo $level->title ?>" />
 	<input type="hidden" name="c_description_1" value="<?php echo $level->title . ' - [ ' . $user->username . ' ]' ?>" />
 	<input type="hidden" name="c_price_1" value="<?php echo sprintf('%02.2f',$subscription->gross_amount) ?>" />
 	<input type="hidden" name="c_tangible_1" value="N" />
-	
+
 	<?php if($data->params->get('demo',0)): ?>
 	<input type="hidden" name="demo" value="Y" />
 	<?php endif;?>
 	<input type="hidden" name="fixed" value="Y" />
 	<input type="hidden" name="lang" value="<?php echo $data->params->get('lang','en') ?>" />
-	<input type="hidden" name="merchant_order_id" value="<?php echo $subscription->id ?>" />
+	<input type="hidden" name="merchant_order_id" value="<?php echo $subscription->akeebasubs_subscription_id ?>" />
 	<input type="hidden" name="pay_method" value="<?php echo strtoupper($data->params->get('pay_method','cc')) ?>" />
 	<?php if($data->params->get('skip_landing',0)): ?>
 	<input type="hidden" name="skip_landing" value="1" />
 	<?php endif; ?>
 	<input type="hidden" name="id_type" value="1" />
 	<input type="hidden" name="x_receipt_link_url" value="<?php echo $data->x_receipt_link_url ?>" />
-	
+
 	<input type="hidden" name="card_holder_name" value="<?php echo $data->name ?>" />
 	<input type="hidden" name="street_address" value="<?php echo $kuser->address1 ?>">
 	<input type="hidden" name="street_address2" value="<?php echo $kuser->address2 ?>">
