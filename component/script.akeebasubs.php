ad3c96a7ea79265b4e71dd8f00b79d52014a4906
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2013-08-19 22:43:26 +0300

    Clear FOF cache on update wasn't really working

--- component/script.akeebasubs.php
+++ component/script.akeebasubs.php
@@ -398,11 +398,11 @@ class Com_AkeebasubsInstallerScript
 		if (!defined('FOF_INCLUDED'))
 		{
 			@include_once JPATH_LIBRARIES . '/fof/include.php';
+		}
 
-			if (defined('FOF_INCLUDED'))
-			{
-				FOFPlatform::getInstance()->clearCache();
-			}
+		if (defined('FOF_INCLUDED'))
+		{
+			FOFPlatform::getInstance()->clearCache();
 		}
 	}
 
