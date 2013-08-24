ff29f43991ccae21562871173ff1edcbed3618a2
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2013-08-24 19:20:38 +0300

    Adding wizardstep configuration variable

--- component/backend/controllers/cpanels.php
+++ component/backend/controllers/cpanels.php
@@ -45,8 +45,50 @@ class AkeebasubsControllerCpanels extends FOFController
 		$params = new JRegistry();
 		$params->loadString($rawparams, 'JSON');
 
-		// Set the displayphpwarning parameter to 0
+		// Set the show2copromo parameter to 0
 		$params->set('show2copromo', 0);
+
+		// Save the component parameters
+		$data = $params->toString('JSON');
+		$sql = $db->getQuery(true)
+			->update($db->qn('#__extensions'))
+			->set($db->qn('params').' = '.$db->q($data))
+			->where($db->qn('type').' = '.$db->q('component'))
+			->where($db->qn('element').' = '.$db->q('com_akeebasubs'));
+
+		$db->setQuery($sql);
+		$db->execute();
+
+		// Redirect back to the control panel
+		$url = '';
+		$returnurl = $this->input->getBase64('returnurl', '');
+		if(!empty($returnurl)) {
+			$url = base64_decode($returnurl);
+		}
+		if(empty($url)) {
+			$url = JURI::base().'index.php?option=com_akeebasubs';
+		}
+		$this->setRedirect($url);
+	}
+
+	public function wizardstep()
+	{
+		$wizardstep = (int)$this->input->getInt('wizardstep', 0);
+
+		// Fetch the component parameters
+		$db = JFactory::getDbo();
+		$sql = $db->getQuery(true)
+			->select($db->qn('params'))
+			->from($db->qn('#__extensions'))
+			->where($db->qn('type').' = '.$db->q('component'))
+			->where($db->qn('element').' = '.$db->q('com_akeebasubs'));
+		$db->setQuery($sql);
+		$rawparams = $db->loadResult();
+		$params = new JRegistry();
+		$params->loadString($rawparams, 'JSON');
+
+		// Set the wzardstep parameter to whatever we were told to
+		$params->set('wizardstep', $wizardstep);
 
 		// Save the component parameters
 		$data = $params->toString('JSON');
