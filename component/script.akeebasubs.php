11f9c542fe761aea52932cf6bddca2f88aa0fe75
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2013-08-27 12:54:29 +0300

    Installation broken when using Discover

--- component/script.akeebasubs.php
+++ component/script.akeebasubs.php
@@ -352,9 +352,12 @@ class Com_AkeebasubsInstallerScript
 		}
 
 		// Bugfix for "Can not build admin menus"
-		if(in_array($type, array('install','discover_install'))) {
+		if(in_array($type, array('install')))
+		{
 			$this->_bugfixDBFunctionReturnedNoError();
-		} else {
+		}
+		elseif ($type != 'discover_install')
+		{
 			$this->_bugfixCantBuildAdminMenus();
 			$this->_fixBrokenSQLUpdates($parent);
 			$this->_fixSchemaVersion();
