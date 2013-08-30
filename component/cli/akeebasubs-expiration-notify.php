134e94b909619cfe05270bcc85e264534768389d
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2013-08-30 19:14:06 +0300

    Minimum PHP version detection in CLI scripts

--- component/cli/akeebasubs-expiration-notify.php
+++ component/cli/akeebasubs-expiration-notify.php
@@ -26,6 +26,56 @@ define('_JEXEC', 1);
 // Required by the CMS
 define('DS', DIRECTORY_SEPARATOR);
 
+$minphp = '5.3.1';
+if (version_compare(PHP_VERSION, $minphp, 'lt'))
+{
+	$curversion = PHP_VERSION;
+	$bindir = PHP_BINDIR;
+	echo <<< ENDWARNING
+================================================================================
+WARNING! Incompatible PHP version $curversion
+================================================================================
+
+This CRON script must be run using PHP version $minphp or later. Your server is
+currently using a much older version which would cause this script to crash. As
+a result we have aborted execution of the script. Please contact your host and
+ask them for the correct path to the PHP CLI binary for PHP $minphp or later, then
+edit your CRON job and replace your current path to PHP with the one your host
+gave you.
+
+For your information, the current PHP version information is as follows.
+
+PATH:    $bindir
+VERSION: $curversion
+
+Further clarifications:
+
+1. There is absolutely no possible way that you are receiving this warning in
+   error. We are using the PHP_VERSION constant to detect the PHP version you
+   are currently using. This is what PHP itself reports as its own version. It
+   simply cannot lie.
+
+2. Even though your *site* may be running in a higher PHP version that the one
+   reported above, your CRON scripts will most likely not be running under it.
+   This has to do with the fact that your site DOES NOT run under the command
+   line and there are different executable files (binaries) for the web and
+   command line versions of PHP.
+
+3. Please note that you MUST NOT ask us for support about this error. We cannot
+   possibly know the correct path to the PHP CLI binary as we have not set up
+   your server. Your host must know and give that information.
+
+4. The latest published versions of PHP can be found at http://www.php.net/
+   Any older version is considered insecure and must NOT be used on a live
+   server. If your server uses a much older version of PHP than that please
+   notify them that their servers are insecure and in need of an update.
+
+This script will now terminate. Goodbye.
+
+ENDWARNING;
+	die();
+}
+
 // Load system defines
 if (file_exists(__DIR__ . '/defines.php'))
 {
