201d01dc97b7574ce5fd200438374adbd2ed7e2c
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2014-01-04 09:49:14 +0200

    Beggining The Great Purge of 2014

--- plugins/akeebasubs/projectfork4/projectfork4/uninstall.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE IF EXISTS `#__akeebasubs_pf4_projects`;
--- plugins/akpayment/googlecheckout/googlecheckout/form.php
+++
@@ -1,5 +0,0 @@
-<?php defined('_JEXEC') or die(); ?>
-
-<h3><?php echo JText::_('PLG_AKPAYMENT_GOOGLECHECKOUT_MSG_READYTOCHECKOUT') ?></h3>
-<p><?php echo JText::_('PLG_AKPAYMENT_GOOGLECHECKOUT_MSG_INFO') ?></p>
-<?php echo @$data->cart->CheckoutButtonCode("SMALL"); ?>
