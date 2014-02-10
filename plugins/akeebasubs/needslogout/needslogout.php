6f6040ea531db957adb4e48f1d78154c7fb58f6c
Davide Tampellini <tampe125@gmail.com>
2014-02-10 11:15:52 +0100

    First implementation gh-17

---
+++ plugins/akeebasubs/needslogout/needslogout.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package		akeebasubs
+ * @copyright	Copyright (c)2010-2014 Nicholas K. Dionysopoulos / AkeebaBackup.com
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html> or later
+ */
+
+defined('_JEXEC') or die();
+
+class plgAkeebasubsNeedslogout extends JPlugin
+{
+	/**
+	 * Public constructor. Overridden to load the language strings.
+	 */
+	public function __construct(& $subject, $config = array())
+	{
+		if(!is_object($config['params'])) {
+			JLoader::import('joomla.registry.registry');
+			$config['params'] = new JRegistry($config['params']);
+		}
+
+		parent::__construct($subject, $config);
+	}
+
+	/**
+	 * Called whenever a subscription is modified. Namely, when its enabled status,
+	 * payment status or valid from/to dates are changed.
+	 */
+	public function onAKSubscriptionChange($row, $info)
+	{
+
+	}
+}
