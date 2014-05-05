4f783dd1acec42731c5095b103468fe3b0d426a4
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2014-05-05 20:27:53 +0300

    Removing obsolete files

--- build/old_utils/remove-index-html.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-find component -type f -name index.html -delete
-find modules -type f -name index.html -delete
-find plugins -type f -name index.html -delete
-find koowa -type f -name index.html -delete
