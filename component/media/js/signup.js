2fe3448c0d49779554dbd9fed47de3b933523911
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2015-01-01 23:03:30 +0200

    Changing copyright headers.

--- component/media/js/signup.js
+++ component/media/js/signup.js
@@ -1,6 +1,6 @@
 /**
  * @package        akeebasubs
- * @copyright    Copyright (c)2010-2014 Nicholas K. Dionysopoulos / AkeebaBackup.com
+ * @copyright    Copyright (c)2010-2015 Nicholas K. Dionysopoulos / AkeebaBackup.com
  * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html> or later
  */
 
