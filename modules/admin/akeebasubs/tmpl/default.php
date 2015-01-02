09cd7aeb74d0e8f9dab8076ed06cd7e35f44164c
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2015-01-02 19:44:56 +0200

    gh-91 Changing copyright headers as I go

--- modules/admin/akeebasubs/tmpl/default.php
+++ modules/admin/akeebasubs/tmpl/default.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package		mod_akeebasubs
- * @copyright 	Copyright (c) 2011-2013 Sander Potjer
+ * @copyright 	Copyright (c) 2011-2015 Sander Potjer
  * @license 	GNU General Public License version 3 or later
  */
 
