cbf8df958b437b15da33b043801a72dae67cafbf
Daniel Dimitrov <daniel@compojoom.com>
2015-01-03 15:55:02 +0100

    # fixed: Fatal error: Cannot use object of type JRegistry as array in the mod_aktaxcountry.php

--- modules/site/aktaxcountry/mod_aktaxcountry.php
+++ modules/site/aktaxcountry/mod_aktaxcountry.php
@@ -123,10 +123,10 @@ $default_option = JFactory::getSession()->get('country', null, 'mod_aktaxcountry
 if (empty($default_option))
 {
 	$taxHelper = F0FModel::getTmpInstance('Taxhelper', 'AkeebasubsModel');
-	$taxparams = $taxHelper->getTaxDefiningParameters;
+	$taxparams = $taxHelper->getTaxDefiningParameters();
 	$default_option = $taxparams['country'];
 
-	if ($params['vies'] && AkeebasubsHelperEuVATInfo::isEUVATCountry($params['country']))
+	if ($taxparams['vies'] && AkeebasubsHelperEuVATInfo::isEUVATCountry($taxparams['country']))
 	{
 		$default_option = 'EU-VIES';
 	}
@@ -137,5 +137,6 @@ if (empty($default_option))
 	$default_option = 'XX';
 }
 
+var_dump($default_option);
 // Load the layout file
 require_once JModuleHelper::getLayoutPath($module->module);
