730407b82b3223a97a1d5f4ed6d8cf0a9133af01
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2015-01-05 10:43:44 +0200

    Close gh-82 The expiration by week report wouldn't work on some sites due to timezone issues

--- component/backend/views/reports/tmpl/expirations_json.php
+++ component/backend/views/reports/tmpl/expirations_json.php
@@ -1,113 +1,115 @@
 <?php
 /**
- *  @package AkeebaSubs
- *  @copyright Copyright (c)2010-2015 Nicholas K. Dionysopoulos
- *  @license GNU General Public License version 3, or later
+ * @package   AkeebaSubs
+ * @copyright Copyright (c)2010-2015 Nicholas K. Dionysopoulos
+ * @license   GNU General Public License version 3, or later
  */
 
 defined('_JEXEC') or die;
 
-	$data     = array();
-	$response = array();
-	$levels   = array();
-	$labels   = array();
+$data = array();
+$response = array();
+$levelsWithExpirations = array();
+$labels = array();
 
-	$start  = $this->input->getString('start', date('Y-m-d', strtotime('last monday')));
-	$jStart = new JDate($start);
-	$start  = date('Y-m-d', strtotime('last monday', $jStart->toUnix()));
-	$jStart = new JDate($start);
+$start = $this->input->getString('start', gmdate('Y-m-d', strtotime('last monday')));
+$jStart = new JDate($start);
+$start = gmdate('Y-m-d', strtotime('last monday', $jStart->toUnix()));
+$jStart = new JDate($start);
 
-	$end     = date('Y-m-d' , strtotime('+16 weeks', $jStart->toUnix()));
-	$jEnd    = new JDate($end);
-	$endUnix = $jEnd->toUnix();
+$end = gmdate('Y-m-d', strtotime('+16 weeks', $jStart->toUnix()));
+$jEnd = new JDate($end);
+$endUnix = $jEnd->toUnix();
 
-	$subs = F0FModel::getTmpInstance('Subscriptions', 'AkeebasubsModel')
-				->expires_from($start)
-				->expires_to($end)
-				->groupbyweek(1)
-				->nojoins(1)
-				->paystate('C')
-				->getList(true);
+$subscriptionsExpiringInDateRange = F0FModel::getTmpInstance('Subscriptions', 'AkeebasubsModel')
+	->expires_from($start)
+	->expires_to($end)
+	->groupbyweek(1)
+	->nojoins(1)
+	->paystate('C')
+	->getList(true);
 
-	$titles = F0FModel::getTmpInstance('Levels', 'AkeebasubsModel')
-				->createIdLookup();
+$allSubscriptionLevelsDetails = F0FModel::getTmpInstance('Levels', 'AkeebasubsModel')
+	->createIdLookup();
 
-	// Get involved levels (aka series)
-	foreach($subs as $sub)
+// Get involved levels (aka series)
+foreach ($subscriptionsExpiringInDateRange as $sub)
+{
+	if (!in_array($sub->akeebasubs_level_id, $levelsWithExpirations))
 	{
-		if(!isset($levels[$sub->akeebasubs_level_id]))
+		$levelsWithExpirations[] = $sub->akeebasubs_level_id;
+	}
+}
+
+// Inject a dummy level, so I won't have errors on empty sets
+if (empty($levelsWithExpirations))
+{
+	$levelsWithExpirations[] = 0;
+	$response['hideLegend'] = true;
+}
+
+// Let's create empty weeks, for next 16 weeks, so I can have a nice chart
+$loopWeek = $jStart->toUnix();
+$startingWeekLabel = gmdate('Y-m-d', $loopWeek);
+$response['labels'][] = $startingWeekLabel;
+
+// Handle starting week
+foreach ($levelsWithExpirations as $levelID)
+{
+	if (!isset($data[$levelID][$startingWeekLabel]))
+	{
+		$data[$levelID][$startingWeekLabel] = array($startingWeekLabel, 0);
+	}
+}
+
+// Handle every other week
+while ($loopWeek <= $endUnix)
+{
+	$loopWeek = strtotime('+1 week', $loopWeek);
+	$weekLabel = gmdate('Y-m-d', $loopWeek);
+	$response['labels'][] = $weekLabel;
+
+	// Initialize the array with empty data, so I can have same length array for stacking
+	foreach ($levelsWithExpirations as $levelID)
+	{
+		if (!isset($data[$levelID][$weekLabel]))
 		{
-			$levels[$sub->akeebasubs_level_id] = 1;
+			$data[$levelID][$weekLabel] = array($weekLabel, 0);
 		}
 	}
+}
 
-	// Inject a dummy level, so I won't have errors on empty sets
-	if(!$levels)
-	{
-		$levels[0] = 1;
-		$response['hideLegend'] = true;
-	}
-
-	// Let's create empty weeks, for next 16 weeks, so I can have a nice chart
-	$loopWeek = $jStart->toUnix();
-	$response['labels'][] = date('Y-m-d', $loopWeek);
-
-	foreach($levels as $serie => $dummy)
-	{
-		if(!isset($data[$serie][date('Y-m-d', $loopWeek)]))
-		{
-			$data[$serie][date('Y-m-d', $loopWeek)] = array(date('Y-m-d', $loopWeek), 0);
-		}
-	}
-
-
-	while($loopWeek <= $endUnix)
-	{
-		$loopWeek = strtotime('+1 week', $loopWeek);
-		$date     = date('Y-m-d', $loopWeek);
-		$response['labels'][] = $date;
-
-		// Initialize the array with empty data, so I can have same length array for stacking
-		foreach($levels as $serie => $dummy)
-		{
-			if(!isset($data[$serie][$date]))
-			{
-				$data[$serie][$date] = array($date, 0);
-			}
-		}
-	}
-
-	foreach($subs as $sub)
+if (count($subscriptionsExpiringInDateRange))
+{
+	foreach ($subscriptionsExpiringInDateRange as $sub)
 	{
 		$year = substr($sub->yearweek, 0, 4);
 		$week = substr($sub->yearweek, -2, 2);
-		$date = date('Y-m-d', strtotime($year.'W'.$week));
+		$weekLabel = gmdate('Y-m-d', strtotime($year . 'W' . $week));
 
-		$data[$sub->akeebasubs_level_id][$date] = array($date, (int) $sub->subs);
+		$data[$sub->akeebasubs_level_id][$weekLabel] = array($weekLabel, (int)$sub->subs);
 	}
 
 	ksort($data);
-	ksort($levels);
+	asort($levelsWithExpirations);
 
-	if($subs)
+	foreach ($levelsWithExpirations as $level)
 	{
-		foreach($levels as $level => $dummy)
-		{
-			$response['seriesLabel'][] = array('label' => $titles[$level]->title);
-		}
+		$response['seriesLabel'][] = array('label' => $allSubscriptionLevelsDetails[$level]->title);
+	}
+}
+
+// jqplot doesn't like associative arrays
+foreach ($data as $allSeries)
+{
+	$temp = array();
+
+	foreach ($allSeries as $dataForThisSeries)
+	{
+		$temp[] = $dataForThisSeries;
 	}
 
-	// jqplot doesn't like associative arrays
-	foreach($data as $series)
-	{
-		$temp = array();
+	$response['data'][] = $temp;
+}
 
-		foreach($series as $serie)
-		{
-			$temp[] = $serie;
-		}
-
-		$response['data'][] = $temp;
-	}
-
-	echo json_encode(($response));
+echo json_encode($response);
