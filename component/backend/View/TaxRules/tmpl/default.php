99439a65d71b3637ce1d78ea6cb9a9a2d21abbba
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2015-03-06 17:29:27 +0200

    Wrong link

--- component/backend/View/TaxRules/tmpl/default.php
+++ component/backend/View/TaxRules/tmpl/default.php
@@ -10,7 +10,7 @@ defined('_JEXEC') or die();
 ?>
 <div class="row-fluid j-toggle-main j-toggle-transition span12">
 	<div class="span12">
-		<a href="index.php?option=com_akeebasubs&view=taxconfigs" class="btn btn-primary">
+		<a href="index.php?option=com_akeebasubs&view=TaxConfigs" class="btn btn-primary">
 			<i class="icon-white icon-wand"></i>
 			<?php echo JText::_('COM_AKEEBASUBS_TITLE_TAXCONFIGS') ?>
 		</a>
