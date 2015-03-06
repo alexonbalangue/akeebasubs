9ebbacf6337ce10bee4ca4473bc248f8ab939165
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2015-03-06 12:49:45 +0200

    Implemented Relations view

---
+++ component/backend/View/Relations/tmpl/form.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ *  @package AkeebaSubs
+ *  @copyright Copyright (c)2010-2015 Nicholas K. Dionysopoulos
+ *  @license GNU General Public License version 3, or later
+ */
+
+// Protect from unauthorized access
+defined('_JEXEC') or die();
+
+JHtml::_('behavior.tooltip');
+JHtml::_('behavior.modal');
+
+$script = <<< JS
+(function($) {
+	$(document).ready(function(){
+		akeebasubs_relations_mode_onChange();
+	});
+})(jQuery);
+
+	function akeebasubs_relations_mode_onChange()
+	{
+		(function($) {
+			var mode = $('#mode').val();
+			$('#akeebasubs-relations-fixed').css('display','none');
+			$('#akeebasubs-relations-flexi').css('display','none');
+			if(mode == 'fixed')
+			{
+				$('#akeebasubs-relations-fixed').css('display','block');
+			}
+			else if(mode == 'flexi')
+			{
+				$('#akeebasubs-relations-flexi').css('display','block');
+			}
+		})(jQuery);
+	}
+JS;
+
+
+JFactory::getDocument()->addScriptDeclaration($script);
+
+/** @var $this \FOF30\View\DataView\Form */
+
+echo $this->getRenderedForm();
