fc6b0ccc27ea0d9e962383c6e1ccd12cdce11ac0
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2015-03-07 15:14:29 +0200

    Invoices: custom form fields

---
+++ component/backend/Form/Field/InvoiceActions.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * @package   AkeebaSubs
+ * @copyright Copyright (c)2010-2015 Nicholas K. Dionysopoulos
+ * @license   GNU General Public License version 3, or later
+ */
+
+namespace Akeeba\Subscriptions\Admin\Form\Field;
+
+use Akeeba\Subscriptions\Admin\Model\Invoices;
+use FOF30\Form\Field\Text;
+use JText;
+
+defined('_JEXEC') or die;
+
+class InvoiceActions extends Text
+{
+	/** @var Invoices */
+	public $item = null;
+
+	public function getRepeatable()
+	{
+		static $extensions = null;
+
+		if (is_null($extensions))
+		{
+			/** @var Invoices $model */
+			$model = $this->form->getModel();
+
+			$extensions = $model->getExtensions();
+		}
+
+		$html = '';
+
+		if ($this->item->extension == 'akeebasubs')
+		{
+			$html .= '<a href="index.php?option=com_akeebasubs&view=Invoices&task=read&id=' .
+				htmlspecialchars($this->item->akeebasubs_subscription_id, ENT_COMPAT, 'UTF-8') .
+				'&tmpl=component" class="btn btn-info modal" rel="{handler: \'iframe\', size: {x: 800, y: 500}}" title="' .
+				JText::_('COM_AKEEBASUBS_INVOICES_ACTION_PREVIEW') . '"><span class="icon icon-file icon-white"></span></a>' .
+				"\n";
+			$html .= '<a href="index.php?option=com_akeebasubs&view=Invoices&task=download&id=' .
+				htmlspecialchars($this->item->akeebasubs_subscription_id, ENT_COMPAT, 'UTF-8') .
+				'" class="btn btn-primary" title="' .
+				JText::_('COM_AKEEBASUBS_INVOICES_ACTION_DOWNLOAD')
+				. '"><span class="icon icon-download-alt icon-white"></span></a>' . "\n";
+			$html .= '<a href="index.php?option=com_akeebasubs&view=Invoices&task=send&id=' .
+				htmlentities($this->item->akeebasubs_subscription_id, ENT_COMPAT, 'UTF-8') .
+				'" class="btn btn-success" title="' .
+				JText::_('COM_AKEEBASUBS_INVOICES_ACTION_RESEND') .
+				'"><span class="icon icon-envelope icon-white"></span></a>'
+				. "\n";
+
+			$db = \JFactory::getDbo();
+			if (empty($this->item->sent_on) || ($this->item->sent_on == $db->getNullDate()))
+			{
+				$html .= '<span class="label"><span class="icon icon-white icon-warning-sign"></span>' .
+					JText::_('COM_AKEEBASUBS_INVOICES_LBL_NOTSENT') . '</span>' . "\n";
+			}
+			else
+			{
+				$html .= '<span class="label label-success"><span class="icon icon-white icon-ok"></span>' .
+					JText::_('COM_AKEEBASUBS_INVOICES_LBL_SENT') . '</span>' . "\n";
+			}
+
+			$html .= '<a href="index.php?option=com_akeebasubs&view=Invoices&task=generate&id=' .
+				htmlentities($this->item->akeebasubs_subscription_id, ENT_COMPAT, 'UTF-8') .
+				'" class="btn btn-mini btn-warning" title="' .
+				JText::_('COM_AKEEBASUBS_INVOICES_ACTION_REGENERATE') .
+				'"><span class="icon icon-retweet icon-white"></span></a>'
+				. "\n";
+		}
+		elseif(array_key_exists($this->item->extension, $extensions))
+		{
+			$html .= '<a class="btn" href="' .
+				sprintf($extensions[$this->item->extension]['backendurl'], $this->item->invoice_no) .
+				'"><span class="icon icon-share-alt"></span>' .
+				JText::_('COM_AKEEBASUBS_INVOICES_LBL_OPENEXTERNAL') .
+				'</a>' . "\n";
+		}
+		else
+		{
+			$html .= '<span class="label">' . JText::_('COM_AKEEBASUBS_INVOICES_LBL_NOACTIONS') . '</span>' . "\n";
+		}
+
+		return $html;
+	}
+}
---
+++ component/backend/Form/Field/InvoiceNumber.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package   AkeebaSubs
+ * @copyright Copyright (c)2010-2015 Nicholas K. Dionysopoulos
+ * @license   GNU General Public License version 3, or later
+ */
+
+namespace Akeeba\Subscriptions\Admin\Form\Field;
+
+use Akeeba\Subscriptions\Admin\Helper\ComponentParams;
+use Akeeba\Subscriptions\Admin\Model\Invoices;
+use FOF30\Form\Field\Text;
+use JText;
+
+defined('_JEXEC') or die;
+
+class InvoiceNumber extends Text
+{
+	public function getRepeatable()
+	{
+		static $invoicetemplates = null;
+
+		if (is_null($invoicetemplates))
+		{
+			/** @var Invoices $model */
+			$model = $this->form->getModel();
+
+			$invoicetemplates = $model->getInvoiceTemplateNames();
+		}
+
+		$value = '';
+
+		if (
+			($this->item->extension == 'akeebasubs')
+			&& array_key_exists($this->item->akeebasubs_invoicetemplate_id, $invoicetemplates)
+		)
+		{
+			$value .= '<span class="label label-info">' . $invoicetemplates[$this->item->akeebasubs_invoicetemplate_id]->title . '</span> ';
+		}
+
+		if (!empty($this->item->display_number))
+		{
+			$value .= htmlentities($this->item->display_number, ENT_COMPAT, 'UTF-8');
+		}
+		else
+		{
+			$value .= htmlentities($this->item->invoice_no, ENT_COMPAT, 'UTF-8');
+		}
+
+		return $value;
+	}
+}
