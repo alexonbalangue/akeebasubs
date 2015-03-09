d64e70c928e3e6d2cec185ad993fff50cd079a14
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2015-03-09 20:50:46 +0200

    Subscriptions view, browse: payment status display

---
+++ component/backend/Form/Field/PaymentStatus.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package   AkeebaSubs
+ * @copyright Copyright (c)2010-2015 Nicholas K. Dionysopoulos
+ * @license   GNU General Public License version 3, or later
+ */
+
+namespace Akeeba\Subscriptions\Admin\Form\Field;
+
+use Akeeba\Subscriptions\Admin\Helper\ComponentParams;
+use Akeeba\Subscriptions\Admin\Model\Invoices;
+use FOF30\Form\Field\Text;
+use JText;
+
+defined('_JEXEC') or die;
+
+class PaymentStatus extends Text
+{
+	public function getRepeatable()
+	{
+		$state = $this->value;
+		$stateLower = strtolower($state);
+		$stateLabel = htmlspecialchars(JText::_('COM_AKEEBASUBS_SUBSCRIPTION_STATE_' . $this->value));
+
+		$processor = htmlspecialchars($this->item->processor);
+		$processorKey = htmlspecialchars($this->item->processor_key);
+
+		return <<< HTML
+<span class="akeebasubs-payment akeebasubs-payment-$stateLower hasTip"
+	title="$stateLabel::$processor &bull; $processorKey">
+</span>
+
+<span class="akeebasubs-subscription-processor">
+	$processor
+</span>
+HTML;
+
+	}
+}
