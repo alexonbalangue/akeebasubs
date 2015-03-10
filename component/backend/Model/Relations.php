385d57beea39fbf191fa3687310098b643d7130b
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2015-03-11 00:37:21 +0200

    Pull in models to the front-end

---
+++ component/backend/Model/Relations.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package   AkeebaSubs
+ * @copyright Copyright (c)2010-2015 Nicholas K. Dionysopoulos
+ * @license   GNU General Public License version 3, or later
+ */
+
+namespace Akeeba\Subscriptions\Admin\Model;
+
+defined('_JEXEC') or die;
+
+use FOF30\Model\DataModel;
+
+/**
+ * Model for Subscription Level Relations
+ */
+class Relations extends DataModel
+{
+}
---
+++ component/frontend/Model/Users.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package   AkeebaSubs
+ * @copyright Copyright (c)2010-2015 Nicholas K. Dionysopoulos
+ * @license   GNU General Public License version 3, or later
+ */
+
+namespace Akeeba\Subscriptions\Site\Model;
+
+defined('_JEXEC') or die;
+
+/**
+ * This model extends the back-end model, pulling it into the frontend without duplicating the code
+ */
+class Users extends \Akeeba\Subscriptions\Admin\Model\APICoupons
+{
+}
