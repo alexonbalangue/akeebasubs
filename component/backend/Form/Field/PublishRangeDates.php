51f6b8fc9362f3b9e5749eeb9e7b7f313de81e82
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2015-03-10 12:00:29 +0200

    Subscriptions view, browse: valid from/to display

---
+++ component/backend/Form/Field/PublishRangeDates.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package   AkeebaSubs
+ * @copyright Copyright (c)2010-2015 Nicholas K. Dionysopoulos
+ * @license   GNU General Public License version 3, or later
+ */
+
+namespace Akeeba\Subscriptions\Admin\Form\Field;
+
+use Akeeba\Subscriptions\Admin\Controller\Subscriptions;
+use Akeeba\Subscriptions\Admin\Helper\Format;
+use FOF30\Form\Field\Text;
+
+defined('_JEXEC') or die;
+
+class PublishRangeDates extends Text
+{
+	public function getRepeatable()
+	{
+		/** @var Subscriptions $subscription */
+		$subscription = $this->item;
+
+		$publish_up = Format::date($subscription->publish_up, 'Y-m-d H:i');
+		$publish_down = Format::date($subscription->publish_down, 'Y-m-d H:i');
+
+		return <<< HTML
+<div class="akeebasubs-susbcription-publishup">
+	$publish_up
+</div>
+<div class="akeebasubs-susbcription-publishdown">
+	$publish_down
+</div>
+HTML;
+
+	}
+}
