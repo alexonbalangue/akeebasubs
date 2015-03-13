2f075f5de5e8921f7ec3d2afda7bd45ab25070b3
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2015-03-13 10:16:05 +0200

    Front-end: Subscribe controller

---
+++ component/frontend/Controller/Subscribe.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * @package   AkeebaSubs
+ * @copyright Copyright (c)2010-2015 Nicholas K. Dionysopoulos
+ * @license   GNU General Public License version 3, or later
+ */
+
+namespace Akeeba\Subscriptions\Site\Controller;
+
+defined('_JEXEC') or die;
+
+use Akeeba\Subscriptions\Admin\Controller\Mixin;
+use Akeeba\Subscriptions\Site\Model\Subscribe as ModelSubscribe;
+use FOF30\Container\Container;
+use FOF30\Controller\Controller;
+use FOF30\Controller\Exception\ItemNotFound;
+use FOF30\View\Exception\AccessForbidden;
+
+class Subscribe extends Controller
+{
+	use Mixin\PredefinedTaskList;
+
+	/**
+	 * Overridden. Limit the tasks we're allowed to execute.
+	 *
+	 * @param   Container $container
+	 * @param   array     $config
+	 */
+	public function __construct(Container $container, array $config = array())
+	{
+		$config['modelName'] = 'Subscribe';
+
+		parent::__construct($container, $config);
+
+		$this->predefinedTaskList = ['subscribe'];
+
+		$this->cacheableTasks = [];
+	}
+
+	/**
+	 * Handles the POST of the subscription form. It will try to create a new subscription (and user, if the POST came
+	 * from a guest and there's valid information).
+	 *
+	 * @return  void
+	 */
+	public function subscribe()
+	{
+		// Load the models
+
+		/** @var ModelSubscribe $model */
+		$model = $this->getModel();
+
+		/** @var \Akeeba\Subscriptions\Site\Model\Levels $levelsModel */
+		$levelsModel = $this->getModel('Levels');
+
+		// Load the id and slug. Either one defines which level we shall load
+		$id = $model->getState('id', 0, 'int');
+		$slug = $model->getState('slug', null, 'string');
+
+		// If the ID is not set but slug is let's try to find the level by slug
+		if (!$id && $slug)
+		{
+			// Note: do note replace $item with $levelsModel or the view won't see the loaded record because of how
+			// references work in PHP.
+			$item = $levelsModel
+				->id(0)
+				->slug($slug)
+				->firstOrNew();
+
+			$id = $item->getId();
+		}
+
+		// If we do not have a valid level ID throw a 404
+		if (!$id)
+		{
+			throw new ItemNotFound(\JText::_($slug), 404);
+		}
+
+		// Load the level
+		$level = $levelsModel->find($id);
+
+		// If the level is marked as only_once we need to make sure we're allowed to access it - TODO Shouldn't I be listing subscriptions?
+		if ($level->only_once)
+		{
+			$levels = $levelsModel
+				->id(0)
+				->slug($level->slug)
+				->only_once(1)
+				->get(true);
+
+			if (!$levelsModel->count())
+			{
+				// User trying to renew a level which is marked as only_once
+				throw new AccessForbidden;
+			}
+		}
+
+		// Check the Joomla! View Access Level for this subscription level
+		$accessLevels = \JFactory::getUser()->getAuthorisedViewLevels();
+
+		if (!in_array($level->access, $accessLevels))
+		{
+			// User trying to subscribe to a level he doesn't have access to
+			throw new AccessForbidden;
+		}
+
+		// Try to create a new subscription record
+		$model->setState('id', $id);
+
+		$result = $model->createNewSubscription();
+
+		// Did we fail to create a new subscription?
+		if (!$result)
+		{
+			$url = str_replace('&amp;', '&', \JRoute::_('index.php?option=com_akeebasubs&view=Level&layout=default&slug=' . $model->slug));
+			$msg = \JText::_('COM_AKEEBASUBS_LEVEL_ERR_VALIDATIONOVERALL');
+
+			$this->setRedirect($url, $msg, 'error');
+
+			return;
+		}
+
+		// Set up and display the view
+		$view = $this->getView();
+
+		$view->setLayout('form');
+		$view->form = $model->getPaymentForm();
+		$view->setDefaultModelName('Subscribe');
+		$view->setModel('Subscribe', $model);
+
+		$view->display();
+	}
+}
