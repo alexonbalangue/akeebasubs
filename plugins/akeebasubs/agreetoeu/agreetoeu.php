79965908f1cba582d2297d9437546a3b6dc2107b
photodude <photodude@users.noreply.github.com>
2015-03-24 14:28:34 -0600

    If $custom is null, or is not an array... make $custom an array

--- plugins/akeebasubs/agreetoeu/agreetoeu.php
+++ plugins/akeebasubs/agreetoeu/agreetoeu.php
@@ -209,6 +209,17 @@ JS;
 
 		$custom = $data->custom;
 
+		// If $custom is null, or is not an array... make $custom an array
+		if (is_null($custom))
+		{
+			$custom = array();
+		}
+
+		if (!is_array($custom))
+ 		{
+ 			$custom = (array) $custom;
+ 		}
+
 		if (!array_key_exists('confirm_informed', $custom))
 		{
 			$custom['confirm_informed'] = 0;
