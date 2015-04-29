91c816fa4d8556e341d56b41d4f0b4a8314d4f5b
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2015-04-29 13:32:04 +0300

    Refactor validator tests

--- tests/Site/Model/Subscribe/Validation/Name.php
+++ tests/Site/Model/Subscribe/Validation/Name.php
@@ -7,49 +7,20 @@
 
 namespace Akeeba\Subscriptions\Tests\Site\Model\Subscribe\Validation;
 
-use Akeeba\Subscriptions\Site\Model\Subscribe\StateData;
-use Akeeba\Subscriptions\Site\Model\Subscribe\ValidatorFactory;
-use FOF30\Container\Container;
-use JUser;
+use Akeeba\Subscriptions\Tests\Stubs\ValidatorTest;
 
 /**
  * Test the Name validator
  *
  * @covers Akeeba\Subscriptions\Site\Model\Subscribe\Validation\Name
  */
-class Name extends \PHPUnit_Framework_TestCase
+class Name extends ValidatorTest
 {
-	/** @var   ValidatorFactory  The validator factory for this class */
-	public static $factory = null;
-
-	/** @var   Container  The container of the component */
-	public static $container = null;
-
-	/** @var   StateData  The state data we're operating on */
-	public static $state = null;
-
-	/** @var   JUser  The currently active Joomla! user object */
-	public static $jUser = null;
-
-	/**
-	 * Set up the static objects before the class is created
-	 */
 	public static function setUpBeforeClass()
 	{
-		if (is_null(static::$container))
-		{
-			static::$container = Container::getInstance('com_akeebasubs');
-		}
+		self::$validatorType = 'Name';
 
-		if (is_null(static::$jUser))
-		{
-			static::$jUser = new JUser();
-		}
-
-		$model = static::$container->factory->model('Subscribe');
-		static::$state = new StateData($model);
-
-		static::$factory = new ValidatorFactory(static::$container, static::$state, static::$jUser);
+		parent::setUpBeforeClass();
 	}
 
 	public function getTestData()
@@ -93,16 +64,8 @@ class Name extends \PHPUnit_Framework_TestCase
 	 */
 	public function testGetValidationResult($state, $expected, $message)
 	{
-		static::$state->reset();
-
-		foreach ($state as $k => $v)
-		{
-			static::$state->$k = $v;
-		}
-
-		$validator = static::$factory->getValidator('Name');
-		$actual = $validator->execute(true);
-
-		$this->assertEquals($expected, $actual, $message);
+		parent::testGetValidationResult($state, $expected, $message);
 	}
+
+
 }
