2c16b19ebd97a742b9e7aee71b94e7be9d19f9bc
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2015-07-05 00:04:12 +0300

    Adding foreign exchange rate helper class

---
+++ Tests/Admin/Helper/ForexTest.php
@@ -0,0 +1,345 @@
+<?php
+/**
+ * @package   AkeebaSubs
+ * @copyright Copyright (c)2010-2015 Nicholas K. Dionysopoulos
+ * @license   GNU General Public License version 3, or later
+ */
+
+namespace Akeeba\Subscriptions\Tests\Admin\Helper;
+
+use FOF30\Container\Container;
+use Akeeba\Subscriptions\Admin\Helper\Forex;
+
+class ForexTest extends \PHPUnit_Framework_TestCase
+{
+	/** @var   Container  The container of the component */
+	public static $container = null;
+
+	/**
+	 * Set up the static objects before the class is created
+	 */
+	public static function setUpBeforeClass()
+	{
+		if (is_null(static::$container))
+		{
+			static::$container = Container::getInstance('com_akeebasubs', [
+				'platformClass' => 'Akeeba\\Subscriptions\\Tests\\Stubs\\CustomPlatform'
+			]);
+		}
+	}
+
+	/**
+	 * @covers Akeeba\Subscriptions\Admin\Helper\Forex::reloadCurrencyData
+	 */
+	public function testReloadCurrencyData_FirstRun()
+	{
+		// Reset the current cache
+		$reflectionClass = new \ReflectionClass('Akeeba\Subscriptions\Admin\Helper\Forex');
+		$refCountryCurrency = $reflectionClass->getProperty('countryCurrency');
+		$refCountryCurrency->setAccessible(true);
+		$refCountryCurrency->setValue([]);
+		$refCurrencySymbols = $reflectionClass->getProperty('currencySymbols');
+		$refCurrencySymbols->setAccessible(true);
+		$refCurrencySymbols->setValue([]);
+
+		Forex::reloadCurrencyData(false, self::$container);
+
+		$actual = $refCountryCurrency->getValue();
+		$this->assertNotEmpty($actual, 'Forex::$countryCurrency should not be empty on first load');
+		$this->assertNotEmpty($refCurrencySymbols->getValue(), 'Forex::$currencySymbols should not be empty  on first load');
+	}
+
+	/**
+	 * @covers Akeeba\Subscriptions\Admin\Helper\Forex::reloadCurrencyData
+	 * @depends testReloadCurrencyData_FirstRun
+	 */
+	public function testReloadCurrencyData_NoReload()
+	{
+		$reflectionClass = new \ReflectionClass('Akeeba\Subscriptions\Admin\Helper\Forex');
+		$refCountryCurrency = $reflectionClass->getProperty('countryCurrency');
+		$refCountryCurrency->setAccessible(true);
+		$refCurrencySymbols = $reflectionClass->getProperty('currencySymbols');
+		$refCurrencySymbols->setAccessible(true);
+
+		// Get current values
+		$valCountry = $refCountryCurrency->getValue();
+		$valSymbols = $refCurrencySymbols->getValue();
+
+		// Make sure they're not empty
+		$this->assertNotEmpty($valCountry, 'Test Self-check: Forex::$countryCurrency should not be empty');
+		$this->assertNotEmpty($valSymbols, 'Test Self-check: Forex::$currencySymbols should not be empty');
+
+		// Set special test data
+		$valCountry['XX'] = 'TEST';
+		$refCountryCurrency->setValue($valCountry);
+
+		$valSymbols['XXX'] = 'TEST';
+		$refCurrencySymbols->setValue($valSymbols);
+
+		Forex::reloadCurrencyData(false, self::$container);
+
+		$valCountry = $refCountryCurrency->getValue();
+		$valSymbols = $refCurrencySymbols->getValue();
+
+		$this->assertNotEmpty($valCountry, 'Forex::$countryCurrency should not be empty');
+		$this->assertNotEmpty($valSymbols, 'Forex::$currencySymbols should not be empty');
+
+		$this->assertArrayHasKey('XX', $valCountry, 'Forex::$countryCurrency must not be reloaded when it is already loaded');
+		$this->assertArrayHasKey('XXX', $valSymbols, 'Forex::$currencySymbols must not be reloaded when it is already loaded');
+	}
+
+	/**
+	 * @covers Akeeba\Subscriptions\Admin\Helper\Forex::reloadCurrencyData
+	 * @depends testReloadCurrencyData_NoReload
+	 */
+	public function testReloadCurrencyData_DoReload()
+	{
+		$reflectionClass = new \ReflectionClass('Akeeba\Subscriptions\Admin\Helper\Forex');
+		$refCountryCurrency = $reflectionClass->getProperty('countryCurrency');
+		$refCountryCurrency->setAccessible(true);
+		$refCurrencySymbols = $reflectionClass->getProperty('currencySymbols');
+		$refCurrencySymbols->setAccessible(true);
+
+		// Get current values
+		$valCountry = $refCountryCurrency->getValue();
+		$valSymbols = $refCurrencySymbols->getValue();
+
+		// Make sure they're not empty
+		$this->assertNotEmpty($valCountry, 'Test Self-check: Forex::$countryCurrency should not be empty');
+		$this->assertNotEmpty($valSymbols, 'Test Self-check: Forex::$currencySymbols should not be empty');
+
+		// Set special test data
+		$valCountry['XX'] = 'TEST';
+		$refCountryCurrency->setValue($valCountry);
+
+		$valSymbols['XXX'] = 'TEST';
+		$refCurrencySymbols->setValue($valSymbols);
+
+		Forex::reloadCurrencyData(true, self::$container);
+
+		$valCountry = $refCountryCurrency->getValue();
+		$valSymbols = $refCurrencySymbols->getValue();
+
+		$this->assertNotEmpty($valCountry, 'Forex::$countryCurrency should not be empty');
+		$this->assertNotEmpty($valSymbols, 'Forex::$currencySymbols should not be empty');
+
+		$this->assertArrayNotHasKey('XX', $valCountry, 'Forex::$countryCurrency must be reloaded when forced');
+		$this->assertArrayNotHasKey('XXX', $valSymbols, 'Forex::$currencySymbols must be reloaded when forced');
+	}
+
+	/**
+	 * @covers Akeeba\Subscriptions\Admin\Helper\Forex::testUpdateRates
+	 * @covers Akeeba\Subscriptions\Admin\Helper\Forex::shouldUpdate
+	 * @covers Akeeba\Subscriptions\Admin\Helper\Forex::setLastUpdateTimestamp
+	 * @covers Akeeba\Subscriptions\Admin\Helper\Forex::getLastUpdateTimestamp
+	 */
+	public function testUpdateRates()
+	{
+		$db = self::$container->db;
+
+		// Delete the last update timestamp key
+		$query = $db->getQuery(true)
+			->delete('#__akeeba_common')
+			->where($db->qn('key') . ' = ' . $db->q('akeebasubs_forex_update_timestamp'));
+		$db->setQuery($query)->execute();
+
+		// Delete the contents of the table
+		$db->truncateTable('#__akeebasubs_forexrates');
+
+		// Reset the current cache
+		$reflectionClass = new \ReflectionClass('Akeeba\Subscriptions\Admin\Helper\Forex');
+
+		$refRates = $reflectionClass->getProperty('rates');
+		$refRates->setAccessible(true);
+		$refRates->setValue([]);
+
+		// Set a local source
+		$refSourceUrl = $reflectionClass->getProperty('rateSourceUrl');
+		$refSourceUrl->setAccessible(true);
+		$refSourceUrl->setValue('file://' . realpath(__DIR__ . '/eurofxref-daily.xml'));
+
+		// ===== Test first load
+		Forex::updateRates(false, self::$container);
+
+		$rates = $refRates->getValue();
+		$this->assertNotEmpty($rates, 'The rates must be loaded from the source');
+		$this->assertEquals('1.1096', $rates['USD']['rate'], 'The rates must be read correctly from the source');
+
+		// ===== Test no reload
+		$rates['USD'] = array('rate' => 12.345, 'currency' => 'USD');
+		$refRates->setValue($rates);
+
+		Forex::updateRates(false, self::$container);
+
+		$rates = $refRates->getValue();
+		$this->assertEquals('12.345', $rates['USD']['rate'], 'The rates must not be reloaded if not forced and not expired');
+
+		// ===== Test force reload
+
+		Forex::updateRates(true, self::$container);
+
+		$rates = $refRates->getValue();
+		$this->assertNotEquals('12.345', $rates['USD']['rate'], 'The rates must be reloaded if forced');
+
+		// ===== Test no reload when up to date
+		$rates['USD'] = array('rate' => 12.345, 'currency' => 'USD');
+		$refRates->setValue($rates);
+
+		$o = (object)[
+			'key' => 'akeebasubs_forex_update_timestamp',
+			'value' => time() - 3600
+		];
+		$db->updateObject('#__akeeba_common', $o, 'key');
+
+		Forex::updateRates(false, self::$container);
+
+		$rates = $refRates->getValue();
+		$this->assertEquals('12.345', $rates['USD']['rate'], 'The rates must not be reloaded if not expired');
+
+		// ===== Test reload on expired
+		$o = (object)[
+			'key' => 'akeebasubs_forex_update_timestamp',
+			'value' => time() - 100000
+		];
+		$db->updateObject('#__akeeba_common', $o, 'key');
+
+		Forex::updateRates(true, self::$container);
+
+		$rates = $refRates->getValue();
+		$this->assertNotEquals('12.345', $rates['USD']['rate'], 'The rates must be reloaded if expired');
+	}
+
+	/**
+	 * @covers Akeeba\Subscriptions\Admin\Helper\Forex::reloadRates
+	 */
+	public function testReloadRates()
+	{
+		// Reset the current cache
+		$reflectionClass = new \ReflectionClass('Akeeba\Subscriptions\Admin\Helper\Forex');
+		$refRates = $reflectionClass->getProperty('rates');
+		$refRates->setAccessible(true);
+		$refRates->setValue([]);
+
+		// ==== Test first load
+		Forex::reloadRates(false, self::$container);
+
+		$rates = $refRates->getValue();
+		$this->assertNotEmpty($rates, 'Forex::$rates should not be empty on first load');
+
+		// ==== Test no reload when not forced
+		$rates['USD'] = array('rate' => 12.345, 'currency' => 'USD');
+		$refRates->setValue($rates);
+
+		Forex::reloadRates(false, self::$container);
+
+		$rates = $refRates->getValue();
+		$this->assertNotEquals('1.1096', $rates['USD']['rate'], 'The rates must not be reloaded when not empty');
+
+		// ==== Test reload when forced
+		Forex::reloadRates(true, self::$container);
+
+		$rates = $refRates->getValue();
+		$this->assertEquals('1.1096', $rates['USD']['rate'], 'The rates must be reloaded when forced');
+	}
+
+	/**
+	 * @covers Akeeba\Subscriptions\Admin\Helper\Forex::getCurrencySymbol
+	 * @depends testReloadRates
+	 *
+	 * @dataProvider Akeeba\Subscriptions\Tests\Admin\Helper\ForexTest::getTestGetCurrencySymbol
+	 */
+	public function testGetCurrencySymbol($currency, $symbol)
+	{
+		$actual = Forex::getCurrencySymbol($currency);
+
+		$this->assertEquals($symbol, $actual, "Symbol for $currency must be $symbol");
+	}
+
+	/**
+	 * @covers Akeeba\Subscriptions\Admin\Helper\Forex::convertFromEuro
+	 * @depends testReloadRates
+	 *
+	 * @dataProvider Akeeba\Subscriptions\Tests\Admin\Helper\ForexTest::getTestConvertFromEuro
+	 */
+	public function testConvertFromEuro($currency, $value, $expected)
+	{
+		$actual = Forex::convertFromEuro($currency, $value, self::$container);
+
+		$this->assertEquals($expected, $actual, "$value EUR to $currency must be $expected, not $actual");
+	}
+
+	/**
+	 * @covers Akeeba\Subscriptions\Admin\Helper\Forex::convertFromEuro
+	 * @depends testReloadRates
+	 *
+	 * @dataProvider Akeeba\Subscriptions\Tests\Admin\Helper\ForexTest::getTestConvertFromEuro
+	 */
+	public function testConvertToEuro($currency, $expected, $value)
+	{
+		$actual = Forex::convertToEuro($currency, $value, self::$container);
+
+		$this->assertEquals($expected, $actual, "$value EUR to $currency must be $expected, not $actual");
+	}
+
+	/**
+	 * @covers Akeeba\Subscriptions\Admin\Helper\Forex::testExhangeRate
+	 * @covers Akeeba\Subscriptions\Admin\Helper\Forex::convertCurrency
+	 * @depends testReloadRates
+	 *
+	 * @dataProvider Akeeba\Subscriptions\Tests\Admin\Helper\ForexTest::getTestExhangeRate
+	 */
+	public function testExhangeRate($from, $to, $expected, $exception)
+	{
+		try
+		{
+			$actual = Forex::exhangeRate($from, $to, self::$container);
+		}
+		catch (\InvalidArgumentException $e)
+		{
+			$actual = 0.0;
+
+			if (!$exception)
+			{
+				$this->fail('Unexpected exception');
+			}
+		}
+
+		$this->assertEquals($expected, $actual, "Exchange rate of $from to $to must be $expected, not $actual");
+	}
+
+	public function testConvertToLocal($country, $value, $expected)
+	{
+		// TODO Write this test
+		$this->markTestIncomplete('This test is not yet written');
+	}
+
+	public static function getTestExhangeRate()
+	{
+		return [
+			['EUR', 'USD', 1.1096, false],
+			['USD', 'EUR', 0.90122566690699, false],
+			['USD', 'AUD', 1.32903749098774, false],
+			['', 'USD', 0, true],
+			['USD', '', 0, true],
+			['', '', 0, true],
+		];
+	}
+
+	public static function getTestConvertFromEuro()
+	{
+		return [
+			['USD', 10, 11.096],
+			['BGN', 10, 19.558],
+			['NOK', 12.85, 113.7225],
+		];
+	}
+
+	public static function getTestGetCurrencySymbol()
+	{
+		return [
+			['EUR', '€'],
+			['USD', '$'],
+			['BGN', 'лв.'],
+		];
+	}
+}
---
+++ component/backend/Helper/Forex.php
@@ -0,0 +1,508 @@
+<?php
+/**
+ * @package   AkeebaSubs
+ * @copyright Copyright (c)2010-2015 Nicholas K. Dionysopoulos
+ * @license   GNU General Public License version 3, or later
+ */
+
+namespace Akeeba\Subscriptions\Admin\Helper;
+
+use FOF30\Container\Container;
+use FOF30\Download\Download;
+
+defined('_JEXEC') or die;
+
+/**
+ * Foreign exchange conversions using exchange rate data from the European Central Bank
+ */
+class Forex
+{
+	/**
+	 * The timestamp update key
+	 *
+	 * @var  string
+	 */
+	protected static $timestampKey = 'akeebasubs_forex_update_timestamp';
+
+	/**
+	 * The European Central Bank exchange rate data source
+	 *
+	 * @var  string
+	 */
+	protected static $rateSourceUrl = 'http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml';
+
+	/**
+	 * Cached foreign exchange rates quotes against Euro, indexed by currency code
+	 *
+	 * @var  array
+	 */
+	protected static $rates = [];
+
+	protected static $countryCurrency = [];
+
+	protected static $currencySymbols = [];
+
+	/**
+	 * Updates the exchange rates from the latest ECB data
+	 *
+	 * @param   bool       $force      Should I force an update (true) or decide based on latest update timestamp (false)?
+	 * @param   Container  $container  The container of the application we're runnign in
+	 *
+	 * @return  void
+	 *
+	 * @throws  \Exception If the download or the database operation fails
+	 */
+	public static function updateRates($force = false, Container $container = null)
+	{
+		if (!($container instanceof Container))
+		{
+			$container = Container::getInstance('com_akeebasubs');
+		}
+
+		// Should I update the rates? I will only do if forced or they have expired.
+		$shouldUpdate = true;
+
+		if (!$force)
+		{
+			$shouldUpdate = self::shouldUpdate($container);
+		}
+
+		if (!$shouldUpdate)
+		{
+			return;
+		}
+
+		// Update the timestamp
+		self::setLastUpdateTimestamp($container);
+
+		// Get the raw XML source
+		$download = new Download($container);
+
+		$xmlData = $download->getFromURL(self::$rateSourceUrl);
+
+		// Parse the data into an array
+		$rates = [];
+		$xml = new \SimpleXMLElement($xmlData);
+
+		foreach($xml->Cube->Cube->Cube as $rate)
+		{
+			$rates[] = (object)[
+				'akeebasubs_forexrate_id'	=> (string)$rate["currency"],
+				'rate'						=> (float)$rate["rate"]
+			];
+		}
+
+		// Clear the table and insert the new data, all wrapped inside a transaction
+		$db = $container->db;
+
+		$db->transactionStart();
+
+		try
+		{
+			// Can't use TRUNCATE TABLE because since MySQL 5.1 it's considered DDL, not DML, causing an implicit flush
+			// See http://stackoverflow.com/questions/5972364/mysql-truncate-table-within-transaction
+			$query = $db->getQuery(true)
+				->delete('#__akeebasubs_forexrates');
+			$db->setQuery($query)->execute();
+
+			foreach ($rates as $rateObject)
+			{
+				$db->insertObject('#__akeebasubs_forexrates', $rateObject);
+			}
+		}
+		catch (\Exception $e)
+		{
+			$db->transactionRollback();
+
+			throw $e;
+		}
+
+		$db->transactionCommit();
+
+		self::reloadRates(true, $container);
+	}
+
+	/**
+	 * Reload the exchange rates from the database
+	 *
+	 * @param   bool       $force      Should I force-reload the rates?
+	 * @param   Container  $container  The container of the application we're running in
+	 *
+	 * @return  void
+	 */
+	public static function reloadRates($force = false, Container $container = null)
+	{
+		if (!$force && !empty(self::$rates))
+		{
+			return;
+		}
+
+		if (!($container instanceof Container))
+		{
+			$container = Container::getInstance('com_akeebasubs');
+		}
+
+		$db = $container->db;
+
+		$query = $db->getQuery(true)
+			->select('*')
+			->from('#__akeebasubs_forexrates');
+
+		try
+		{
+			self::$rates = $db->setQuery($query)->loadAssocList('akeebasubs_forexrate_id');
+		}
+		catch (\Exception $e)
+		{
+			// Couldn't load rates? Do nothing.
+		}
+	}
+
+	/**
+	 * Reload the currency data from the database. This loads the country to currency mapping and the currency code to
+	 * currency symbol mapping.
+	 *
+	 * @param   bool      $force      Force reload this information? If false it will be loaded only if it's not loaded yet.
+	 * @param   Container $container  The container of the application we're running in
+	 *
+	 * @return  void
+	 */
+	public static function reloadCurrencyData($force = false, Container $container = null)
+	{
+		if (!$force && !empty(self::$countryCurrency))
+		{
+			return;
+		}
+
+		if (!($container instanceof Container))
+		{
+			$container = Container::getInstance('com_akeebasubs');
+		}
+
+		$db = $container->db;
+
+		$query = $db->getQuery(true)
+					->select('*')
+					->from('#__akeebasubs_countrycurrencies');
+
+		try
+		{
+			self::$countryCurrency = $db->setQuery($query)->loadAssocList('country');
+			self::$currencySymbols = [];
+
+			foreach (self::$countryCurrency as $country => $currencyInfo)
+			{
+				self::$currencySymbols[$currencyInfo['currency']] = $currencyInfo['symbol'];
+			}
+		}
+		catch (\Exception $e)
+		{
+			// Couldn't load rates? Do nothing.
+		}
+	}
+
+	/**
+	 * Converts a value expressed in the component's default currency (default: Euros) into the specified country's
+	 * local currency. The information is returned as a hashed array with the keys:
+	 * - currency  The currency code of that country, e.g. USD
+	 * - symbol    The currency symbol of that country, e.g. $
+	 * - value     The converted value
+	 * - rate      The exchange rate used
+	 *
+	 * @param   string     $country    The country which the data will be converted for, 2 letter ISO code, e.g. US
+	 * @param   float      $value      The value you want to convert
+	 * @param   Container  $container  The container of the application we're running in
+	 *
+	 * @return  array  Keys: currency (e.g. USD), symbol (e.g. $), value (converted), rate (exchange rate)
+	 */
+	public static function convertToLocal($country, $value, Container $container = null)
+	{
+		self::reloadCurrencyData(false, $container);
+
+		$defaultAkeebaSubsCurrency = ComponentParams::getParam('currency', 'EUR');
+
+		$defaultReturn = [
+			'currency' => $defaultAkeebaSubsCurrency,
+			'symbol'   => ComponentParams::getParam('currencysymbol', '€'),
+			'value'    => $value,
+			'rate'     => 1.00
+		];
+
+		$country = strtoupper($country);
+
+		if (!isset(self::$countryCurrency[$country]))
+		{
+			return $defaultReturn;
+		}
+
+		$newCurrency = self::$countryCurrency[$country]['currency'];
+
+		try
+		{
+			$newValue = self::convertCurrency($defaultAkeebaSubsCurrency, $newCurrency, $value, $container);
+			$exchangeRate = self::exhangeRate($defaultAkeebaSubsCurrency, $newCurrency, $container);
+			$newSymbol = self::getCurrencySymbol($newCurrency);
+		}
+		catch (\Exception $e)
+		{
+			// On error (e.g. currency not known in our system) we return the default return, i.e. no conversion
+			return $defaultReturn;
+		}
+
+		return [
+			'currency' => $newCurrency,
+			'symbol'   => $newSymbol,
+			'value'    => $newValue,
+			'rate'     => $exchangeRate
+		];
+	}
+
+	/**
+	 * Return the currency symbol for the specified currency.
+	 *
+	 * @param   string  $currency  The currency to get the symbol for, e.g. EUR
+	 *
+	 * @return  string  The currency symbol, e.g. €
+	 */
+	public static function getCurrencySymbol($currency)
+	{
+		$currency = strtoupper($currency);
+
+		if (isset(self::$currencySymbols[$currency]))
+		{
+			return self::$currencySymbols[$currency];
+		}
+
+		return $currency;
+	}
+
+	/**
+	 * Convert a value between currencies
+	 *
+	 * @param   string     $from       The currency you're converting from (the one $value is expressed in), e.g. EUR
+	 * @param   string     $to         The currency you're converting to, e.g. USD
+	 * @param   float      $value      The value you want to convert
+	 * @param   Container  $container  The container of the application we're running in
+	 *
+	 * @return  float
+	 *
+	 * @throw  \InvalidArgumentException  If an empty currency is specified or the currency is unknown
+	 */
+	public static function convertCurrency($from, $to, $value, Container $container = null)
+	{
+		if (empty($from) || empty($to))
+		{
+			throw new \InvalidArgumentException('Invalid currency');
+		}
+
+		if ($from == $to)
+		{
+			return $value;
+		}
+
+		$from = strtoupper($from);
+		$to = strtoupper($to);
+
+		if ($from == 'EUR')
+		{
+			return self::convertFromEuro($to, $value, $container);
+		}
+		elseif ($to == 'EUR')
+		{
+			return self::convertToEuro($from, $value, $container);
+		}
+		else
+		{
+			$intermediate = self::convertToEuro($from, $value, $container);
+			return self::convertFromEuro($to, $intermediate, $container);
+		}
+	}
+
+	/**
+	 * Get the exchange rate between two currencies
+	 *
+	 * @param   string     $from       Currency code to convert from, e.g. EUR
+	 * @param   string     $to         Currency code to convert to, e.g. USD
+	 * @param   Container  $container  The container of the application we're running in
+	 *
+	 * @return  float  The exchange rate
+	 */
+	public static function exhangeRate($from, $to, Container $container = null)
+	{
+		return self::convertCurrency($from, $to, 1.00, $container);
+	}
+
+	/**
+	 * Convert a value from Euro to a foreign currency
+	 *
+	 * @param   string     $to         The currency you're converting to, e.g. USD
+	 * @param   float      $value      The value you want to convert
+	 * @param   Container  $container  The container of the application we're running in
+	 *
+	 * @return  float
+	 *
+	 * @throw  \InvalidArgumentException  If an empty currency is specified or the currency is unknown
+	 */
+	public static function convertFromEuro($to, $value, Container $container = null)
+	{
+		// Reload rates if necessary
+		if (!($container instanceof Container))
+		{
+			$container = Container::getInstance('com_akeebasubs');
+		}
+
+		self::reloadRates(false, $container);
+
+		// Make sure the currency exists
+		if (!isset(self::$rates[$to]))
+		{
+			throw new \InvalidArgumentException("Unkown currency code $to");
+		}
+
+		// Return the result
+		return $value * self::$rates[$to]['rate'];
+	}
+
+	/**
+	 * Convert a value from a foreign currency to Euro
+	 *
+	 * @param   string     $from       The currency you're converting from (the one $value is expressed in), e.g. USD
+	 * @param   float      $value      The value you want to convert
+	 * @param   Container  $container  The container of the application we're running in
+	 *
+	 * @return  float
+	 *
+	 * @throw  \InvalidArgumentException  If an empty currency is specified or the currency is unknown
+	 */
+	public static function convertToEuro($from, $value, Container $container = null)
+	{
+		// Reload rates if necessary
+		if (!($container instanceof Container))
+		{
+			$container = Container::getInstance('com_akeebasubs');
+		}
+
+		self::reloadRates(false, $container);
+
+		// Make sure the currency exists
+		if (!isset(self::$rates[$from]))
+		{
+			throw new \InvalidArgumentException("Unkown currency code $from");
+		}
+
+		// Return the result
+		return $value / self::$rates[$from]['rate'];
+	}
+
+	/**
+	 * Get the last update timestamp from the database
+	 *
+	 * @param   Container  $container  The container of the application we're running in
+	 *
+	 * @return  int  The last update timestamp, zero if not set
+	 */
+	protected static function getLastUpdateTimestamp(Container $container = null)
+	{
+		if (!($container instanceof Container))
+		{
+			$container = Container::getInstance('com_akeebasubs');
+		}
+
+		$db = $container->db;
+
+		$query = $db->getQuery(true)
+			->select($db->qn('value'))
+			->from($db->qn('#__akeeba_common'))
+			->where($db->qn('key') . ' = ' . $db->q(self::$timestampKey));
+
+		try
+		{
+			$ret = $db->setQuery($query)->loadResult();
+
+			if (is_null($ret))
+			{
+				$ret = 0;
+			}
+
+			return $ret;
+		}
+		catch (\Exception $e)
+		{
+			return 0;
+		}
+	}
+
+	/**
+	 * Set the last update timestamp to right now
+	 *
+	 * @param   Container  $container  The container of the application we're running in
+	 *
+	 * @return  void
+	 */
+	protected static function setLastUpdateTimestamp(Container $container = null)
+	{
+		if (!($container instanceof Container))
+		{
+			$container = Container::getInstance('com_akeebasubs');
+		}
+
+		$db = $container->db;
+
+		$query = $db->getQuery(true)
+					->delete($db->qn('#__akeeba_common'))
+					->where($db->qn('key') . ' = ' . $db->q(self::$timestampKey));
+
+		try
+		{
+			$db->lockTable('#__akeeba_common');
+			$db->setQuery($query)->execute();
+		}
+		catch (\Exception $e)
+		{
+			// No problem if the record doesn't exists
+		}
+
+		$time = time();
+		$query = $db->getQuery(true)
+			->insert($db->qn('#__akeeba_common'))
+			->columns([
+				$db->qn('key'), $db->qn('value')
+			])->values($db->q(self::$timestampKey) . ', ' . $db->q($time));
+
+		try
+		{
+			$db->setQuery($query)->execute();
+		}
+		catch (\Exception $e)
+		{
+			// If we couldn't update the record it's not the end of the world
+		}
+
+		$db->unlockTables();
+	}
+
+	/**
+	 * Do I need to update the foreign exchange rate?
+	 *
+	 * @param   Container  $container  The container of the application we're running in
+	 *
+	 * @return  bool  True if we do have to update
+	 */
+	protected static function shouldUpdate(Container $container = null)
+	{
+		if (!($container instanceof Container))
+		{
+			$container = Container::getInstance('com_akeebasubs');
+		}
+
+		$lastTimestamp = self::getLastUpdateTimestamp($container);
+
+		$jNextUpdate = $container->platform->getDate($lastTimestamp);
+		$interval = new \DateInterval('P1D');
+		$jNextUpdate->add($interval);
+
+		$jNow = $container->platform->getDate();
+
+		return $jNow->toUnix() > $jNextUpdate->toUnix();
+	}
+}
