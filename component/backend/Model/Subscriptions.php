alexonbalangue/akeebasubs

commit d4a41e5393708f0f42b2907dcfddbc15fbf1ef68

# [HIGH] Expiration control is broken

component/backend/Model/Subscriptions.php
@@ -765,7 +765,7 @@ class Subscriptions extends DataModel
 
 		foreach ($resultArray as $index => &$row)
 		{
-			if (!property_exists($row, 'params'))
+			if (!($row instanceof Subscriptions))
 			{
 				continue;
 			}

commit ec2f20e76bdc28940bc82eba9154ed12892cb887

Infinite recursion on onAfterGetItemsArray triggering memory issue
This is a regression coming from commit d4a41e5393708f0f42b2907dcfddbc15fbf1ef68  It did fix the issue with the list processing not taking place but exposed this infinite recursion in the block of code which wasn't running before.

component/backend/Model/Subscriptions.php
@@ -748,6 +748,8 @@ class Subscriptions extends DataModel
 	 */
 	protected function onAfterGetItemsArray(array &$resultArray)
 	{
+		static $alreadyRunning = false;
+
 		// Implement the subscription automatic expiration
 		if (empty($resultArray))
 		{
@@ -759,9 +761,16 @@ class Subscriptions extends DataModel
 			return;
 		}
 
+		if ($alreadyRunning)
+		{
+			return;
+		}
+
 		JLoader::import('joomla.utilities.date');
 		$jNow = new JDate();
 		$uNow = $jNow->toUnix();
+
+		$alreadyRunning = true;
 
 		foreach ($resultArray as $index => &$row)
 		{
@@ -770,14 +779,6 @@ class Subscriptions extends DataModel
 				continue;
 			}
 
-			// TODO: This should no longer be necessary
-			if (!is_array($row->params))
-			{
-				if (!empty($row->params))
-				{
-					$row->params = json_decode($row->params, true);
-				}
-			}
 			if (is_null($row->params) || empty($row->params))
 			{
 				$row->params = array();
@@ -793,7 +794,7 @@ class Subscriptions extends DataModel
 				continue;
 			}
 
-			if ($row->publish_down && ($row->publish_down != '0000-00-00 00:00:00'))
+			if ($row->publish_down && ($row->publish_down != $this->getDbo()->getNullDate()))
 			{
 				$regex = '/^\d{1,4}(\/|-)\d{1,2}(\/|-)\d{2,4}[[:space:]]{0,}(\d{1,2}:\d{1,2}(:\d{1,2}){0,1}){0,1}$/';
 
@@ -827,6 +828,8 @@ class Subscriptions extends DataModel
 				$row->save();
 			}
 		}
+
+		$alreadyRunning = false;
 	}
 
 	/**