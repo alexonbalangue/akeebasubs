de4dff796610c8c4281ec267f52433527011aa02
Davide Tampellini <tampe125@gmail.com>
2015-08-20 16:44:38 +0200

    Added country fields to the payment plugin gh-183

---
+++ component/backend/assets/elements/akeebasubscountries.php
@@ -0,0 +1,14 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+
+JFormHelper::loadFieldClass('list');
+
+class JFormFieldAkeebasubscountries extends JFormFieldList
+{
+    protected function getOptions()
+    {
+        $options = array();
+
+        return $options;
+    }
+}
