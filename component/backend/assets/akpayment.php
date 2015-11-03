6bf022b8c4902751b6c06521c1b8bc41b53ff7e7
Piotr Moćko <mocko.piotr@gmail.com>
2015-11-03 13:51:47 +0100

    Fix replacing params when saving subscription in payment plugin
    In PayPal plugin on recurring payment there is create param recurring_id on line
    https://github.com/akeeba/akeebasubs/blob/4.x/plugins/akpayment/paypal/paypal.php#L321
    which is being discarded in method plgAkpaymentAbstract::fixSubscriptionDates

--- component/backend/assets/akpayment.php
+++ component/backend/assets/akpayment.php
@@ -184,7 +184,7 @@ abstract class plgAkpaymentAbstract extends JPlugin
 	{
 		// Take into account the params->fixdates data to determine when
 		// the new subscription should start and/or expire the old subscription
-		$subcustom = $subscription->params;
+		$subcustom = (!empty($updates['params']) ? $updates['params'] : $subscription->params);
 		if (is_string($subcustom))
 		{
 			$subcustom = json_decode($subcustom, true);
