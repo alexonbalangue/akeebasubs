181f4a40908efc3bc374bffe03a31bed37273c8f
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2016-03-07 15:59:12 +0200

    # [MEDIUM] The "Run Integrations" button didn't really work

---
+++ component/backend/Model/TaxHelper.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package		Akeeba Subscriptions
+ * @copyright	2015 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license		GNU GPL version 3 or later
+ */
+
+namespace Akeeba\Subscriptions\Admin\Model;
+
+defined('_JEXEC') or die;
+
+use Akeeba\Subscriptions\Site\Model\TaxHelper as FrontendTaxHelper;
+
+class TaxHelper extends FrontendTaxHelper
+{
+
+}
