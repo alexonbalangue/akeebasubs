1d534cf02300ca3920ae872bd86d96d4a84a45df
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2016-04-02 12:59:11 +0300

    Ongoing refactoring of the susbcription page
    One less file

--- component/frontend/View/Level/tmpl/default_prepayment.blade.php
+++ component/frontend/View/Level/tmpl/default_prepayment.blade.php
@@ -15,9 +15,50 @@ $jResponse = $this->getContainer()->platform->runPlugins('onSubscriptionFormPrep
 
 if (!is_array($jResponse) || empty($jResponse)) return;
 ?>
+@repeatable('customField', $field)
+<?php
+$field['isValid']  = array_key_exists('isValid', $field) ? $field['isValid'] : true;
+
+$customField_class = '';
+$classValidLabel   = '';
+$classInvalidLabel = '';
+
+if ($this->apply_validation == 'true')
+{
+	$customField_class = $field['isValid'] ? 'success has-success' : 'error has-error';
+	$classValidLabel   = $field['isValid'] ? '' : 'hidden';
+	$classInvalidLabel = !$field['isValid'] ? '' : 'hidden';
+}
+?>
+<div class="control-group form-group {{{$customField_class}}}">
+	<label for="{{{$field['id']}}}" class="control-label col-sm-2">
+		{{$field['label']}}
+	</label>
+
+	<div class="controls">
+		<span class="col-sm-3">
+			{{$field['elementHTML']}}
+		</span>
+
+		@if (array_key_exists('validLabel', $field))
+			<span id="{{{$field['id']}}}_valid" class="help-inline help-block {{$classValidLabel}}">
+			{{$field['validLabel']}}
+		</span>
+		@endif
+
+		@if (array_key_exists('invalidLabel', $field))
+			<span id="{{$field['id']}}_invalid" class="help-inline help-block {{$classInvalidLabel}}">
+			{{$field['invalidLabel']}}
+		</span>
+		@endif
+	</div>
+</div>
+@endRepeatable
 
 @foreach($jResponse as $customFields)
 	@if (is_array($customFields) && !empty($customFields))
-		@each('site:com_akeebasubs/Level/tmpl_customfield', $customFields, 'field', 'raw|No users found')
+		@foreach($customFields as $field)
+			@yieldRepeatable('customField', $field)
+		@endforeach
 	@endif
 @endforeach
